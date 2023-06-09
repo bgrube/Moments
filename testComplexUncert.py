7b7d1a14a21ca3406d5b0e798316db3e710fd811
bgrube <bgrube@jlab.org>
2023-06-09 15:09:10 -0400

    Restructure code. Improve naming.

--- testComplexUncert.py
+++ testComplexUncert.py
@@ -11,7 +11,7 @@ NMB_VARS = 4
 RNG = np.random.default_rng(seed = 12345)
 
 
-def getRandomCovariance(
+def getRandomCovarianceReal(
   nmbVars: int,
   rng:     np.random.Generator,
 ) -> np.ndarray:
@@ -27,34 +27,35 @@ def getRandomCovariance(
 
 
 A = RNG.random((NMB_VARS, NMB_VARS))
-def func(x: np.ndarray) -> np.ndarray:
+def realFunc(x: np.ndarray) -> np.ndarray:
   '''Function for which to perform uncertainty propagation'''
   # return x
   # return 2 * x
   return A @ x
 
 
-def funcJacobian(x: np.ndarray) -> np.ndarray:
+def realFuncJacobian(x: np.ndarray) -> np.ndarray:
   '''Returns Jacobian matrix of function evaluated at given point'''
   # return np.identity(x.shape[0])
   # return 2 * np.identity(x.shape[0])
   return A
 
 
-if __name__ == "__main__":
+def testRealVectorCase() -> None:
+  '''Tests uncertainty propagation for R^n -> R^n function'''
   # define means and covariance matrix of input values
   xMeans = RNG.random(NMB_VARS)
-  xCovMat = getRandomCovariance(NMB_VARS, RNG)
+  xCovMat = getRandomCovarianceReal(NMB_VARS, RNG)
 
   # perform Monte Carlo uncertainty propagation
   print(f"in: mu = {xMeans}, V = \n{xCovMat}")
   print(f"A = \n{A}")
   # generate samples from multi-variate Gaussian
-  nmbSamples = 10000000
+  nmbSamples = 1000000
   samples = RNG.multivariate_normal(mean = xMeans, cov = xCovMat, size = nmbSamples)
   print(samples.shape, samples[0])
   # function values for each sample
-  ySamples = np.array([func(x) for x in samples])
+  ySamples = np.array([realFunc(x) for x in samples])
   print(ySamples.shape, ySamples[0])
   # means and covariance matrix of function values
   yMeansMc  = np.mean(ySamples, axis = 0)
@@ -63,8 +64,12 @@ if __name__ == "__main__":
   print(f"factor = \n{np.divide(yCovMatMc, xCovMat)}")
 
   # perform analytic uncertainty propagation
-  yMeans  = func(xMeans)
-  J       = funcJacobian(xMeans)
+  yMeans  = realFunc(xMeans)
+  J       = realFuncJacobian(xMeans)
   yCovMat = J @ (xCovMat @ J.T)  #!Note! @ is left-associative
   print(f"analytic: mu = {yMeans}, V = \n{yCovMat}")
   print(f"factor = \n{np.divide(yCovMat, yCovMatMc)}")
+
+
+if __name__ == "__main__":
+  testRealVectorCase()
