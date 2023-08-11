c5ae34bf5af47066d51cdb713d8bd8ed0aa660f0
bgrube <bgrube@jlab.org>
2023-08-11 16:28:12 -0400

    Try to make OpenMP work on ifarm.

--- testBasisFunc.py
+++ testBasisFunc.py
@@ -5,6 +5,7 @@ os.environ["OMP_NUM_THREADS"] = "5"  # limit number of OpenMP threads; see also 
 
 import functools
 import numpy as np
+import subprocess
 
 import ROOT
 
@@ -44,7 +45,29 @@ def enableRootACLiCOpenMp():
   elif "linux" in arch.lower():
     print(f"Enabling ACLiC compilation with OpenMP for Linux")
     ROOT.gSystem.SetFlagsOpt("-fopenmp")  # compiler flags for optimized mode  # type: ignore
-    ROOT.gSystem.AddLinkedLibs("-lomp")  # type: ignore
+    ROOT.gSystem.AddLinkedLibs("-lgomp")  # type: ignore
+    # For usual setups, the above should be sufficient.
+    # On ifarm, however, ROOT seems to be compiled with a weird gcc setup.
+    # ACLiC does not search the gcc include directory at `/usr/lib/gcc/x86_64-redhat-linux/4.8.5/include`.`
+    # Instead, it seems to go to /usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../include/c++/4.8.5/,
+    # which is /usr/include/c++/4.8.5/ but does not contain omp.h.
+    # There seems to be no way to fix this without changing the source code.
+    # ACLiC always complaints that it cannot find `omp.h`.`
+    # # get gcc include directory
+    # result = subprocess.run("gcc -print-search-dirs",
+    #                         shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
+    # lines= result.stdout.split("\n")
+    # gccIncludePath = None
+    # for line in lines:
+    #   if "install:" in line:
+    #     gccIncludePath = line.split(": ")[1] + "include"
+    # ROOT.gSystem.AddIncludePath(f"-I \"{gccIncludePath}\"")  # this causes compile errors because it clashes with headers in `/usr/include/c++/4.8.5/`, which weirdly does not contain `omp.h``  # type: ignore
+    # ROOT.gSystem.AddIncludePath(f"-idirafter \"{gccIncludePath}\"")  # this has no effect # type: ignore
+    # ROOT.gSystem.AddIncludePath(f"-isystem \"{gccIncludePath}\"")  # this has no effect # type: ignore
+    #
+    # The only way to make it work is to copy/link `omp.h`` from the
+    # gcc include dir here and turn the system include statement in
+    # `wigner.C`` into the quoted form.
 enableRootACLiCOpenMp()
 
 
