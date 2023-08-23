ac591e37a29eccf5a6cba2e301b4ae891cda38d6
bgrube <bgrube@jlab.org>
2023-08-23 15:08:21 -0400

    Remove code moved to OpenMp module. Remove non-working code.

--- testBasisFunc.py
+++ testBasisFunc.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-import os
-OMP_NUM_THREADS_save = None
-if "OMP_NUM_THREADS" in os.environ:
-  OMP_NUM_THREADS_save = os.environ["OMP_NUM_THREADS"]
-os.environ["OMP_NUM_THREADS"] = "5"  # limit number of OpenMP threads; see also threadpoolctl
+import OpenMp
 
 import functools
 import numpy as np
-import subprocess
 import timeit
 
 import ROOT
@@ -18,72 +13,17 @@ import ROOT
 print = functools.partial(print, flush = True)
 
 
-def printRootACLiCSettings():
-  '''Prints ROOT settings that affect ACLiC compilation'''
-  print(f" GetBuildArch()               = {ROOT.gSystem.GetBuildArch()}")
-  print(f" GetBuildCompiler()           = {ROOT.gSystem.GetBuildCompiler()}")
-  print(f" GetBuildCompilerVersion()    = {ROOT.gSystem.GetBuildCompilerVersion()}")
-  print(f" GetBuildCompilerVersionStr() = {ROOT.gSystem.GetBuildCompilerVersionStr()}")
-  print(f" GetBuildDir()                = {ROOT.gSystem.GetBuildDir()}")
-  print(f" GetFlagsDebug()              = {ROOT.gSystem.GetFlagsDebug()}")
-  print(f" GetFlagsOpt()                = {ROOT.gSystem.GetFlagsOpt()}")
-  print(f" GetIncludePath()             = {ROOT.gSystem.GetIncludePath()}")
-  print(f" GetDynamicPath()             = {ROOT.gSystem.GetDynamicPath()}")
-  print(f" GetLinkedLibs()              = {ROOT.gSystem.GetLinkedLibs()}")
-  print(f" GetLinkdefSuffix()           = {ROOT.gSystem.GetLinkdefSuffix()}")
-  print(f" GetLibraries()               = {ROOT.gSystem.GetLibraries()}")
-  print(f" GetMakeExe()                 = {ROOT.gSystem.GetMakeExe()}")
-  print(f" GetMakeSharedLib()           = {ROOT.gSystem.GetMakeSharedLib()}")
-
-
-def enableRootACLiCOpenMp():
-  '''Enables openMP support for ROOT macros compiled via ACLiC'''
-  arch = ROOT.gSystem.GetBuildArch()
-  if "macos" in arch.lower():
-    # !Note! MacOS (Apple does not ship libomp; needs to be installed via MacPorts or Homebrew see testOpenMp.c)
-    print(f"Enabling ACLiC compilation with OpenMP for MacOS")
-    ROOT.gSystem.SetFlagsOpt("-Xpreprocessor -fopenmp")  # compiler flags for optimized mode
-    ROOT.gSystem.AddIncludePath("-I/opt/local/include/libomp")
-    ROOT.gSystem.AddDynamicPath("/opt/local/lib/libomp")
-    ROOT.gSystem.AddLinkedLibs("-L/opt/local/lib/libomp -lomp")
-  elif "linux" in arch.lower():
-    print(f"Enabling ACLiC compilation with OpenMP for Linux")
-    ROOT.gSystem.SetFlagsOpt("-fopenmp")  # compiler flags for optimized mode
-    ROOT.gSystem.AddLinkedLibs("-lgomp")
-    # For usual setups, the above should be sufficient.
-    # On ifarm, however, ROOT seems to be compiled with a weird gcc setup.
-    # ACLiC does not search the gcc include directory at `/usr/lib/gcc/x86_64-redhat-linux/4.8.5/include`.`
-    # Instead, it seems to go to /usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../include/c++/4.8.5/,
-    # which is /usr/include/c++/4.8.5/ but does not contain omp.h.
-    # There seems to be no way to fix this without changing the source code.
-    # ACLiC always complaints that it cannot find `omp.h`.`
-    # # get gcc include directory
-    # result = subprocess.run("gcc -print-search-dirs",
-    #                         shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
-    # lines= result.stdout.split("\n")
-    # gccIncludePath = None
-    # for line in lines:
-    #   if "install:" in line:
-    #     gccIncludePath = line.split(": ")[1] + "include"
-    # ROOT.gSystem.AddIncludePath(f"-I \"{gccIncludePath}\"")  # this causes compile errors because it clashes with headers in `/usr/include/c++/4.8.5/`, which weirdly does not contain `omp.h``
-    # ROOT.gSystem.AddIncludePath(f"-idirafter \"{gccIncludePath}\"")  # this has no effect # type: ignore
-    # ROOT.gSystem.AddIncludePath(f"-isystem \"{gccIncludePath}\"")  # this has no effect # type: ignore
-    #
-    # The only way to make it work is to copy/link `omp.h`` from the
-    # gcc include dir here and turn the system include statement in
-    # `wigner.C`` into the quoted form.
-
-
 if __name__ == "__main__":
   ROOT.gROOT.SetBatch(True)
 
-  enableRootACLiCOpenMp()
-  printRootACLiCSettings()
+  OpenMp.enableRootACLiCOpenMp()
+  OpenMp.printRootACLiCSettings()
   # C++ implementation of (complex conjugated) Wigner D function and spherical harmonics
   # also provides complexT typedef for std::complex<double>
+  # OpenMp.setNmbOpenMpThreads(1)
+  OpenMp.setNmbOpenMpThreads(5)
   ROOT.gROOT.LoadMacro("./wignerD.C++")
   print(f"Using {ROOT.getNmbOpenMpThreads()} OpenMP threads")
-
   ROOT.testOpenMp()
 
   # see https://root.cern/doc/master/pyroot001__arrayInterface_8py.html
@@ -97,18 +37,8 @@ if __name__ == "__main__":
   phi   = ROOT.std.vector["double"](vals)
   Phi   = ROOT.std.vector["double"](vals)
   start = timeit.default_timer()
-  fcnResults1 = np.asarray(ROOT.f_phys(1, 3, 2, theta, phi, Phi, 1.0))
+  fcnResults = np.asarray(ROOT.f_phys(1, 3, 2, theta, phi, Phi, 1.0))
   stop = timeit.default_timer()
-  time1 = stop -start
-  print(f"1 thread: {str(time1)} sec")
-  start = timeit.default_timer()
-  fcnResults2 = np.asarray(ROOT.f_phys_omp(1, 3, 2, theta, phi, Phi, 1.0))
-  stop = timeit.default_timer()
-  time2 = stop -start
-  print(f"{ROOT.getNmbOpenMpThreads()} threads: {str(time2)} sec; speedup = {time1 / time2}; efficiency = {(time1 / time2) / ROOT.getNmbOpenMpThreads()}")
-  print(f"maximum difference: {max(fcnResults1 - fcnResults2):.16e}")
-  # unset OpenMP variable
-  if OMP_NUM_THREADS_save:
-    os.environ["OMP_NUM_THREADS"] = OMP_NUM_THREADS_save
-  else:
-    del os.environ["OMP_NUM_THREADS"]
+  time = stop -start
+  print(f"{ROOT.getNmbOpenMpThreads()} threads: {str(time)} sec")
+  OpenMp.restoreNmbOpenMpThreads()
