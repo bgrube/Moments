ca0e1ad4e5a01a097233eb8d5ed6d214de3d4a69
bgrube <bgrube@jlab.org>
2023-08-24 12:09:22 -0400

    Add first implementation of classes that calculate moments. Fix bug in AcceptanceIntegralMatrix.load(). Add string conversion methods. Use int instead of numbers.Integral. Improve naming, console output, and comments.

--- MomentCalculator.py
+++ MomentCalculator.py
@@ -2,12 +2,10 @@
 
 
 import bidict as bd
-from dataclasses import dataclass
-import math
-import numbers
+from dataclasses import dataclass, field
 import numpy as np
 import nptyping as npt
-from typing import Dict, Generator, Optional, Tuple, Union
+from typing import Generator, Optional, Tuple, Union
 
 import ROOT
 
@@ -70,31 +68,38 @@ class DataSet:
 class AcceptanceIntegralMatrix:
   '''Calculates and provides access to acceptance integral matrix'''
   index:      MomentIndex  # index mapping and iterators
-  dataSet:    DataSet      # access to data samples
+  dataSet:    DataSet      # info on data samples
   _IFlatIndex: Optional[npt.NDArray[npt.Shape["Dim, Dim"], npt.Complex128]] = None  # integral matrix with flat indices
+
+  def __str__(self) -> str:
+    if self._IFlatIndex is None:
+      return "None"
+    else:
+      return np.array2string(self._IFlatIndex, precision = 3, suppress_small = True, max_line_width = 150)
 
   def __getitem__(
     self,
-    indices: Tuple[Union[numbers.Integral, QnIndex, slice], Union[numbers.Integral, QnIndex, slice]],
+    indices: Tuple[Union[int, QnIndex, slice], Union[int, QnIndex, slice]],
   ) -> Optional[Union[complex, npt.NDArray[npt.Shape["*"], npt.Complex128], npt.NDArray[npt.Shape["*, *"], npt.Complex128]]]:
     '''Returns integral matrix elements for any combination of flat and quantum-number indices'''
     if self._IFlatIndex is None:
       return None
     else:
       # turn quantum-number indices to flat indices
-      flatIndexMeas: Union[numbers.Integral, slice] = self.index.indexMap.flatIndex_for[indices[0]] if isinstance(indices[0], QnIndex) else indices[0]
-      flatIndexPhys: Union[numbers.Integral, slice] = self.index.indexMap.flatIndex_for[indices[1]] if isinstance(indices[1], QnIndex) else indices[1]
+      flatIndexMeas: Union[int, slice] = self.index.indexMap.flatIndex_for[indices[0]] if isinstance(indices[0], QnIndex) else indices[0]
+      flatIndexPhys: Union[int, slice] = self.index.indexMap.flatIndex_for[indices[1]] if isinstance(indices[1], QnIndex) else indices[1]
       return self._IFlatIndex[flatIndexMeas, flatIndexPhys]
 
-  def calculateMatrix(self) -> None:
+  def calculate(self) -> None:
     '''Calculates integral matrix of basis functions from (accepted) phase-space data'''
     # get phase-space data data as NumPy arrays
     thetas = self.dataSet.phaseSpaceData.AsNumpy(columns = ["theta"])["theta"]
     phis   = self.dataSet.phaseSpaceData.AsNumpy(columns = ["phi"]  )["phi"]
     Phis   = self.dataSet.phaseSpaceData.AsNumpy(columns = ["Phi"]  )["Phi"]
+    print(f"Phase-space data column: {type(thetas)}; {thetas.shape}; {thetas.dtype}; {thetas.dtype.type}")
     nmbAccEvents = len(thetas)
     assert thetas.shape == (nmbAccEvents,) and thetas.shape == phis.shape == Phis.shape, (
-      f"Not all NumPy arrays with input data have shape ({nmbAccEvents},): theta: {thetas.shape} vs. phi: {phis.shape} vs. Phi: {Phis.shape}")
+      f"Not all NumPy arrays with input data have the correct shape. Expected ({nmbAccEvents},) but got theta: {thetas.shape}, phi: {phis.shape}, and Phi: {Phis.shape}")
     # calculate basis-function values for physical and measured moments; Eqs. (175) and (176); defined in `wignerD.C`
     nmbMoments = self.index.nmbMoments
     fMeas: npt.NDArray[npt.Shape["*"], npt.Complex128] = np.empty((nmbMoments, nmbAccEvents), dtype = np.complex128)
@@ -107,12 +112,12 @@ class AcceptanceIntegralMatrix:
     self._IFlatIndex = np.empty((nmbMoments, nmbMoments), dtype = np.complex128)
     for flatIndexMeas in self.index.flatIndices():
       for flatIndexPhys in self.index.flatIndices():
-        self._IFlatIndex[flatIndexMeas, flatIndexPhys] = (8 * math.pi**2 / self.dataSet.nmbGenEvents) * np.dot(fMeas[flatIndexMeas], fPhys[flatIndexPhys])
+        self._IFlatIndex[flatIndexMeas, flatIndexPhys] = (8 * np.pi**2 / self.dataSet.nmbGenEvents) * np.dot(fMeas[flatIndexMeas], fPhys[flatIndexPhys])
 
   def isValid(self) -> bool:
     return (self._IFlatIndex is not None) and self._IFlatIndex.shape == (self.index.nmbMoments, self.index.nmbMoments)
 
-  def saveMatrix(
+  def save(
     self,
     fileName: str = "./integralMatrix.npy",
   ) -> None:
@@ -121,24 +126,184 @@ class AcceptanceIntegralMatrix:
       print(f"Saving integral matrix to file '{fileName}'.")
       np.save(fileName, self._IFlatIndex)
 
-  def loadMatrix(
+  def load(
     self,
     fileName: str = "./integralMatrix.npy",
   ) -> None:
     '''Loads NumPy array that holds the integral matrix from file with given name'''
     print(f"Loading integral matrix from file '{fileName}'.")
     array = np.load(fileName)
-    if not self.isValid():
-      raise IndexError(f"Integral loaded from file '{fileName}' has wrong shape. Expected {(self.index.nmbMoments, self.index.nmbMoments)}, got {array.shape}.")
+    if not array.shape == (self.index.nmbMoments, self.index.nmbMoments):
+      raise IndexError(f"Integral loaded from file '{fileName}' has wrong shape. Expected {(self.index.nmbMoments, self.index.nmbMoments)} but got {array.shape}.")
     self._IFlatIndex = array
 
-  def loadOrCalculateMatrix(
+  def loadOrCalculate(
     self,
     fileName: str = "./integralMatrix.npy",
   ) -> None:
     '''Loads NumPy array that holds the integral matrix from file with given name; and calculates the integral matrix if loading failed'''
     try:
-      self.loadMatrix(fileName)
-    except:
-      print(f"Could not load integral matrix from file '{fileName}'; calculating matrix instead.")
-      self.calculateMatrix()
+      self.load(fileName)
+    except Exception as e:
+      print(f"Could not load integral matrix from file '{fileName}': {e} Calculating matrix instead.")
+      self.calculate()
+
+
+@dataclass
+class MomentResult:
+  '''Stores and provides access to moment values'''
+  index:       MomentIndex  # index mapping and iterators
+  label:       str = ""     # label used for printing
+  _HFlatIndex: npt.RecArray[npt.Shape["*"], npt.Structure["val: Complex, uncertRe: Float, uncertIm: Float, covReIm: Float"]] = field(init = False)
+  # flat array with moment values, uncertainties for real and imaginary parts and covariance between the two
+
+  def __post_init__(self) -> None:
+    self._HFlatIndex = np.recarray((self.index.nmbMoments, ), dtype = [("val", npt.Complex128), ("uncertRe", float), ("uncertIm", float), ("covReIm", float)])
+
+  def __str__(self) -> str:
+    result = ""
+    for flatIndex in self.index.flatIndices():
+      qnIndex = self.index.indexMap.QnIndex_for[flatIndex]
+      momentSymbol = f"H{'^' + self.label if self.label else ''}_{qnIndex.momentIndex}(L = {qnIndex.L}, M = {qnIndex.M})"
+      result += (f"Re[{momentSymbol}] = {self._HFlatIndex[flatIndex].val.real} +- {self._HFlatIndex[flatIndex].uncertRe}\n"
+                 f"Im[{momentSymbol}] = {self._HFlatIndex[flatIndex].val.imag} +- {self._HFlatIndex[flatIndex].uncertIm}\n")
+    return result
+
+  def __getitem__(
+    self,
+    index: Union[int, QnIndex, slice],
+  ):  #TODO add type hint
+    '''Returns moment value and corresponding uncertainties at the given flat or quantum-number index'''
+    # turn quantum-number index to flat index
+    flatIndex: Union[int, slice] = self.index.indexMap.flatIndex_for[index] if isinstance(index, QnIndex) else index
+    return self._HFlatIndex[flatIndex]
+
+  @property
+  def values(self) -> npt.NDArray[npt.Shape["*"], npt.Complex128]:
+    return self._HFlatIndex.val
+  #TODO add setters
+
+  @property
+  def uncertaintiesReal(self) -> npt.NDArray[npt.Shape["*"], npt.Float64]:
+    return self._HFlatIndex.uncertRe
+
+  @property
+  def uncertaintiesImag(self) -> npt.NDArray[npt.Shape["*"], npt.Float64]:
+    return self._HFlatIndex.uncertIm
+
+  @property
+  def covariancesRealImag(self) -> npt.NDArray[npt.Shape["*"], npt.Float64]:
+    return self._HFlatIndex.covReIm
+
+
+@dataclass
+class MomentCalculator:
+  '''Calculates and provides access to moments'''
+  index:          MomentIndex  # index mapping and iterators
+  dataSet:        DataSet      # info on data samples
+  integralMatrix: Optional[AcceptanceIntegralMatrix] = None  # acceptance integral matrix
+  HMeas:          Optional[MomentResult]             = None  # calculated measured moments
+  HPhys:          Optional[MomentResult]             = None  # calculated physical moments
+
+  def _calcReImCovMatrices(
+    self,
+    V_aug: npt.NDArray[npt.Shape["Dim, Dim"], npt.Complex128],  # augmentented covariance matrix
+  ) -> Tuple[npt.NDArray[npt.Shape["Dim, Dim"], npt.Float64], npt.NDArray[npt.Shape["Dim, Dim"], npt.Float64], npt.NDArray[npt.Shape["Dim, Dim"], npt.Float64]]:
+    '''Calculates covariance matrices for real parts, for imaginary parts, and for real and imaginary parts from augmented covariance matrix'''
+    nmbMoments = self.index.nmbMoments
+    V_Hermit = V_aug[:nmbMoments, :nmbMoments]  # Hermitian covariance matrix; Eq. (88)
+    V_pseudo = V_aug[:nmbMoments, nmbMoments:]  # pseudo-covariance matrix; Eq. (88)
+    V_ReRe = (np.real(V_Hermit) + np.real(V_pseudo)) / 2  # Eq. (91)
+    V_ImIm = (np.real(V_Hermit) - np.real(V_pseudo)) / 2  # Eq. (92)
+    V_ReIm = (np.imag(V_pseudo) - np.imag(V_Hermit)) / 2  # Eq. (93)
+    return (V_ReRe, V_ImIm, V_ReIm)
+
+  def calculate(self) -> None:
+    '''Calculates photoproduction moments and their covariances'''
+    # get input data as NumPy arrays
+    thetas = self.dataSet.data.AsNumpy(columns = ["theta"])["theta"]
+    phis   = self.dataSet.data.AsNumpy(columns = ["phi"]  )["phi"]
+    Phis   = self.dataSet.data.AsNumpy(columns = ["Phi"]  )["Phi"]
+    print(f"Input data column: {type(thetas)}; {thetas.shape}; {thetas.dtype}; {thetas.dtype.type}")
+    nmbEvents = len(thetas)
+    assert thetas.shape == (nmbEvents,) and thetas.shape == phis.shape == Phis.shape, (
+      f"Not all NumPy arrays with input data have the correct shape. Expected ({nmbEvents},) but got theta: {thetas.shape}, phi: {phis.shape}, and Phi: {Phis.shape}")
+    # get number of moments (the poor-man's way)
+    nmbMoments = self.index.nmbMoments
+    # calculate basis-function values and values of measured moments
+    fMeas  = np.empty((nmbMoments, nmbEvents), dtype = npt.Complex128)
+    for flatIndex in self.index.flatIndices():
+      qnIndex = self.index.indexMap.QnIndex_for[flatIndex]
+      fMeas[flatIndex] = np.asarray(ROOT.f_meas(qnIndex.momentIndex, qnIndex.L, qnIndex.M, thetas, phis, Phis, self.dataSet.polarization))  # Eq. (176)  # type: ignore
+    # calculate covariances; Eqs. (88), (180), and (181)
+    V_meas_aug = (2 * np.pi)**2 * nmbEvents * np.cov(fMeas, np.conjugate(fMeas))  # augmented covariance matrix
+    V_meas_ReRe, V_meas_ImIm, V_meas_ReIm = self._calcReImCovMatrices(V_meas_aug)
+    self.HMeas = MomentResult(self.index, "meas")
+    # calculate values and uncertainties of measured moments
+    for flatIndex in self.index.flatIndices():
+      self.HMeas[flatIndex].val      = 2 * np.pi * np.sum(fMeas[flatIndex])        # Eq. (179)
+      self.HMeas[flatIndex].uncertRe = np.sqrt(V_meas_ReRe[flatIndex, flatIndex])  # uncertainty of real part
+      self.HMeas[flatIndex].uncertIm = np.sqrt(V_meas_ImIm[flatIndex, flatIndex])  # uncertainty of imaginary part
+      self.HMeas[flatIndex].covReIm  = V_meas_ReIm[flatIndex, flatIndex]             # covariance of real and imaginary part
+    print(self.HMeas)
+    # self.HPhys = MomentResult(self.index, "phys")
+    # V_phys_aug = np.empty((2 * nmbMoments, 2 * nmbMoments), dtype = npt.Complex128)
+    # if self.integralMatrix is None:
+    #   # ideal detector
+    #   self.HPhys = self.HMeas
+    #   V_phys_aug = V_meas_aug
+    # else:
+    #   # get acceptance integral matrix
+    #   assert self.integralMatrix._IFlatIndex is not None, "Integral matrix is None."
+    #   print(f"Acceptance integral matrix = \n{self.integralMatrix}")
+    #   I_acc: npt.NDArray[npt.Shape["Dim, Dim"], npt.Complex128] = self.integralMatrix._IFlatIndex
+    #   eigenVals, eigenVecs = np.linalg.eig(I_acc)
+    #   print(f"I_acc eigenvalues = {eigenVals}")
+    #   # print(f"I_acc eigenvectors = {eigenVecs}")
+    #   # print(f"I_acc determinant = {np.linalg.det(I_acc)}")
+    #   # print(f"I_acc = \n{np.array2string(I_acc, precision = 3, suppress_small = True, max_line_width = 150)}")
+    #   #TODO move to utilities module
+    #   # plotComplexMatrix(I_acc, fileNamePrefix = "I_acc")
+    #   I_inv = np.linalg.inv(I_acc)
+    #   # eigenVals, eigenVecs = np.linalg.eig(I_inv)
+    #   # print(f"I^-1 eigenvalues = {eigenVals}")
+    #   # print(f"I^-1 = \n{np.array2string(I_inv, precision = 3, suppress_small = True, max_line_width = 150)}")
+    #   # plotComplexMatrix(I_inv, fileNamePrefix = "I_inv")
+    #   # calculate physical moments, i.e. correct for detection efficiency
+    #   H_phys = I_inv @ self.HMeas.values  # Eq. (83)
+    #   # perform linear uncertainty propagation
+    #   J = I_inv  # Jacobian of efficiency correction; Eq. (101)
+    #   J_conj = np.zeros((nmbMoments, nmbMoments), dtype = npt.Complex128)  # conjugate Jacobian; Eq. (101)
+    #   J_aug = np.block([
+    #     [J,                    J_conj],
+    #     [np.conjugate(J_conj), np.conjugate(J)],
+    #   ])  # augmented Jacobian; Eq. (98)
+    #   V_phys_aug = J_aug @ (V_meas_aug @ np.asmatrix(J_aug).H)  #!Note! @ is left-associative; Eq. (85)
+    # # normalize such that H_0(0, 0) = 1
+    # norm = H_phys[0]
+    # H_phys /= norm
+    # V_phys_aug /= norm**2
+    # V_phys_ReRe, V_phys_ImIm, V_phys_ReIm = MomentCalculator._calcReImCovMatrices(V_phys_aug)
+    # # # reformat output
+    # # momentsPhys:    List[Tuple[Tuple[int, int, int], complex]] = []
+    # # momentsPhysCov: Dict[Tuple[int, ...], Tuple[float, ...]]   = {}  # cov[(i, L, M, j, L', M')] = (cov[ReRe], cov[ImIm], cov[ReIm])
+    # # iMoment_1 = 0
+    # # for momentIndex_1 in range(3):
+    # #   for L_1 in range(maxL + 1):
+    # #     for M_1 in range(L_1 + 1):
+    # #       if momentIndex_1 == 2 and M_1 == 0:
+    # #         continue  # H_2(L, 0) are always zero
+    # #       momentsPhys.append(((momentIndex_1, L_1, M_1), H_phys[iMoment_1]))
+    # #       iMoment_2 = 0
+    # #       for momentIndex_2 in range(3):
+    # #         for L_2 in range(maxL + 1):
+    # #           for M_2 in range(L_2 + 1):
+    # #             if momentIndex_2 == 2 and M_2 == 0:
+    # #               continue  # H_2(L, 0) are always zero
+    # #             momentsPhysCov[(momentIndex_2, L_2, M_2, momentIndex_1, L_1, M_1)] = (
+    # #               (V_phys_ReRe[iMoment_2, iMoment_1],
+    # #                V_phys_ImIm[iMoment_2, iMoment_1],
+    # #                V_phys_ReIm[iMoment_2, iMoment_1]))
+    # #             iMoment_2 += 1
+    # #       iMoment_1 += 1
+    # # return momentsPhys, momentsPhysCov
