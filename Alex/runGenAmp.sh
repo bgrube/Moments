e5397cdfe6178141e24117d852588c2248043b9c
bgrube <bgrube@jlab.org>
2023-09-27 17:41:07 -0400

    Improve file name.

--- Alex/runGenAmp.sh
+++ Alex/runGenAmp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-gen_amp -c gen_amp_030994_002.conf -o gen_amp.root -n 100000 -l 0.6 -u 0.88 -a 8.2 -b 8.8 -tmin 0.05
+gen_amp -c gen_amp_030994_002.conf -o gen_amp_030994.root -n 100000 -l 0.6 -u 0.88 -a 8.2 -b 8.8 -tmin 0.05
