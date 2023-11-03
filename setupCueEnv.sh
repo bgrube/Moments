e06b339f47e564598da168151012f8b96c1b22af
bgrube <bgrube@jlab.org>
2023-11-03 15:44:31 -0400

    Add include search path for OpenMP.

--- setupCueEnv.sh
+++ setupCueEnv.sh
@@ -1,3 +1,5 @@
 module load gcc/10.2.0
 module load python/3.9.5
 source /apps/root/6.28.06/setROOT_CUE-gcc10.2.0.sh
+# add gcc includes to search path; otherwise ACLiC cannot find omp.h
+export CPATH="/apps/gcc/10.2.0/lib/gcc/x86_64-pc-linux-gnu/10.2.0/include/:${CPATH}"
