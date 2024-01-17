39a65b24e1161508f79d6dfd5464785b190bf3f5
bgrube <bgrube@jlab.org>
2024-01-17 13:54:33 -0500

    Improve comments and naming.

--- Alex/FSRootMacros.C
+++ Alex/FSRootMacros.C
@@ -18,10 +18,10 @@ class MyFSMath{
 
 	public:
 
-	// for beam + target -> X + recoil and X -> a + b (see FSBasic/FSMath.h)
-	//      D               R     C        R    A   B
-	// azimuthal angle of particle a in X Gottfried-Jackson RF [rad]
-	//   GJPHI(a; b; recoil; beam)
+	// GJPHI(a; b; recoil; beam) returns azimuthal angle of particle a in X Gottfried-Jackson RF [rad]
+	// for beam + target -> X + recoil and X -> a + b
+	//     D                    C               A   B
+	// copied from FSRoot/FSBasic/FSMath.{h,C}
 	static
 	double
 	gjphi(
@@ -30,37 +30,37 @@ class MyFSMath{
 		const double PxPC, const double PyPC, const double PzPC, const double EnPC,
 		const double PxPD, const double PyPD, const double PzPD, const double EnPD
 	) {
-		TLorentzVector       pa    (PxPA, PyPA, PzPA, EnPA);
-		const TLorentzVector pb    (PxPB, PyPB, PzPB, EnPB);
+		TLorentzVector       pA    (PxPA, PyPA, PzPA, EnPA);
+		const TLorentzVector pB    (PxPB, PyPB, PzPB, EnPB);
 		TLorentzVector       recoil(PxPC, PyPC, PzPC, EnPC);
 		TLorentzVector       beam  (PxPD, PyPD, PzPD, EnPD);
-		const TLorentzVector resonance = pa + pb;
+		const TLorentzVector resonance = pA + pB;
 		// boost all needed four-vectors to the resonance rest frame
 		const TVector3 boostVect = -resonance.BoostVector();
-		pa.Boost    (boostVect);
+		pA.Boost    (boostVect);
 		recoil.Boost(boostVect);
 		beam.Boost  (boostVect);
 		// rotate so beam is aligned along the z-axis
-		pa.RotateZ(-beam.Phi());    recoil.RotateZ(-beam.Phi());
-		pa.RotateY(-beam.Theta());  recoil.RotateY(-beam.Theta());
+		pA.RotateZ(-beam.Phi());    recoil.RotateZ(-beam.Phi());
+		pA.RotateY(-beam.Theta());  recoil.RotateY(-beam.Theta());
 		// rotate so recoil is in the xz-plane
-		pa.RotateZ(-recoil.Phi());
-		return pa.Phi();
+		pA.RotateZ(-recoil.Phi());
+		return pA.Phi();
 	}
 
 
+	// BIGPHI(recoil; beam; polarization angle) returns azimuthal angle of photon polarization vector in X rest frame [rad]
 	// for beam + target -> X + recoil and X -> a + b
-	//      D               R     C        R    A   B
-	// azimuthal angle of photon polarization vector in R rest frame [rad]
-	// BIGPHI(a; b; recoil; beam)
+	//     D                    C
 	// code taken from https://github.com/JeffersonLab/halld_sim/blob/538677ee1347891ccefa5780e01b158e035b49b1/src/libraries/AMPTOOLS_AMPS/TwoPiAngles.cc#L94
 	static
 	double
 	bigPhi(
 		const double PxPC, const double PyPC, const double PzPC, const double EnPC,
 		const double PxPD, const double PyPD, const double PzPD, const double EnPD,
-		const double polAngle  // [deg]
+		const double polAngle  // polarization angle [deg]
 	) {
+		assert(polAngle == 45);  // MC data was generated with this value
 		TLorentzVector recoil(PxPC, PyPC, PzPC, EnPC);
 		TLorentzVector beam  (PxPD, PyPD, PzPD, EnPD);
 		const TVector3 yAxis = (beam.Vect().Unit().Cross(-recoil.Vect().Unit())).Unit();  // normal of production plane in lab frame
@@ -73,7 +73,6 @@ class MyFSMath{
 		while (Phi < -TMath::Pi()) {
 			Phi += TMath::TwoPi();
 		}
-		assert(polAngle == 45);
 		return Phi;
 	}
 
