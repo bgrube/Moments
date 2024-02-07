d6bc9f336a7cd0d06aaba647de55098d92588ec1
bgrube <bgrube@jlab.org>
2024-02-07 17:28:02 -0500

    Improve code layout.

--- testMomentsPhotoProd.py
+++ testMomentsPhotoProd.py
@@ -107,16 +107,18 @@ def genDataFromWaves(
     std::vector<double> point = {{cosTheta, phiDeg, PhiDeg}};
     return point;
   """  # C++ code that throws random point in angular space
-  df = ROOT.RDataFrame(nmbEvents) \
-           .Define("point",    pointFunc) \
-           .Define("cosTheta", "point[0]") \
-           .Define("theta",    "std::acos(cosTheta)") \
-           .Define("phiDeg",   "point[1]") \
-           .Define("phi",      "TMath::DegToRad() * phiDeg") \
-           .Define("PhiDeg",   "point[2]") \
-           .Define("Phi",      "TMath::DegToRad() * PhiDeg") \
-           .Filter('if (rdfentry_ == 0) { cout << "Running event loop in genDataFromWaves()" << endl; } return true;') \
-           .Snapshot(treeName, fileName, ROOT.std.vector[ROOT.std.string](["cosTheta", "theta", "phiDeg", "phi", "PhiDeg", "Phi"]))
+  df = (
+    ROOT.RDataFrame(nmbEvents)
+        .Define("point",    pointFunc)
+        .Define("cosTheta", "point[0]")
+        .Define("theta",    "std::acos(cosTheta)")
+        .Define("phiDeg",   "point[1]")
+        .Define("phi",      "TMath::DegToRad() * phiDeg")
+        .Define("PhiDeg",   "point[2]")
+        .Define("Phi",      "TMath::DegToRad() * PhiDeg")
+        .Filter('if (rdfentry_ == 0) { cout << "Running event loop in genDataFromWaves()" << endl; } return true;')
+        .Snapshot(treeName, fileName, ROOT.std.vector[ROOT.std.string](["cosTheta", "theta", "phiDeg", "phi", "PhiDeg", "Phi"]))
+  )
     # snapshot is needed or else the `point` column would be regenerated for every triggered loop
     # noop filter before snapshot logs when event loop is running
     # !Note! for some reason, this is very slow
