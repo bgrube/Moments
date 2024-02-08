48ce0a6a5724a2ddc7d4e167309706dee0f9dbe2
bgrube <bgrube@jlab.org>
2024-02-08 16:44:19 -0500

    Add code to verify that formula for Phi used in eta pi0 analysis is equivalent. Improve comments and constness.

--- dataPhotoProdRho/FSRootMacros.C
+++ dataPhotoProdRho/FSRootMacros.C
@@ -49,7 +49,7 @@ class MyFSMath{
 	}
 
 
-	// BIGPHI(recoil; beam; polarization angle) returns azimuthal angle of photon polarization vector in X rest frame [rad]
+	// BIGPHI(recoil; beam; polarization angle) returns azimuthal angle of photon polarization vector in lab frame [rad]
 	// for beam + target -> X + recoil and X -> a + b
 	//     D                    C
 	// code taken from https://github.com/JeffersonLab/halld_sim/blob/538677ee1347891ccefa5780e01b158e035b49b1/src/libraries/AMPTOOLS_AMPS/TwoPiAngles.cc#L94
@@ -61,8 +61,8 @@ class MyFSMath{
 		const double polAngle  // polarization angle [deg]
 	) {
 		assert(polAngle == 45);  // MC data was generated with this value
-		TLorentzVector recoil(PxPC, PyPC, PzPC, EnPC);
-		TLorentzVector beam  (PxPD, PyPD, PzPD, EnPD);
+		const TLorentzVector recoil(PxPC, PyPC, PzPC, EnPC);
+		const TLorentzVector beam  (PxPD, PyPD, PzPD, EnPD);
 		const TVector3 yAxis = (beam.Vect().Unit().Cross(-recoil.Vect().Unit())).Unit();  // normal of production plane in lab frame
 		const TVector3 eps(1, 0, 0);  // reference beam polarization vector at 0 degrees in lab frame
 		double Phi = polAngle * TMath::DegToRad() + atan2(yAxis.Dot(eps), beam.Vect().Unit().Dot(eps.Cross(yAxis)));  // angle in lab frame [rad]
@@ -72,6 +72,27 @@ class MyFSMath{
 		}
 		while (Phi < -TMath::Pi()) {
 			Phi += TMath::TwoPi();
+		}
+		if (false) {
+			// test code in https://github.com/lan13005/EtaPi-Analysis/blob/99c4c8045d75619bb2bfde6e800da72078723490/DSelector_etapi.C#L740
+			const TLorentzVector target(0, 0, 0, 0.938272);
+			const TLorentzRotation cmRestBoost(-(beam + target).BoostVector());
+			const TLorentzVector beam_cm = cmRestBoost * beam;
+			const TLorentzVector recoil_cm = cmRestBoost * recoil;
+			const TVector3 y = (beam_cm.Vect().Unit().Cross(-recoil_cm.Vect().Unit())).Unit();
+			const TVector3 eps2(TMath::Cos(polAngle * TMath::DegToRad()), TMath::Sin(polAngle * TMath::DegToRad()), 0);  // beam polarization vector
+			double Phi2 = TMath::ATan2(y.Dot(eps2), beam_cm.Vect().Unit().Dot(eps2.Cross(y)));
+			// ensure [-pi, +pi] range
+			while (Phi2 > TMath::Pi()) {
+				Phi2 -= TMath::TwoPi();
+			}
+			while (Phi2 < -TMath::Pi()) {
+				Phi2 += TMath::TwoPi();
+			}
+			const double deltaPhi = Phi2 - Phi;
+			if (std::abs(deltaPhi) > 1e-15) {
+				std::cout << "!!! Phi2 = " << Phi2 << " - Phi = " << Phi << ": " << deltaPhi << std::endl;
+			}
 		}
 		return Phi;
 	}
