30aae61b64deafd4ecd937d99d111c76efd8e1cd
bgrube <bgrube@jlab.org>
2024-02-09 17:18:50 -0500

    Calculate Phi from 4-vectors.

--- dataPhotoProdEtaPi0/makeMomentsInputTree.py
+++ dataPhotoProdEtaPi0/makeMomentsInputTree.py
@@ -53,6 +53,54 @@ if __name__ == "__main__":
   # ensure that quantities used to calculate moments are doubles
   # coordSys = "gj"
   coordSys = "hel"
+  ROOT.gInterpreter.Declare(
+"""
+// returns azimuthal angle of photon polarization vector in lab frame [rad]
+// for beam + target -> X + recoil and X -> a + b
+//     D                    C
+// code taken from https://github.com/JeffersonLab/halld_sim/blob/538677ee1347891ccefa5780e01b158e035b49b1/src/libraries/AMPTOOLS_AMPS/TwoPiAngles.cc#L94
+double
+bigPhi(
+	const double PxPC, const double PyPC, const double PzPC, const double EnPC,  // recoil
+	const double PxPD, const double PyPD, const double PzPD, const double EnPD   // beam
+) {
+	const double polAngle = 0;  // polarization angle [deg]
+	const TLorentzVector recoil(PxPC, PyPC, PzPC, EnPC);
+	const TLorentzVector beam  (PxPD, PyPD, PzPD, EnPD);
+	const TVector3 yAxis = (beam.Vect().Unit().Cross(-recoil.Vect().Unit())).Unit();  // normal of production plane in lab frame
+	const TVector3 eps(1, 0, 0);  // reference beam polarization vector at 0 degrees in lab frame
+	double Phi = polAngle * TMath::DegToRad() + atan2(yAxis.Dot(eps), beam.Vect().Unit().Dot(eps.Cross(yAxis)));  // angle in lab frame [rad]
+	// ensure [-pi, +pi] range
+	while (Phi > TMath::Pi()) {
+		Phi -= TMath::TwoPi();
+	}
+	while (Phi < -TMath::Pi()) {
+		Phi += TMath::TwoPi();
+	}
+	if (true) {
+		// test code in https://github.com/lan13005/EtaPi-Analysis/blob/99c4c8045d75619bb2bfde6e800da72078723490/DSelector_etapi.C#L740
+		const TLorentzVector target(0, 0, 0, 0.938272);
+		const TLorentzRotation cmRestBoost(-(beam + target).BoostVector());
+		const TLorentzVector beam_cm = cmRestBoost * beam;
+		const TLorentzVector recoil_cm = cmRestBoost * recoil;
+		const TVector3 y = (beam_cm.Vect().Unit().Cross(-recoil_cm.Vect().Unit())).Unit();
+		const TVector3 eps2(TMath::Cos(polAngle * TMath::DegToRad()), TMath::Sin(polAngle * TMath::DegToRad()), 0);  // beam polarization vector
+		double Phi2 = TMath::ATan2(y.Dot(eps2), beam_cm.Vect().Unit().Dot(eps2.Cross(y)));
+		// ensure [-pi, +pi] range
+		while (Phi2 > TMath::Pi()) {
+			Phi2 -= TMath::TwoPi();
+		}
+		while (Phi2 < -TMath::Pi()) {
+			Phi2 += TMath::TwoPi();
+		}
+		const double deltaPhi = Phi2 - Phi;
+		if (std::abs(deltaPhi) > 1e-15) {
+			std::cout << "!!! Phi2 = " << Phi2 << " - Phi = " << Phi << ": " << deltaPhi << std::endl;
+		}
+	}
+	return Phi;
+}
+""")
   data = (
     data.Define  ("beamPol",     f"(double){beamPol}")
         .Define  ("beamPolPhi",  f"(double){beamPolAngle}")
@@ -60,8 +108,10 @@ if __name__ == "__main__":
         .Define  ("theta",       f"(double)acos(cosTheta_eta_{coordSys})")
         .Alias   ("phiDeg",      f"phi_eta_{coordSys}")
         .Define  ("phi",         f"(double)phi_eta_{coordSys} * TMath::DegToRad()")
-        .Define  ("PhiDeg",      "Phi")  # cannot be an Alias because Redefine below would lead to infinite recursion
-        .Redefine("Phi",         "(double)Phi * TMath::DegToRad()")
+        # .Define  ("PhiDeg",      "Phi")  # cannot be an Alias because Redefine below would lead to infinite recursion
+        # .Redefine("Phi",         "(double)PhiDeg * TMath::DegToRad()")
+        .Redefine("Phi",         "bigPhi(Px_FinalState[0], Py_FinalState[0], Pz_FinalState[0], E_FinalState[0], Px_Beam, Py_Beam, Pz_Beam, E_Beam)")
+        .Define  ("PhiDeg",      "Phi * TMath::RadToDeg()")
         .Alias   ("mass",        "Mpi0eta")
         # .Alias   ("mass",        "Mpi0eta_thrown")
         .Define  ("eventWeight", f"(double){weightColumnName}")
