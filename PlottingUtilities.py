Add accessor for bin titles. Add kinematic bin to title of bootstrap plots. Fix x-axis titles for bootstrap plots as function of kinematic variable. Indicate average differences in plots. Improve naming and code layout.

PlottingUtilities.py
@@ -373,6 +373,7 @@ def plotMomentsBootstrapDistributions(
   HData:             MomentCalculator.MomentResult,  # moment values extracted from data
   HTrue:             Optional[MomentCalculator.MomentResult] = None,  # true moment values
   pdfFileNamePrefix: str = "h",  # name prefix for output files
+  histTitle:         str = "",   # histogram title
   nmbBins:           int = 100,  # number of bins for bootstrap histograms
 ) -> None:
   """Plots bootstrap distributions for H_0, H_1, and H_2 and overlays the true value and the estimate from uncertainty propagation"""
@@ -388,7 +389,7 @@ def plotMomentsBootstrapDistributions(
       max = np.max(momentSamplesBs)
       halfRange = (max - min) * 1.1 / 2.0
       center = (min + max) / 2.0
-      histBs = ROOT.TH1D(f"{pdfFileNamePrefix}bootstrap_{HVal.qn.label}_{momentPart}", f";{HVal.qn.title} {legendEntrySuffix};Count",
+      histBs = ROOT.TH1D(f"{pdfFileNamePrefix}bootstrap_{HVal.qn.label}_{momentPart}", f"{histTitle};{HVal.qn.title} {legendEntrySuffix};Count",
                          nmbBins, center - halfRange, center + halfRange)
       # fill histogram
       np.vectorize(histBs.Fill)(momentSamplesBs)
@@ -482,15 +483,27 @@ def plotMomentsBootstrapDiff(
     canv = ROOT.TCanvas()
     histBinning = (len(HVals), -0.5, len(HVals) - 0.5) if binning is None else binning.astuple
     histDummy = ROOT.TH1D("dummy", "", *histBinning)  # dummy histogram needed for x-axis bin labels
+    xAxis = histDummy.GetXaxis()
     if binning is None:
       for binIndex, HVal in enumerate(HVals):
-        histDummy.GetXaxis().SetBinLabel(binIndex + 1, HVal.qn.title)
+        xAxis.SetBinLabel(binIndex + 1, HVal.qn.title)
+    histDummy.SetTitle(graph.GetTitle())
+    histDummy.SetXTitle(graph.GetXaxis().GetTitle())
     histDummy.SetYTitle(graph.GetYaxis().GetTitle())
-    histDummy.SetMinimum(-1)
-    histDummy.SetMaximum(+1)
+    histDummy.SetMinimum(-0.5)
+    histDummy.SetMaximum(+0.5)
+    histDummy.SetLineColor(ROOT.kBlack)
+    histDummy.SetLineStyle(ROOT.kDashed)
     histDummy.Draw()
     graph.Draw("P")  # !NOTE! graphs don't have a SAME option; "SAME" will be interpreted as "A"
-    canv.BuildLegend(0.7, 0.75, 0.99, 0.99)
+    # draw average difference
+    for g in graph.GetListOfGraphs():
+      avg = g.GetMean(2)
+      avgLine = ROOT.TLine()
+      avgLine.SetLineColor(g.GetMarkerColor())
+      avgLine.SetLineStyle(ROOT.kDotted)
+      avgLine.DrawLine(xAxis.GetBinLowEdge(xAxis.GetFirst()), avg, xAxis.GetBinUpEdge(xAxis.GetLast()), avg)
+    # draw legend
     legend = ROOT.TLegend(0.7, 0.75, 0.99, 0.99)
     for g in graph.GetListOfGraphs():
       legend.AddEntry(g, g.GetTitle(), "P")
@@ -514,9 +527,11 @@ def plotMomentsBootstrapDiff1D(
 def plotMomentsBootstrapDiffInBin(
   HData:             MomentCalculator.MomentResult,  # moment values extracted from data
   pdfFileNamePrefix: str = "h",  # name prefix for output files
+  graphTitle:        str = "",   # graph title
 ) -> None:
   """Plots relative differences of estimates and their uncertainties for all moments"""
   for momentIndex in range(3):
     # get moments with index momentIndex
     HVals = tuple(MomentValueAndTruth(*HData[qnIndex], truth = None) for qnIndex in HData.indices.QnIndices() if qnIndex.momentIndex == momentIndex)  # type: ignore
-    plotMomentsBootstrapDiff(HVals, momentLabel = f"{MomentCalculator.QnMomentIndex.momentSymbol}{momentIndex}", pdfFileNamePrefix = pdfFileNamePrefix)
+    plotMomentsBootstrapDiff(HVals, momentLabel = f"{MomentCalculator.QnMomentIndex.momentSymbol}{momentIndex}",
+                             pdfFileNamePrefix = pdfFileNamePrefix, graphTitle = graphTitle)

testMomentsPhotoProdEtaPi0.py
@@ -80,8 +80,8 @@ if __name__ == "__main__":
     # maxL                 = 1  # define maximum L quantum number of moments
     maxL                 = 5  # define maximum L quantum number of moments
     normalizeMoments     = False
-    nmbBootstrapSamples  = 0
-    # nmbBootstrapSamples  = 10000
+    # nmbBootstrapSamples  = 0
+    nmbBootstrapSamples  = 10000
     nmbOpenMpThreads = ROOT.getNmbOpenMpThreads()
 
     # plot all signal and phase-space data
@@ -168,7 +168,7 @@ if __name__ == "__main__":
       print(f"Eigenvalues of acceptance integral matrix for first bin\n{np.sort(eigenVals)}")
       # plot acceptance integral matrices for all kinematic bins
       for momentsInBin in moments:
-        binLabel = "_".join(momentsInBin.fileNameBinLabels)
+        binLabel = "_".join(momentsInBin.binLabels)
         plotComplexMatrix(momentsInBin.integralMatrix.matrixNormalized, pdfFileNamePrefix = f"{outFileDirName}/I_acc_{binLabel}")
         plotComplexMatrix(momentsInBin.integralMatrix.inverse,          pdfFileNamePrefix = f"{outFileDirName}/I_inv_{binLabel}")
 
@@ -193,12 +193,15 @@ if __name__ == "__main__":
       # plot moments in each kinematic bin
       namePrefix = "norm" if normalizeMoments else "unnorm"
       for massBinIndex, momentsInBin in enumerate(moments):
-        binLabel = "_".join(momentsInBin.fileNameBinLabels)
+        binLabel = "_".join(momentsInBin.binLabels)
+        binTitle = ", ".join(momentsInBin.binTitles)
         plotMomentsInBin(momentsInBin.HPhys, normalizeMoments, momentsTruth[massBinIndex].HPhys,
                          pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_")
         if nmbBootstrapSamples > 0:
-          plotMomentsBootstrapDistributions(momentsInBin.HPhys, momentsTruth[massBinIndex].HPhys, pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_")
-          plotMomentsBootstrapDiffInBin    (momentsInBin.HPhys,                                   pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_")
+          graphTitle = f"({binLabel})"
+          plotMomentsBootstrapDistributions(momentsInBin.HPhys, momentsTruth[massBinIndex].HPhys, pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_",
+                                            histTitle = binTitle)
+          plotMomentsBootstrapDiffInBin(momentsInBin.HPhys, pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_", graphTitle = binTitle)
 
       # plot kinematic dependences of all moments
       for qnIndex in momentIndices.QnIndices():

testMomentsPhotoProdWeighted.py
@@ -227,7 +227,7 @@ if __name__ == "__main__":
       print(f"Eigenvalues of acceptance integral matrix\n{np.sort(eigenVals)}")
       # plot acceptance integral matrices for all kinematic bins
       for momentsInBin in moments:
-        binLabel = "_".join(momentsInBin.fileNameBinLabels)
+        binLabel = "_".join(momentsInBin.binLabels)
         plotComplexMatrix(moments[0].integralMatrix.matrixNormalized, pdfFileNamePrefix = f"{outFileDirName}/I_acc_{binLabel}")
         plotComplexMatrix(moments[0].integralMatrix.inverse,          pdfFileNamePrefix = f"{outFileDirName}/I_inv_{binLabel}")
 
@@ -239,7 +239,7 @@ if __name__ == "__main__":
       print(f"Physical moments of data generated according to partial-wave amplitudes\n{moments[0].HPhys}")
       # plot moments in each kinematic bin
       namePrefix = "norm" if normalizeMoments else "unnorm"
-      binLabel = "_".join(moments[0].fileNameBinLabels)
+      binLabel = "_".join(moments[0].binLabels)
       plotMomentsInBin(moments[0].HPhys, normalizeMoments, HTrue, pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_")
       if nmbBootstrapSamples > 0:
         plotMomentsBootstrapDistributions(moments[0].HPhys, HTrue, pdfFileNamePrefix = f"{outFileDirName}/{namePrefix}_{binLabel}_")