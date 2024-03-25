Fix uncertainty calculation by taking into account that event weights are also a random variable. Adjust plot range and number of threads.

MomentCalculator.py
@@ -744,12 +744,12 @@ class MomentCalculator:
       # all events have weight 1
       eventWeights = np.ones(nmbEvents, dtype = npt.Float64)
     assert eventWeights.shape == (nmbEvents,), f"NumPy arrays with event weights does not have the correct shape. Expected ({nmbEvents},) but got {eventWeights.shape}"
-    sumOfWeights        = np.sum(eventWeights)
-    sumOfSquaredWeights = np.sum(np.square(eventWeights))
+    # sumOfWeights        = np.sum(eventWeights)
+    # sumOfSquaredWeights = np.sum(np.square(eventWeights))
     # calculate basis-function values and values of measured moments
     nmbMoments = len(self.indices)
     fMeas:      npt.NDArray[npt.Shape["nmbMoments, nmbEvents"], npt.Complex128] = np.empty((nmbMoments, nmbEvents), dtype = npt.Complex128)
-    fMeasMeans: npt.NDArray[npt.Shape["nmbMoments"],            npt.Complex128] = np.empty((nmbMoments,),           dtype = npt.Complex128)  # weighted means of fMeas values
+    # fMeasMeans: npt.NDArray[npt.Shape["nmbMoments"],            npt.Complex128] = np.empty((nmbMoments,),           dtype = npt.Complex128)  # weighted means of fMeas values
     bootstrapIndices = BootstrapIndices(nmbEvents, nmbBootstrapSamples, bootstrapSeed)
     self._HMeas = MomentResult(self.indices, label = "meas", nmbBootstrapSamples = nmbBootstrapSamples, bootstrapSeed = bootstrapSeed)
     for flatIndex in self.indices.flatIndices():
@@ -757,7 +757,7 @@ class MomentCalculator:
       fMeas[flatIndex] = np.asarray(ROOT.f_meas(qnIndex.momentIndex, qnIndex.L, qnIndex.M, thetas, phis, Phis, dataSet.polarization))  # Eq. (176)
       weightedSum = eventWeights.dot(fMeas[flatIndex])
       self._HMeas._valsFlatIndex[flatIndex] = 2 * np.pi * weightedSum  # Eq. (179)
-      fMeasMeans[flatIndex] = weightedSum / sumOfWeights
+      # fMeasMeans[flatIndex] = weightedSum / sumOfWeights
       # perform bootstrapping of H_meas
       for bsSampleIndex, bsDataIndices in enumerate(bootstrapIndices):  # loop over same set of random data indices for each flatIndex
         # resample data
@@ -765,18 +765,20 @@ class MomentCalculator:
         eventWeightsBsSample = eventWeights    [bsDataIndices]
         # calculate bootstrap sample for H_meas
         self._HMeas._bsSamplesFlatIndex[flatIndex, bsSampleIndex] = 2 * np.pi * eventWeightsBsSample.dot(fMeasBsSample)
-    # calculate covariance matrices for measured moments; Eqs. (88), (180), and (181)
-    # unfortunately, np.cov() does not accept negative weights
-    # reimplement code from https://github.com/numpy/numpy/blob/d35cd07ea997f033b2d89d349734c61f5de54b0d/numpy/lib/function_base.py#L2530-L2749
-    delta_fMeas = fMeas - fMeasMeans[:, None]
-    delta_fMeas_aug = np.concatenate((delta_fMeas, np.conjugate(delta_fMeas)), axis = 0)  # augmented vector, i.e. delta_fMeas stacked on top of delta_fMeas^*
-    # see https://juliastats.org/StatsBase.jl/stable/weights/#Implementations and https://juliastats.org/StatsBase.jl/stable/cov/
-    # for a sample of ~1000 background-subtracted events the uncertainty estimates using the various Bessel corrections differ only in the 4th decimal place
-    besselCorrection = 1 / (sumOfWeights - 1)  # assuming frequency weights, i.e. the sum of weights is the number of background-subtracted events
-    # besselCorrection = 1 / (sumOfWeights - sumOfSquaredWeights / sumOfWeights)  # assuming analytic weights that describe importance of each measurement
-    # nmbNonZeroWeights = np.count_nonzero(eventWeights)
-    # besselCorrection = nmbNonZeroWeights / ((nmbNonZeroWeights - 1) * sumOfWeights)  # assuming probability weights that represent the inverse of the sampling probability for each observation
-    V_meas_aug = (2 * np.pi)**2 * sumOfSquaredWeights * besselCorrection * ((eventWeights * delta_fMeas_aug) @ np.asmatrix(delta_fMeas_aug).H)
+    #TODO update: calculate covariance matrices for measured moments; Eqs. (88), (180), and (181)
+    # # unfortunately, np.cov() does not accept negative weights
+    # # reimplement code from https://github.com/numpy/numpy/blob/d35cd07ea997f033b2d89d349734c61f5de54b0d/numpy/lib/function_base.py#L2530-L2749
+    # # delta_fMeas = fMeas - fMeasMeans[:, None]
+    # # delta_fMeas_aug = np.concatenate((delta_fMeas, np.conjugate(delta_fMeas)), axis = 0)  # augmented vector, i.e. delta_fMeas stacked on top of delta_fMeas^*
+    # # see https://juliastats.org/StatsBase.jl/stable/weights/#Implementations and https://juliastats.org/StatsBase.jl/stable/cov/
+    # # for a sample of ~1000 background-subtracted events the uncertainty estimates using the various Bessel corrections differ only in the 4th decimal place
+    # besselCorrection = 1 / (sumOfWeights - 1)  # assuming frequency weights, i.e. the sum of weights is the number of background-subtracted events
+    # # besselCorrection = 1 / (sumOfWeights - sumOfSquaredWeights / sumOfWeights)  # assuming analytic weights that describe importance of each measurement
+    # # nmbNonZeroWeights = np.count_nonzero(eventWeights)
+    # # besselCorrection = nmbNonZeroWeights / ((nmbNonZeroWeights - 1) * sumOfWeights)  # assuming probability weights that represent the inverse of the sampling probability for each observation
+    # V_meas_aug = (2 * np.pi)**2 * sumOfSquaredWeights * besselCorrection * ((eventWeights * delta_fMeas_aug) @ np.asmatrix(delta_fMeas_aug).H)
+    fMeasWeighted = eventWeights * fMeas
+    V_meas_aug = (2 * np.pi)**2 * nmbEvents * np.cov(fMeasWeighted, np.conjugate(fMeasWeighted), ddof = 1)
     self._HMeas._covReReFlatIndex, self._HMeas._covImImFlatIndex, self._HMeas._covReImFlatIndex = self._calcReImCovMatrices(V_meas_aug)
     # calculate physical moments and propagate uncertainty
     self._HPhys = MomentResult(self.indices, label = "phys", nmbBootstrapSamples = nmbBootstrapSamples, bootstrapSeed = bootstrapSeed)