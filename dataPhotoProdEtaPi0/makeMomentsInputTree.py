c4294a4d7ee1ba9c2ac4354c2a5ab321a9c7ba4e
bgrube <bgrube@jlab.org>
2024-04-04 18:21:28 -0400

    Loop over data sets. Add data sets for MC study with constant amplitudes. Declare C++ function only once.

--- dataPhotoProdEtaPi0/makeMomentsInputTree.py
+++ dataPhotoProdEtaPi0/makeMomentsInputTree.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-
 import functools
 import os
-from typing import Dict
+from typing import (
+  Dict,
+  List,
+)
 
 import ROOT
 
@@ -12,14 +14,8 @@ import ROOT
 print = functools.partial(print, flush = True)
 
 
-def defineDataFrameColumns(
-  data:             ROOT.RDataFrame,
-  coordSys:         str  = "hel",  # either "hel" for helicity frame or "gj" for Gottfried-Jackson frame
-  weightColumnName: str  = "Weight",
-  thrownData:       bool = False,
-) -> ROOT.RDataFrame:
-  """Returns RDataFrame with additional columns for moments analysis"""
-  ROOT.gInterpreter.Declare(
+# delcare C++ function to calculate azimuthal angle of photon polarization vector
+ROOT.gInterpreter.Declare(
 """
 // returns azimuthal angle of photon polarization vector in lab frame [rad]
 // for beam + target -> X + recoil and X -> a + b
@@ -67,6 +63,15 @@ bigPhi(
 	return Phi;
 }
 """)
+
+
+def defineDataFrameColumns(
+  data:             ROOT.RDataFrame,
+  coordSys:         str  = "hel",  # either "hel" for helicity frame or "gj" for Gottfried-Jackson frame
+  weightColumnName: str  = "Weight",
+  thrownData:       bool = False,
+) -> ROOT.RDataFrame:
+  """Returns RDataFrame with additional columns for moments analysis"""
   columnSuffix = "_thrown" if thrownData else ""
   df = (
     # ensure that quantities used to calculate moments are doubles
@@ -97,100 +102,115 @@ bigPhi(
 if __name__ == "__main__":
   ROOT.gROOT.SetBatch(True)
 
-  dataSet = "signal_acc"
-  inputFileNamePattern = "a0a2_raw/a0a2_2bw_acc_flat.root"
-  # dataSet = "signal_gen"
-  # inputFileNamePattern = "a0a2_raw/a0a2_2bw_gen_data_flat.root"
-  # dataSet = "phaseSpace_acc"
-  # inputFileNamePattern = "a0a2_raw/a0a2_flat_acc_flat.root"
-  # dataSet = "phaseSpace_gen"
-  # inputFileNamePattern = "a0a2_raw/a0a2_flat_gen_data_flat.root"
-  outputFileName = f"./a0a2_{dataSet}_flat.root"
-  inputTreeName = "kin"
-  outputTreeName = "etaPi0"
-  weightColumnName = "Weight"
-  beamPol = 1.0
-  beamPolAngle = 0
-  thrownData = ((dataSet == "signal_gen") or (dataSet == "phaseSpace_gen"))
-
-  # apply fiducial cuts
-  data = ROOT.RDataFrame(inputTreeName, inputFileNamePattern)
-  if not thrownData:
-    data = data.Filter(
-             "("
-               "(pVH > 0.5)"
-               "&& (unusedEnergy < 0.01)"  # [GeV]
-               "&& (chiSq < 13.277)"
-               "&& (((2.5 < photonTheta1) && (photonTheta1 < 10.3)) || (photonTheta1 > 11.9))"  # [deg]
-               "&& (((2.5 < photonTheta2) && (photonTheta2 < 10.3)) || (photonTheta2 > 11.9))"  # [deg]
-               "&& (((2.5 < photonTheta3) && (photonTheta3 < 10.3)) || (photonTheta3 > 11.9))"  # [deg]
-               "&& (((2.5 < photonTheta4) && (photonTheta4 < 10.3)) || (photonTheta4 > 11.9))"  # [deg]
-               "&& (photonE1 > 0.1)"  # [GeV]
-               "&& (photonE2 > 0.1)"  # [GeV]
-               "&& (photonE3 > 0.1)"  # [GeV]
-               "&& (photonE4 > 0.1)"  # [GeV]
-               "&& (proton_momentum > 0.3)"  # [GeV]
-               "&& ((52 < proton_z) && (proton_z < 78))"  # [cm]
-               "&& (abs(mmsq) < 0.05)"  # [GeV^2]
-               "&& ((0.8 < Mpi0eta_thrown) && (Mpi0eta_thrown < 2.0))"  # [GeV]
-             ")"
-           )
-
-  data = defineDataFrameColumns(data, coordSys = "hel",  weightColumnName = weightColumnName, thrownData = thrownData)
-
-  # define background-subtracted histograms
-  histDefs = []
-  if not thrownData:
-    histDefs += [
-      # cut variables
-      {"columnName" : "pVH",             "xAxisUnit" : "",        "yAxisTitle" : "Combos",                 "binning" : (100, -0.5, 1.5)},
-      {"columnName" : "unusedEnergy",    "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 1 MeV",         "binning" : (100, 0, 0.1)},
-      {"columnName" : "chiSq",           "xAxisUnit" : "",        "yAxisTitle" : "Combos",                 "binning" : (100, 0, 15)},
-      {"columnName" : "photonTheta1",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
-      {"columnName" : "photonTheta2",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
-      {"columnName" : "photonTheta3",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
-      {"columnName" : "photonTheta4",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
-      {"columnName" : "photonE1",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
-      {"columnName" : "photonE2",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
-      {"columnName" : "photonE3",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
-      {"columnName" : "photonE4",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
-      {"columnName" : "proton_momentum", "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 2 MeV",         "binning" : (100, 0.2, 1.2)},
-      {"columnName" : "proton_z",        "xAxisUnit" : "cm",      "yAxisTitle" : "Combos / 0.4 cm",        "binning" : (100, 40, 80)},
-      {"columnName" : "mmsq",            "xAxisUnit" : "GeV^{2}", "yAxisTitle" : "Combos / 0.002 GeV^{2}", "binning" : (100, -0.1, 0.1)},
-      # other kinematic variables
-      {"columnName" : "Mpi0",           "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 2 MeV",  "binning" : (100, 0, 0.2)},
-      {"columnName" : "Meta",           "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 3 MeV",  "binning" : (100, 0.4, 0.7)},
-      {"columnName" : "Mpi0eta_thrown", "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 10 MeV", "binning" : (100, 0, 2.5)},
-      {"columnName" : "rfTime",         "xAxisUnit" : "ns",  "yAxisTitle" : "Combos / 0.5 ns", "binning" : (100, -25, 25)},
-    ]
-  histDefs += [
-    # moment variables
-    {"columnName" : "beamPol",    "xAxisUnit" : "",    "yAxisTitle" : "Combos",            "binning" : (110, 0, 1.1)},
-    {"columnName" : "beamPolPhi", "xAxisUnit" : "deg", "yAxisTitle" : "Combos / 1 deg",    "binning" : (360, -180, 180)},
-    {"columnName" : "cosTheta",   "xAxisUnit" : "",    "yAxisTitle" : "Combos",            "binning" : (100, -1, 1)},
-    {"columnName" : "theta",      "xAxisUnit" : "rad", "yAxisTitle" : "Combos / 0.04 rad", "binning" : (100, 0, 4)},
-    {"columnName" : "phiDeg",     "xAxisUnit" : "deg", "yAxisTitle" : "Combos / 1 deg",    "binning" : (360, -180, 180)},
-    {"columnName" : "phi",        "xAxisUnit" : "rad", "yAxisTitle" : "Combos / 0.08 rad", "binning" : (100, -4, 4)},
-    {"columnName" : "PhiDeg",     "xAxisUnit" : "deg", "yAxisTitle" : "Combos / 1 deg",    "binning" : (360, -180, 180)},
-    {"columnName" : "Phi",        "xAxisUnit" : "rad", "yAxisTitle" : "Combos / 0.08 rad", "binning" : (100, -4, 4)},
-    {"columnName" : "mass",       "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 10 MeV",   "binning" : (100, 0, 2.5)},
+  dataSets: List[Dict[str, str]] = [
+    # {"dataSetLabel"         : "signal_acc",
+    #  "inputFileNamePattern" : "a0a2_raw/a0a2_2bw_acc_flat.root"},
+    # {"dataSetLabel"         : "signal_gen",
+    #  "inputFileNamePattern" : "a0a2_raw/a0a2_2bw_gen_data_flat.root"},
+    # {"dataSetLabel"         : "phaseSpace_acc",
+    #  "inputFileNamePattern" : "a0a2_raw/a0a2_flat_acc_flat.root"},
+    # {"dataSetLabel"         : "phaseSpace_gen",
+    #  "inputFileNamePattern" : "a0a2_raw/a0a2_flat_gen_data_flat.root"},
+    #
+    {"dataSetLabel"         : "signal_acc",
+     "inputFileNamePattern" : "SELECTED_a0a2_forBoris/a0a2_bin_10_acc_flat.root"},
+    {"dataSetLabel"         : "signal_gen",
+     "inputFileNamePattern" : "SELECTED_a0a2_forBoris/a0a2_bin10_gen_data_flat.root"},
+    {"dataSetLabel"         : "phaseSpace_acc",
+     "inputFileNamePattern" : "SELECTED_a0a2_forBoris/flat_acc_flat.root"},
+    {"dataSetLabel"         : "phaseSpace_gen",
+     "inputFileNamePattern" : "SELECTED_a0a2_forBoris/flat_gen_data_flat.root"},
   ]
-  hists = []
-  for histDef in histDefs:
-    cName = histDef["columnName"]
-    unit  = histDef["xAxisUnit"]
-    hists.append(data.Histo1D((f"h_{cName}", f";{cName}" + (f" [{unit}]" if unit else "") + f";{histDef['yAxisTitle']}",
-                               *histDef["binning"]), (cName,), weightColumnName))
+  inputTreeName    = "kin"
+  outputTreeName   = "etaPi0"
+  weightColumnName = "Weight"
+  beamPol          = 1.0
+  beamPolAngle     = 0
 
-  # write root tree for moments analysis
-  print(f"Writing skimmed tree to file '{outputFileName}'")
-  data.Snapshot(outputTreeName, outputFileName,
-                ("beamPol", "beamPolPhi", "cosTheta", "theta", "phiDeg", "phi", "PhiDeg", "Phi", "mass", "eventWeight"))
+  for dataSet in dataSets:
+    dataSetLabel         = dataSet["dataSetLabel"]
+    inputFileNamePattern = dataSet["inputFileNamePattern"]
+    outputFileName       = f"./a0a2_{dataSetLabel}_flat.root"
+    thrownData           = ((dataSetLabel == "signal_gen") or (dataSetLabel == "phaseSpace_gen"))
 
-  # fill and draw histograms
-  ROOT.gStyle.SetOptStat(111111)
-  for hist in hists:
-    canv = ROOT.TCanvas(f"{hist.GetName()}.{dataSet}")
-    hist.SetMinimum(0)
-    hist.Draw("HIST")
-    canv.SaveAs(".pdf")
+    # apply fiducial cuts
+    data = ROOT.RDataFrame(inputTreeName, inputFileNamePattern)
+    if not thrownData:
+      data = data.Filter(
+              "("
+                "(pVH > 0.5)"
+                "&& (unusedEnergy < 0.01)"  # [GeV]
+                "&& (chiSq < 13.277)"
+                "&& (((2.5 < photonTheta1) && (photonTheta1 < 10.3)) || (photonTheta1 > 11.9))"  # [deg]
+                "&& (((2.5 < photonTheta2) && (photonTheta2 < 10.3)) || (photonTheta2 > 11.9))"  # [deg]
+                "&& (((2.5 < photonTheta3) && (photonTheta3 < 10.3)) || (photonTheta3 > 11.9))"  # [deg]
+                "&& (((2.5 < photonTheta4) && (photonTheta4 < 10.3)) || (photonTheta4 > 11.9))"  # [deg]
+                "&& (photonE1 > 0.1)"  # [GeV]
+                "&& (photonE2 > 0.1)"  # [GeV]
+                "&& (photonE3 > 0.1)"  # [GeV]
+                "&& (photonE4 > 0.1)"  # [GeV]
+                "&& (proton_momentum > 0.3)"  # [GeV]
+                "&& ((52 < proton_z) && (proton_z < 78))"  # [cm]
+                "&& (abs(mmsq) < 0.05)"  # [GeV^2]
+                "&& ((0.8 < Mpi0eta_thrown) && (Mpi0eta_thrown < 2.0))"  # [GeV]
+              ")"
+            )
+
+    data = defineDataFrameColumns(data, coordSys = "hel",  weightColumnName = weightColumnName, thrownData = thrownData)
+
+    # define background-subtracted histograms
+    histDefs = []
+    if not thrownData:
+      histDefs += [
+        # cut variables
+        {"columnName" : "pVH",             "xAxisUnit" : "",        "yAxisTitle" : "Combos",                 "binning" : (100, -0.5, 1.5)},
+        {"columnName" : "unusedEnergy",    "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 1 MeV",         "binning" : (100, 0, 0.1)},
+        {"columnName" : "chiSq",           "xAxisUnit" : "",        "yAxisTitle" : "Combos",                 "binning" : (100, 0, 15)},
+        {"columnName" : "photonTheta1",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
+        {"columnName" : "photonTheta2",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
+        {"columnName" : "photonTheta3",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
+        {"columnName" : "photonTheta4",    "xAxisUnit" : "deg",     "yAxisTitle" : "Combos / 0.1 deg",       "binning" : (200, 0, 20)},
+        {"columnName" : "photonE1",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
+        {"columnName" : "photonE2",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
+        {"columnName" : "photonE3",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
+        {"columnName" : "photonE4",        "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 0.1 GeV",       "binning" : (90, 0, 9)},
+        {"columnName" : "proton_momentum", "xAxisUnit" : "GeV",     "yAxisTitle" : "Combos / 2 MeV",         "binning" : (100, 0.2, 1.2)},
+        {"columnName" : "proton_z",        "xAxisUnit" : "cm",      "yAxisTitle" : "Combos / 0.4 cm",        "binning" : (100, 40, 80)},
+        {"columnName" : "mmsq",            "xAxisUnit" : "GeV^{2}", "yAxisTitle" : "Combos / 0.002 GeV^{2}", "binning" : (100, -0.1, 0.1)},
+        # other kinematic variables
+        {"columnName" : "Mpi0",           "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 2 MeV",  "binning" : (100, 0, 0.2)},
+        {"columnName" : "Meta",           "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 3 MeV",  "binning" : (100, 0.4, 0.7)},
+        {"columnName" : "Mpi0eta_thrown", "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 10 MeV", "binning" : (100, 0, 2.5)},
+        {"columnName" : "rfTime",         "xAxisUnit" : "ns",  "yAxisTitle" : "Combos / 0.5 ns", "binning" : (100, -25, 25)},
+      ]
+    histDefs += [
+      # moment variables
+      {"columnName" : "beamPol",    "xAxisUnit" : "",    "yAxisTitle" : "Combos",            "binning" : (110, 0, 1.1)},
+      {"columnName" : "beamPolPhi", "xAxisUnit" : "deg", "yAxisTitle" : "Combos / 1 deg",    "binning" : (360, -180, 180)},
+      {"columnName" : "cosTheta",   "xAxisUnit" : "",    "yAxisTitle" : "Combos",            "binning" : (100, -1, 1)},
+      {"columnName" : "theta",      "xAxisUnit" : "rad", "yAxisTitle" : "Combos / 0.04 rad", "binning" : (100, 0, 4)},
+      {"columnName" : "phiDeg",     "xAxisUnit" : "deg", "yAxisTitle" : "Combos / 1 deg",    "binning" : (360, -180, 180)},
+      {"columnName" : "phi",        "xAxisUnit" : "rad", "yAxisTitle" : "Combos / 0.08 rad", "binning" : (100, -4, 4)},
+      {"columnName" : "PhiDeg",     "xAxisUnit" : "deg", "yAxisTitle" : "Combos / 1 deg",    "binning" : (360, -180, 180)},
+      {"columnName" : "Phi",        "xAxisUnit" : "rad", "yAxisTitle" : "Combos / 0.08 rad", "binning" : (100, -4, 4)},
+      {"columnName" : "mass",       "xAxisUnit" : "GeV", "yAxisTitle" : "Combos / 10 MeV",   "binning" : (100, 0, 2.5)},
+    ]
+    hists = []
+    for histDef in histDefs:
+      cName = histDef["columnName"]
+      unit  = histDef["xAxisUnit"]
+      hists.append(data.Histo1D((f"h_{cName}", f";{cName}" + (f" [{unit}]" if unit else "") + f";{histDef['yAxisTitle']}",
+                                *histDef["binning"]), (cName,), weightColumnName))
+
+    # write root tree for moments analysis
+    print(f"Writing skimmed tree to file '{outputFileName}'")
+    data.Snapshot(outputTreeName, outputFileName,
+                  ("beamPol", "beamPolPhi", "cosTheta", "theta", "phiDeg", "phi", "PhiDeg", "Phi", "mass", "eventWeight"))
+
+    # fill and draw histograms
+    ROOT.gStyle.SetOptStat(111111)
+    for hist in hists:
+      canv = ROOT.TCanvas(f"{hist.GetName()}.{dataSetLabel}")
+      hist.SetMinimum(0)
+      hist.Draw("HIST")
+      canv.SaveAs(".pdf")
