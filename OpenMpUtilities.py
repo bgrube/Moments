6f0d77924f038636cce57fd032db3cf62dbc57f2
bgrube <bgrube@jlab.org>
2024-07-01 18:19:46 -0400

    Adapt ACLiC settings to ifarm Alma 9 environment.

--- OpenMpUtilities.py
+++ OpenMpUtilities.py
@@ -1,6 +1,8 @@
 """Module that provides helper function to use OpenMp in ACLiC compiled code"""
 
 import functools
+import socket
+import subprocess
 
 import ROOT
 
@@ -41,25 +43,15 @@ def enableRootACLiCOpenMp() -> None:
     print(f"Enabling ACLiC compilation with OpenMP for Linux")
     ROOT.gSystem.SetFlagsOpt("-fopenmp")  # compiler flags for optimized mode
     ROOT.gSystem.AddLinkedLibs("-lgomp")
-    # For usual setups, the above should be sufficient.
-    # On ifarm, however, ROOT seems to be compiled with a weird gcc setup.
-    # ACLiC does not search the gcc include directory at `/usr/lib/gcc/x86_64-redhat-linux/4.8.5/include`.`
-    # Instead, it seems to go to /usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../include/c++/4.8.5/,
-    # which is /usr/include/c++/4.8.5/ but does not contain omp.h.
-    # There seems to be no way to fix this without changing the source code.
-    # ACLiC always complaints that it cannot find `omp.h`.`
-    # # get gcc include directory
-    # result = subprocess.run("gcc -print-search-dirs",
-    #                         shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
-    # lines= result.stdout.split("\n")
-    # gccIncludePath = None
-    # for line in lines:
-    #   if "install:" in line:
-    #     gccIncludePath = line.split(": ")[1] + "include"
-    # ROOT.gSystem.AddIncludePath(f"-I \"{gccIncludePath}\"")  # this causes compile errors because it clashes with headers in `/usr/include/c++/4.8.5/`, which weirdly does not contain `omp.h`
-    # ROOT.gSystem.AddIncludePath(f"-idirafter \"{gccIncludePath}\"")  # this has no effect # type: ignore
-    # ROOT.gSystem.AddIncludePath(f"-isystem \"{gccIncludePath}\"")  # this has no effect # type: ignore
-    #
-    # The only way to make it work is to copy/link `omp.h` from the
-    # gcc include dir here and turn the system include statement in
-    # `wigner.C` into the quoted form.
+    if socket.gethostname().startswith("ifarm"):
+      # For usual setups, the above should be sufficient.
+      # On ifarm, however, ACLiC does not search the gcc include directory at `/usr/lib/gcc/x86_64-redhat-linux/11/include/` and hence does not find `omp.h`.
+      # Get the include directory from gcc and add it to the include path.
+      result = subprocess.run("gcc -print-search-dirs",
+                              shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
+      lines= result.stdout.splitlines()
+      gccIncludePath = None
+      for line in lines:
+        if "install:" in line:
+          gccIncludePath = line.split(": ")[1] + "include"
+      ROOT.gSystem.AddIncludePath(f"-I \"{gccIncludePath}\"")
