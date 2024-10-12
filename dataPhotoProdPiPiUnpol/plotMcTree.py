0eb1f947a6443ee16feb024f08e62cf4d629523b
bgrube <bgrube@jlab.org>
2024-10-11 20:48:23 -0400

    Add mass plots for particle pairs. Add y-axis labels.

--- dataPhotoProdPiPiUnpol/plotMcTree.py
+++ dataPhotoProdPiPiUnpol/plotMcTree.py
@@ -8,7 +8,7 @@ import ROOT
 
 if __name__ == "__main__":
   ROOT.gROOT.SetBatch(True)
-  ROOT.gStyle.SetOptStat("")
+  ROOT.gStyle.SetOptStat("i")
   ROOT.gROOT.ProcessLine(f".x {os.environ['FSROOT']}/rootlogon.FSROOT.C")
   # declare C++ function to calculate invariant mass of a particle
   CPP_CODE = """
@@ -17,6 +17,19 @@ if __name__ == "__main__":
 	{
 		const TLorentzVector p(Px, Py, Pz, E);
 		return p.M();
+	}
+  """
+  ROOT.gInterpreter.Declare(CPP_CODE)
+  # declare C++ function to calculate invariant mass of a pair of particles
+  CPP_CODE = """
+	double
+	massPair(
+		const double Px1, const double Py1, const double Pz1, const double E1,
+		const double Px2, const double Py2, const double Pz2, const double E2
+	)	{
+		const TLorentzVector p1(Px1, Py1, Pz1, E1);
+		const TLorentzVector p2(Px2, Py2, Pz2, E2);
+		return (p1 + p2).M();
 	}
   """
   ROOT.gInterpreter.Declare(CPP_CODE)
@@ -34,6 +47,9 @@ if __name__ == "__main__":
            .Define("FsMassRecoil", f"mass({lvRecoil})") \
            .Define("FsMassPip",    f"mass({lvPip})") \
            .Define("FsMassPim",    f"mass({lvPim})") \
+           .Define("MassPiPi",     f"massPair({lvPip}, {lvPim})") \
+           .Define("MassPipP",     f"massPair({lvPip}, {lvRecoil})") \
+           .Define("MassPimP",     f"massPair({lvPim}, {lvRecoil})") \
            .Define("GjCosTheta",   f"FSMath::gjcostheta({lvPip}, {lvPim}, {lvBeam})") \
            .Define("GjTheta",      "std::acos(GjCosTheta)") \
            .Define("GjPhi",        f"FSMath::gjphi({lvPip}, {lvPim}, {lvRecoil}, {lvBeam})") \
@@ -42,10 +58,17 @@ if __name__ == "__main__":
            .Define("HfTheta",      "std::acos(HfCosTheta)") \
            .Define("HfPhi",        f"FSMath::helphi({lvPim}, {lvPip}, {lvRecoil}, {lvBeam})") \
            .Define("HfPhiDeg",     "HfPhi * TMath::RadToDeg()")
+  yAxisLabel = "Events"
   hists = (
-    df.Histo1D(ROOT.RDF.TH1DModel("hMcFsMassRecoil", ";m_{Recoil} [GeV]",       100, 0, 2), "FsMassRecoil"),
-    df.Histo1D(ROOT.RDF.TH1DModel("hMcFsMassPip",    ";m_{#pi^{#plus}} [GeV]",  100, 0, 2), "FsMassPip"),
-    df.Histo1D(ROOT.RDF.TH1DModel("hMcFsMassPim",    ";m_{#pi^{#minus}} [GeV]", 100, 0, 2), "FsMassPim"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcFsMassRecoil", ";m_{Recoil} [GeV]"         + yAxisLabel, 100, 0,      2),      "FsMassRecoil"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcFsMassPip",    ";m_{#pi^{#plus}} [GeV]"    + yAxisLabel, 100, 0,      2),      "FsMassPip"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcFsMassPim",    ";m_{#pi^{#minus}} [GeV]"   + yAxisLabel, 100, 0,      2),      "FsMassPim"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcMassPiPi",     ";m_{#pi#pi} [GeV];"        + yAxisLabel, 400, 0.28,   2.28),   "MassPiPi"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcMassPiPiClas", ";m_{#pi#pi} [GeV];"        + yAxisLabel, 200, 0,      2),      "MassPiPi"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcMassPipP",     ";m_{p#pi^{#plus}} [GeV];"  + yAxisLabel, 400, 1,      5),      "MassPipP"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcMassPipPClas", ";m_{p#pi^{#plus}} [GeV];"  + yAxisLabel,  72, 1,      2.8),    "MassPipP"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcMassPimP",     ";m_{p#pi^{#minus}} [GeV];" + yAxisLabel, 400, 1,      5),      "MassPimP"),
+    df.Histo1D(ROOT.RDF.TH1DModel("hMcMassPimPClas", ";m_{p#pi^{#minus}} [GeV];" + yAxisLabel,  72, 1,      2.8),    "MassPimP"),
     df.Histo2D(ROOT.RDF.TH2DModel("hMcAnglesGj",     ";cos#theta_{GJ};#phi_{GJ} [deg]", 50, -1, +1, 50, -180, +180), "GjCosTheta", "GjPhiDeg"),
     df.Histo2D(ROOT.RDF.TH2DModel("hMcAnglesHf",     ";cos#theta_{HF};#phi_{HF} [deg]", 50, -1, +1, 50, -180, +180), "HfCosTheta", "HfPhiDeg"),
   )
