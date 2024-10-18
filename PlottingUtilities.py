78e482b1f399b47debef52cb85614e60cf7ccc67
bgrube <bgrube@jlab.org>
2024-10-18 15:48:10 -0400

    Copy over functions to set color-blind friendly style from https://github.com/bgrube/ReactionEfficiency.

--- PlottingUtilities.py
+++ PlottingUtilities.py
@@ -41,6 +41,85 @@ mpl.rcParams["mathtext.fontset"] = "stix"
 print = functools.partial(print, flush = True)
 
 
+# black + 7 colorblind-friendly colors rom M. Okabe and K. Ito, "How to make figures and presentations that are friendly to color blind people," University of Tokyo, 2002.
+# see also Bang Wong, https://www.nature.com/articles/nmeth.1618.pdf
+#     https://davidmathlogic.com/colorblind
+#     https://yoshke.org/blog/colorblind-friendly-diagrams
+COLORS_CB_FRIENDLY: tuple[str, ...] = (
+  "#000000",  # black
+  "#0072B2",  # blue
+  "#D55E00",  # vermillion
+  "#009E73",  # bluish green
+  "#CC79A7",  # reddish purple
+  "#56B4E9",  # sky blue
+  "#E69F00",  # orange
+  "#F0E442",  # yellow
+)
+
+
+#TODO move into separate module
+def getRootColor(hexColor: str) -> int:
+  """Returns ROOT color index for given hex string in form #RRGGBB; if color does not exist yet in ROOT it is created"""
+  ROOT.TColor.SetColorThreshold(0)  # ensure GetColor() returns exact color
+  return ROOT.TColor.GetColor(hexColor)
+
+def getCbFriendlyRootColor(
+  index:     int,
+  skipBlack: bool = False,  # if set black color is not used
+) -> int:
+  """Returns ROOT color index for given index in colorblind-friendly palette"""
+  return getRootColor(COLORS_CB_FRIENDLY[index + (1 if skipBlack else 0)])
+
+# 11 filled marker styles
+# the float is a relative scaling factor to obtain approximately equal apparent sizes
+MARKERS_FILLED: tuple[tuple[int, float], ...] = (
+  (ROOT.kFullCircle,            0.75),
+  (ROOT.kFullSquare,            0.70),
+  (ROOT.kFullDiamond,           1.00),
+  (ROOT.kFullCross,             0.85),
+  (ROOT.kFullCrossX,            0.85),
+  (ROOT.kFullStar,              1.00),
+  (ROOT.kFullFourTrianglesX,    0.90),
+  (ROOT.kFullFourTrianglesPlus, 0.85),
+  (ROOT.kFullTriangleUp,        0.85),
+  (ROOT.kFullTriangleDown,      0.85),
+  (ROOT.kFullDoubleDiamond,     1.10),
+)
+# 11 open marker styles
+MARKERS_OPEN: tuple[tuple[int, float], ...] = (
+  (ROOT.kOpenCircle,            0.75),
+  (ROOT.kOpenSquare,            0.70),
+  (ROOT.kOpenDiamond,           1.00),
+  (ROOT.kOpenCross,             0.85),
+  (ROOT.kOpenCrossX,            0.85),
+  (ROOT.kOpenStar,              1.00),
+  (ROOT.kOpenFourTrianglesX,    0.90),
+  (ROOT.kOpenFourTrianglesPlus, 0.85),
+  (ROOT.kOpenTriangleUp,        0.85),
+  (ROOT.kOpenTriangleDown,      0.85),
+  (ROOT.kOpenDoubleDiamond,     1.10),
+)
+
+#TODO take TObject and check wether it is TAttLine etc.
+#TODO add cycle option; provide more styles by combining colors and markers
+def setCbFriendlyStyle(
+  graphOrHist:   ROOT.TGraph | ROOT.TH1,
+  styleIndex:    int,  # index that switches between styles
+  skipBlack:     bool  = True,  # if set black color is not used
+  setMarker:     bool  = True,
+  markerSize:    float = 1.5,
+  filledMarkers: bool  = True,
+) -> None:
+  """Sets line color and marker style, color, and size of a TGraph or TH1 according to a style index"""
+  nmbStyles = min(len(COLORS_CB_FRIENDLY) - (1 if skipBlack else 0), len(MARKERS_FILLED), len(MARKERS_OPEN))
+  assert styleIndex < nmbStyles, f"The style index {styleIndex} goes beyond the maximum of {nmbStyles} styles that are implemented"
+  color = getCbFriendlyRootColor(styleIndex, skipBlack)
+  graphOrHist.SetLineColor(color)
+  if setMarker:
+    graphOrHist.SetMarkerColor(color)
+    graphOrHist.SetMarkerStyle(MARKERS_FILLED[styleIndex][0] if filledMarkers else MARKERS_OPEN[styleIndex][0])
+    graphOrHist.SetMarkerSize(markerSize * MARKERS_FILLED[styleIndex][1] if filledMarkers else MARKERS_OPEN[styleIndex][1])
+
 @dataclass
 class MomentValueAndTruth(MomentValue):
   """Stores and provides access to single moment value and provides truth value"""
