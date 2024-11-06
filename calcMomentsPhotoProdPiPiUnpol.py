bf12d6ec3701ae6bbe2846d8546cb46ca6e20dab
bgrube <bgrube@jlab.org>
2024-11-06 13:39:12 -0500

    Add function to calculate moments from PWA fit result. Use same mass binning as in PWA fit.

--- calcMomentsPhotoProdPiPiUnpol.py
+++ calcMomentsPhotoProdPiPiUnpol.py
@@ -16,17 +16,23 @@ from dataclasses import (
 )
 import functools
 import numpy as np
+import pandas as pd
 import threadpoolctl
 
 import ROOT
 from wurlitzer import pipes, STDOUT
 
 from MomentCalculator import (
+  AmplitudeSet,
+  AmplitudeValue,
   DataSet,
   KinematicBinningVariable,
   MomentCalculator,
   MomentCalculatorsKinematicBinning,
   MomentIndices,
+  MomentResult,
+  MomentResultsKinematicBinning,
+  QnWaveIndex,
 )
 from PlottingUtilities import (
   HistAxisBinning,
@@ -64,8 +70,9 @@ class AnalysisConfig:
   limitNmbPsAccEvents:      int                      = 0
   # limitNmbPsAccEvents:      int                      = 100000
   binVarMass:               KinematicBinningVariable = KinematicBinningVariable(name = "mass", label = "#it{m}_{#it{#pi}^{#plus}#it{#pi}^{#minus}}", unit = "GeV/#it{c}^{2}", nmbDigits = 3)
-  massBinning:              HistAxisBinning          = HistAxisBinning(nmbBins = 100, minVal = 0.4, maxVal = 1.4, _var = binVarMass)  # same binning as used by CLAS
+  # massBinning:              HistAxisBinning          = HistAxisBinning(nmbBins = 100, minVal = 0.4, maxVal = 1.4, _var = binVarMass)  # same binning as used by CLAS
   # massBinning:              HistAxisBinning          = HistAxisBinning(nmbBins = 1, minVal = 1.25, maxVal = 1.29, _var = binVarMass)  # f_2(1270) region
+  massBinning:              HistAxisBinning          = HistAxisBinning(nmbBins = 56, minVal = 0.28, maxVal = 1.40, _var = binVarMass)  # binning used in PWA
 
   def init(self) -> None:
     """Creates output directory and initializes member variables"""
@@ -86,6 +93,66 @@ class AnalysisConfig:
     self.init()
 
 CFG = AnalysisConfig()
+
+
+
+def readMomentResultsPwa(
+  dataFileName:          str,
+  binVarMass:            KinematicBinningVariable,  # binning variable for mass bins
+  momentResultsFileName: str | None = None,  # if not `None`, moments are written to this file; if file already exists moments are read from this file instead of recalculating them
+  overwriteExistingFile: bool = False,  # False: read values from existing file; True: recalculate moments and write new file
+) -> MomentResultsKinematicBinning:
+  """Reads the partial-amplitude values from the PWA fit and calculates the corresponding moments"""
+  if momentResultsFileName and not overwriteExistingFile:
+    try:
+      print(f"Reading PWA moment values from file '{momentResultsFileName}'")
+      return MomentResultsKinematicBinning.load(momentResultsFileName)
+    except FileNotFoundError:
+      print(f"Could not find file '{momentResultsFileName}'. Calculating PWA moments.")
+  print(f"Reading partial-wave amplitude values from file '{dataFileName}'")
+  waves = ["S_0", "P_0", "P_+1", "P_-1", "D_0", "D_+1", "D_+2", "D_-1", "D_-2"]
+  amplitudesDf = pd.read_csv(
+    dataFileName,
+    sep   = r"\s+",  # values are whitespace separated
+    names = ["mass", ] + waves,
+  )
+  # converts columns to correct types
+  def strToComplex(s: str) -> complex:
+    """Converts string of form '(float,float)' to complex"""
+    real, imag = s.strip("()").split(",")
+    return complex(float(real), float(imag))
+  for wave in waves:
+    amplitudesDf[wave] = amplitudesDf[wave].apply(strToComplex)
+  # convert dataframe to MomentResultsKinematicBinning
+  momentResults: list[MomentResult] = []
+  for amplitudesRow in amplitudesDf.to_dict(orient = "records"):  # iterate over list of dictionaries
+    massBinCenter = amplitudesRow["mass"]
+    print(f"Reading partial-wave amplitudes for mass bin at {massBinCenter} GeV")
+    partialWaveAmplitudes = [
+      AmplitudeValue(QnWaveIndex(refl = None, l = 0, m =  0), val = amplitudesRow["S_0" ]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 1, m = -1), val = amplitudesRow["P_-1"]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 1, m =  0), val = amplitudesRow["P_0" ]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 1, m = +1), val = amplitudesRow["P_+1"]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 2, m = -2), val = amplitudesRow["D_-2"]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 2, m = -1), val = amplitudesRow["D_-1"]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 2, m =  0), val = amplitudesRow["D_0" ]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 2, m = +1), val = amplitudesRow["D_+1"]),
+      AmplitudeValue(QnWaveIndex(refl = None, l = 2, m = +2), val = amplitudesRow["D_+2"]),
+    ]
+    amplitudeSet = AmplitudeSet(amps = partialWaveAmplitudes, tolerance = 1e-8)
+    momentResults.append(
+      amplitudeSet.photoProdMomentSet(
+        maxL                = 2 * amplitudeSet.maxSpin,
+        normalize           = False,
+        printMomentFormulas = False,
+        binCenters          = {binVarMass: massBinCenter},
+      )
+    )
+  momentResultsPwa = MomentResultsKinematicBinning(momentResults)
+  if momentResultsFileName:
+    print(f"Writing moments to file '{momentResultsFileName}'")
+    momentResultsPwa.save(momentResultsFileName)
+  return momentResultsPwa
 
 
 def calculateAllMoments(
@@ -181,6 +248,15 @@ if __name__ == "__main__":
         print(f"State of ThreadpoolController after setting number of threads:\n{threadController.info()}")
 
         timer.start("Total execution time")
+
+        if True:
+          with timer.timeThis(f"Time to load moments from partial-wave analysis"):
+            readMomentResultsPwa(
+              dataFileName          = "./dataPhotoProdPiPiUnpol/amplitudes_range_tbin.txt",
+              binVarMass            = CFG.binVarMass,
+              momentResultsFileName = f"{CFG.outFileDirName}/{CFG.outFileNamePrefix}_moments_pwa.pkl",
+              overwriteExistingFile = False,
+            )  # binning used in PWA: HistAxisBinning(nmbBins = 56, minVal = 0.28, maxVal = 1.40, _var = CFG.binVarMass)
 
         calculateAllMoments(CFG, timer)
 
