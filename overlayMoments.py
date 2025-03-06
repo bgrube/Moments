a60703ea774bce4ad197aedc414339230de534df
bgrube <bgrube@jlab.org>
2025-03-05 19:33:37 -0500

    Overlay polarized data. Add code to normalize moments to H_0(0, 0) of first fit result.

--- overlayMoments.py
+++ overlayMoments.py
@@ -103,28 +103,23 @@ if __name__ == "__main__":
 
   # define what to overlay
   # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_CLAS)  # perform analysis of unpolarized pi+ pi- data
-  cfg = deepcopy(CFG_UNPOLARIZED_PIPI_PWA)  # perform analysis of unpolarized pi+ pi- data
-  # cfg = deepcopy(CFG_POLARIZED_PIPI)  # perform analysis of polarized pi+ pi- data
-  fitResults: tuple[tuple[str, str, float | None], ...]  # tuple: (<directory name>, <legend label>, optional: <scale factor>); last entry defines which moments are plotted
-  fitResults = (
-    # (f"{cfg.outFileDirBaseName}.maxL_2",  "#it{L}_{max} = 2",  None),
-    (f"{cfg.outFileDirBaseName}.maxL_4",  "#it{L}_{max} = 4",  None),
-    # (f"{cfg.outFileDirBaseName}.maxL_5",  "#it{L}_{max} = 5",  None),
-    (f"{cfg.outFileDirBaseName}.maxL_6",  "#it{L}_{max} = 6",  None),
-    (f"{cfg.outFileDirBaseName}.maxL_8",  "#it{L}_{max} = 8",  None),
-    # (f"{cfg.outFileDirBaseName}.maxL_10", "#it{L}_{max} = 10", None),
-    (f"{cfg.outFileDirBaseName}.maxL_12", "#it{L}_{max} = 12", None),
-    # (f"{cfg.outFileDirBaseName}.maxL_14", "#it{L}_{max} = 14", None),
+  # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_PWA)  # perform analysis of unpolarized pi+ pi- data
+  cfg = deepcopy(CFG_POLARIZED_PIPI)  # perform analysis of polarized pi+ pi- data
+  normToFirstResult = True  # if set moments are normalized to H_0(0, 0) of first fit result
+  fitResults: tuple[tuple[str, str, float | None], ...] = (  # tuple: (<directory name>, <legend label>, optional: <scale factor>); last entry defines which moments are plotted
+    # (f"{cfg._outFileDirBaseName}.tbin_0.1_0.2.PARA_0.maxL_4",   "#Phi_{0} = 0#circ",   None),  # 1.419187e7 events, 4.345e6 combos
+    # (f"{cfg._outFileDirBaseName}.tbin_0.1_0.2.PARA_135.maxL_4", "#Phi_{0} = 135#circ", None),  # 1.387172e7 events, 4.209e6 combos
+    # (f"{cfg._outFileDirBaseName}.tbin_0.1_0.2.PERP_45.maxL_4",  "#Phi_{0} = 45#circ",  None),  # 1.335436e7 events, 4.100e6 combos
+    # (f"{cfg._outFileDirBaseName}.tbin_0.1_0.2.PERP_90.maxL_4",  "#Phi_{0} = 90#circ",  None),  # 1.529724e7 events, 4.672e6 combos
+    #
+    (f"{cfg._outFileDirBaseName}.tbin_0.2_0.3.PARA_0.maxL_4",   "#Phi_{0} = 0#circ",   None),
+    (f"{cfg._outFileDirBaseName}.tbin_0.2_0.3.PARA_135.maxL_4", "#Phi_{0} = 135#circ", None),
+    (f"{cfg._outFileDirBaseName}.tbin_0.2_0.3.PERP_45.maxL_4",  "#Phi_{0} = 45#circ",  None),
+    (f"{cfg._outFileDirBaseName}.tbin_0.2_0.3.PERP_90.maxL_4",  "#Phi_{0} = 90#circ",  None),
+    #
+    # (f"{cfg._outFileDirBaseName}.tbin_0.1_0.2.PARA_0.maxL_4", "0.1 < #minus t < 0.2 GeV^{2}", None),
+    # (f"{cfg._outFileDirBaseName}.tbin_0.2_0.3.PARA_0.maxL_4", "0.2 < #minus t < 0.3 GeV^{2}", None),
   )
-  # fitResults = (
-  #   # (f"{cfg.outFileDirBaseName}.maxL_4.oldMc", "Old MC",   0.03228842737363692 * (8 * math.pi)),
-  #   # (f"{cfg.outFileDirBaseName}.maxL_4",       "New MC", None),
-  #   (f"{cfg.outFileDirBaseName}.maxL_4", "#it{L}_{max} = 4", None),
-  #   (f"{cfg.outFileDirBaseName}.maxL_5", "#it{L}_{max} = 5", None),
-  #   (f"{cfg.outFileDirBaseName}.maxL_6", "#it{L}_{max} = 6", None),
-  #   (f"{cfg.outFileDirBaseName}.maxL_7", "#it{L}_{max} = 7", None),
-  #   (f"{cfg.outFileDirBaseName}.maxL_8", "#it{L}_{max} = 8", None),
-  # )
   outFileDirName = Utilities.makeDirPath(f"./plotsPhotoProdPiPi{'Unpol' if cfg.polarization is None else 'Pol'}.overlay")
 
   # load moment results
@@ -140,10 +135,25 @@ if __name__ == "__main__":
     momentResultsToOverlay[fitResultLabel] = (momentResultsPhys, scaleFactor)
 
   # ensure that all moment results have identical kinematic binning and identical order of kinematic bins
-  momentResults: tuple[MomentResultsKinematicBinning, ...]         = tuple(value[0]for value in momentResultsToOverlay.values())
+  momentResults: tuple[MomentResultsKinematicBinning, ...]         = tuple(value[0] for value in momentResultsToOverlay.values())
   binCenters:    tuple[dict[KinematicBinningVariable, float], ...] = momentResults[0].binCenters  # bin centers of first moment result
   for momentResult in momentResults[1:]:
     assert momentResult.binCenters == binCenters
+
+  if normToFirstResult:
+    # get integrals of H_0(0, 0) moments of all fit results
+    H000Integrals: list[float] = []
+    H000Index = QnMomentIndex(momentIndex = 0, L = 0, M =0)
+    for moments, _ in momentResultsToOverlay.values():
+      # get H_0(0, 0) moment values in all mass bins
+      H000Vals: tuple[MomentValue, ...] = tuple(HPhys[H000Index] for HPhys in moments if H000Index in HPhys)
+      # calculate integral of H_0(0, 0) moment
+      H000Integrals.append(getHistFromMomentValues(H000Vals, cfg.massBinning, momentPart = "Re").Integral())
+    # set scale factors such that all moments are normalized to H_0(0, 0) of the first fit result
+    scaleFactors = [H000Integrals[0] / integral for integral in H000Integrals]
+    for index, label in enumerate(momentResultsToOverlay.keys()):
+      print(f"Applying scale factor {scaleFactors[index]} to fit result '{label}'")
+      momentResultsToOverlay[label] = (momentResultsToOverlay[label][0], scaleFactors[index])
 
   # plot kinematic dependences of all moments
   lastLabel = fitResults[-1][1]
