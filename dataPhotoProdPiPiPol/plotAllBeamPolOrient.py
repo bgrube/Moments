9495e197b7624ce0da52f2b0264ffb3553bcc33c
bgrube <bgrube@jlab.org>
2025-03-31 16:54:22 -0400

    Plot histograms of `hadd`ed ROOT files.

--- dataPhotoProdPiPiPol/plotAllBeamPolOrient.py
+++ dataPhotoProdPiPiPol/plotAllBeamPolOrient.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
   histsMassPiPi   = []
   histsMassPiPiMc = []
   for beamPolLabel in beamPolLabels:
-    inputFileName = f"./{tBinLabel}/{beamPolLabel}/dataPlots.root"
+    inputFileName = f"./{tBinLabel}/data_{beamPolLabel}/dataPlots.root"
     print(f"Reading data from '{inputFileName}'")
     inputFile = ROOT.TFile.Open(inputFileName, "READ")
     histStack = inputFile.Get("hMassPiPiDataAndMc")
@@ -43,3 +43,26 @@ if __name__ == "__main__":
   histStack.Draw("NOSTACK")
   canv.BuildLegend(0.7, 0.8, 0.99, 0.99)
   canv.SaveAs(f"./{tBinLabel}/{histStack.GetName()}.pdf")
+
+  # plot histograms of `hadd`ed ROOT file
+  inputFileName = f"./{tBinLabel}/dataPlots.root"
+  histNamesToPlot = [
+    "hDataMassPiPi",
+  ]
+  massPiPiRange = (0.28, 2.28)  # [GeV]
+  massPiPiNmbBins = 50
+  massPiPiBinWidth = (massPiPiRange[1] - massPiPiRange[0]) / massPiPiNmbBins
+  for binIndex in range(0, massPiPiNmbBins):
+    massPiPiBinMin = massPiPiRange[0] + binIndex * massPiPiBinWidth
+    massPiPiBinMax = massPiPiBinMin + massPiPiBinWidth
+    histNameSuffix = f"_{massPiPiBinMin:.2f}_{massPiPiBinMax:.2f}"
+    histNamesToPlot += [f"hDataAnglesGjPiPi{histNameSuffix}", f"hDataAnglesHfPiPi{histNameSuffix}"]
+  print(f"Reading data from '{inputFileName}'")
+  inputFile = ROOT.TFile.Open(inputFileName, "READ")
+  for histName in histNamesToPlot:
+    print(f"Plotting histogram '{histName}'")
+    hist = inputFile.Get(histName)
+    canv = ROOT.TCanvas()
+    hist.SetMinimum(0)
+    hist.Draw("COLZ")
+    canv.SaveAs(f"./{tBinLabel}/{histName}.pdf")
