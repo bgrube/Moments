e729feb75cac4dd8eb4d97591c0787229e43e4cb
bgrube <bgrube@jlab.org>
2025-08-11 18:42:00 -0400

    Improve naming and comments.

--- photoProdCalcMoments.py
+++ photoProdCalcMoments.py
@@ -49,9 +49,8 @@ def calculateAllMoments(
   limitToDataEntryRange: tuple[int, int] | None = None,  # for debugging: limit analysis to entry range [begin, end) of real-data data tree
 ) -> None:
   """Performs the moment analysis for the given configuration"""
-  # setup MomentCalculators for all mass bins
-  # momentCalculators = MomentCalculatorsKinematicBinning([])
-  momentCalculators: dict[str | None, MomentCalculatorsKinematicBinning] = {
+  # setup MomentCalculators for all data samples and mass bins
+  momentCalculators: dict[str | None, MomentCalculatorsKinematicBinning] = {  # dict: key = data sample label
     None : MomentCalculatorsKinematicBinning([]),
   } if cfg.method == AnalysisConfig.MethodType.LIN_ALG_BG_SUBTR_NEG_WEIGHTS else {
     "Sig" : MomentCalculatorsKinematicBinning([]),
@@ -59,7 +58,7 @@ def calculateAllMoments(
   }
   assert len(cfg.massBinning) > 0, f"Need at least one mass bin, but found {len(cfg.massBinning)}"
   with timer.timeThis(f"Time to load data and setup MomentCalculators for {len(cfg.massBinning)} bins"):
-    data: dict[str | None, ROOT.RDataFrame] = {
+    dataSamples: dict[str | None, ROOT.RDataFrame] = {  # dict: key = data sample label
       None : cfg.loadData(AnalysisConfig.DataType.REAL_DATA),
     } if cfg.method == AnalysisConfig.MethodType.LIN_ALG_BG_SUBTR_NEG_WEIGHTS else {
       "Sig" : cfg.loadData(AnalysisConfig.DataType.REAL_DATA_SIGNAL),    # select events in signal region (mixture of signal and background)
@@ -78,15 +77,15 @@ def calculateAllMoments(
       dataPsGen = cfg.loadData(AnalysisConfig.DataType.GENERATED_PHASE_SPACE)
     else:
       print("??? Warning: File name for generated phase-space data was not provided. Cannot calculate acceptance correctly.")
-    for label, df in data.items():
-      print(f"Loaded {df.Count().GetValue()} real-data events" + (f" of type '{label}'" if label is not None else ""))
+    for labelDataSample, dataSample in dataSamples.items():
+      print(f"Loaded {dataSample.Count().GetValue()} real-data events" + (f" of type '{labelDataSample}'" if labelDataSample is not None else ""))
       for massBinIndex, massBinCenter in enumerate(cfg.massBinning):
         massBinRange = cfg.massBinning.binValueRange(massBinIndex)
         print(f"Preparing {cfg.binVarMass.name} bin [{massBinIndex + 1} of {len(cfg.massBinning)}] at {massBinCenter} {cfg.binVarMass.unit} with range {massBinRange} {cfg.binVarMass.unit}")
         massBinFilter = cfg.massBinning.binFilter(massBinIndex)
         print(f"Applying filter '{massBinFilter}' to select kinematic bin")
-        dfInBin = df.Filter(massBinFilter)
-        print(f"Loaded {dfInBin.Count().GetValue()} data events; {dfInBin.Sum('eventWeight').GetValue()} after applying event weights")
+        dataInBin = dataSample.Filter(massBinFilter)
+        print(f"Loaded {dataInBin.Count().GetValue()} data events; {dataInBin.Sum('eventWeight').GetValue()} after applying event weights")
         dataPsAccInBin = dataPsAcc.Filter(massBinFilter)
         nmbPsAccEvents = dataPsAccInBin.Count().GetValue()
         nmbPsGenEvents = None
@@ -100,18 +99,18 @@ def calculateAllMoments(
           print(f"Loaded phase-space events: number accepted = {nmbPsAccEvents}")
         # setup moment calculators for data
         dataSet = DataSet(
-          data           = dfInBin,
+          data           = dataInBin,
           phaseSpaceData = dataPsAccInBin,
           nmbGenEvents   = nmbPsGenEvents or nmbPsAccEvents,
           polarization   = cfg.polarization,
         )
-        momentCalculators[label].append(
+        momentCalculators[labelDataSample].append(
           MomentCalculator(
             indices              = MomentIndices(maxL = cfg.maxL, polarized = (cfg.polarization is not None)),
             dataSet              = dataSet,
             binCenters           = {cfg.binVarMass : massBinCenter},
             integralFileBaseName = f"{cfg.outFileDirName}/integralMatrix",
-            flipSignOfWeights    = (label == "Bkg"),  # flip sign of weights for background events
+            flipSignOfWeights    = (labelDataSample == "Bkg"),  # flip sign of weights for background events
           )
         )
 
@@ -119,18 +118,18 @@ def calculateAllMoments(
     # calculate integral matrix for all mass bins in all data sets
     nmbOpenMpThreads = ROOT.getNmbOpenMpThreads()
     # since all data sets use identical MC events, calculate integral matrix only for the first real-data sample
-    firstLabel, m = next(iter(momentCalculators.items()))  # get first entry in momentCalculators  #TODO improve naming
-    with timer.timeThis(f"Time to calculate integral matrices for {len(m)} bins using {nmbOpenMpThreads} OpenMP threads"):
-      print(f"Calculating acceptance integral matrices for {len(m)} bins using {nmbOpenMpThreads} OpenMP threads")
-      m.calculateIntegralMatrices(forceCalculation = True)
-      print(f"Acceptance integral matrix for first bin at {cfg.massBinning[0]} {cfg.binVarMass.unit}:\n{m[0].integralMatrix}")
-      eigenVals, _ = m[0].integralMatrix.eigenDecomp
+    labelFirstSample, momentCalculatorsFirstSample = next(iter(momentCalculators.items()))  # get first entry in momentCalculators
+    with timer.timeThis(f"Time to calculate integral matrices for {len(momentCalculatorsFirstSample)} bins using {nmbOpenMpThreads} OpenMP threads"):
+      print(f"Calculating acceptance integral matrices for {len(momentCalculatorsFirstSample)} bins using {nmbOpenMpThreads} OpenMP threads")
+      momentCalculatorsFirstSample.calculateIntegralMatrices(forceCalculation = True)
+      print(f"Acceptance integral matrix for first bin at {cfg.massBinning[0]} {cfg.binVarMass.unit}:\n{momentCalculatorsFirstSample[0].integralMatrix}")
+      eigenVals, _ = momentCalculatorsFirstSample[0].integralMatrix.eigenDecomp
       print(f"Sorted eigenvalues of acceptance integral matrix for first bin at {cfg.massBinning[0]} {cfg.binVarMass.unit}:\n{np.sort(eigenVals)}")
     # assign integral matrices to `MomentCalculators` for other data samples
-    otherLabels = (label for label in momentCalculators.keys() if label is not firstLabel)
-    for label in otherLabels:
+    labelsOtherSamples = (label for label in momentCalculators.keys() if label is not labelFirstSample)
+    for labelDataSample in labelsOtherSamples:
       for massBinIndex in range(len(cfg.massBinning)):
-        momentCalculators[label][massBinIndex]._integralMatrix = m[massBinIndex].integralMatrix
+        momentCalculators[labelDataSample][massBinIndex]._integralMatrix = momentCalculatorsFirstSample[massBinIndex].integralMatrix
 
   momentResultsFileBaseName = f"{cfg.outFileDirName}/{cfg.outFileNamePrefix}_moments"
   if cfg.calcAccPsMoments:
@@ -138,31 +137,30 @@ def calculateAllMoments(
     if cfg.method == AnalysisConfig.MethodType.LIN_ALG_BG_SUBTR_NEG_WEIGHTS or AnalysisConfig.MethodType.LIN_ALG_BG_SUBTR_MOMENTS:
       with timer.timeThis(f"Time to calculate moments of phase-space MC data using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads"):
         # since all data samples use identical MC events, calculate moments only for first accepted phase-space sample
-        m = next(iter(momentCalculators.values()))  # get first entry in momentCalculators  #TODO improve naming
-        print(f"Calculating moments of phase-space MC data for {len(m)} bins using {nmbOpenMpThreads} OpenMP threads")
-        m.calculateMoments(dataSourceType = MomentCalculator.DataSourceType.ACCEPTED_PHASE_SPACE, normalize = cfg.normalizeMoments)
-        m.momentResultsMeas.savePickle(f"{momentResultsFileBaseName}_accPs_meas.pkl")
-        m.momentResultsPhys.savePickle(f"{momentResultsFileBaseName}_accPs_phys.pkl")
+        momentCalculatorsFirstSample = next(iter(momentCalculators.values()))  # get first entry in momentCalculators
+        print(f"Calculating moments of phase-space MC data for {len(momentCalculatorsFirstSample)} bins using {nmbOpenMpThreads} OpenMP threads")
+        momentCalculatorsFirstSample.calculateMoments(dataSourceType = MomentCalculator.DataSourceType.ACCEPTED_PHASE_SPACE, normalize = cfg.normalizeMoments)
+        momentCalculatorsFirstSample.momentResultsMeas.savePickle(f"{momentResultsFileBaseName}_accPs_meas.pkl")
+        momentCalculatorsFirstSample.momentResultsPhys.savePickle(f"{momentResultsFileBaseName}_accPs_phys.pkl")
     else:
       #TODO calculate also using FIT method
       raise NotImplementedError(f"Calculation of accepted phase-space moments using method {cfg.method} is not implemented yet")
 
   if cfg.method == AnalysisConfig.MethodType.LIN_ALG_BG_SUBTR_NEG_WEIGHTS:
     #TODO calculate normalized and unnormalized moments
-    mCs = momentCalculators[None]  #TODO improve naming
-    with timer.timeThis(f"Time to calculate moments of real data for {len(mCs)} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background using negative weights"):
-      print(f"Calculating moments of real data for {len(mCs)} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background using negative weights")
-      mCs.calculateMoments(normalize = cfg.normalizeMoments, nmbBootstrapSamples = cfg.nmbBootstrapSamples)
-      mCs.momentResultsMeas.savePickle(f"{momentResultsFileBaseName}_meas.pkl")
-      mCs.momentResultsPhys.savePickle(f"{momentResultsFileBaseName}_phys.pkl")
+    with timer.timeThis(f"Time to calculate moments of real data for {len(momentCalculators[None])} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background using negative weights"):
+      print(f"Calculating moments of real data for {len(momentCalculators[None])} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background using negative weights")
+      momentCalculators[None].calculateMoments(normalize = cfg.normalizeMoments, nmbBootstrapSamples = cfg.nmbBootstrapSamples)
+      momentCalculators[None].momentResultsMeas.savePickle(f"{momentResultsFileBaseName}_meas.pkl")
+      momentCalculators[None].momentResultsPhys.savePickle(f"{momentResultsFileBaseName}_phys.pkl")
 
   elif cfg.method == AnalysisConfig.MethodType.LIN_ALG_BG_SUBTR_MOMENTS:
-    for label, mCs in momentCalculators.items():  #TODO improve naming
-      with timer.timeThis(f"Time to calculate moments of '{label}' real data with {len(mCs)} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background at moment level"):
-        print(f"Calculating moments of '{label}' real data with {len(mCs)} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background at moment level")
-        mCs.calculateMoments(normalize = cfg.normalizeMoments, nmbBootstrapSamples = cfg.nmbBootstrapSamples)
-        mCs.momentResultsMeas.savePickle(f"{momentResultsFileBaseName}_meas.pkl")
-        mCs.momentResultsPhys.savePickle(f"{momentResultsFileBaseName}_phys.pkl")
+    for labelDataSample, momentCalculatorsDataSample in momentCalculators.items():
+      with timer.timeThis(f"Time to calculate moments of '{labelDataSample}' real data with {len(momentCalculatorsDataSample)} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background at moment level"):
+        print(f"Calculating moments of '{labelDataSample}' real data with {len(momentCalculatorsDataSample)} bins using the linear-algebra method with {nmbOpenMpThreads} OpenMP threads and subtracting background at moment level")
+        momentCalculatorsDataSample.calculateMoments(normalize = cfg.normalizeMoments, nmbBootstrapSamples = cfg.nmbBootstrapSamples)
+        momentCalculatorsDataSample.momentResultsMeas.savePickle(f"{momentResultsFileBaseName}_meas.pkl")
+        momentCalculatorsDataSample.momentResultsPhys.savePickle(f"{momentResultsFileBaseName}_phys.pkl")
     # perform background subtraction at moment level
     #!NOTE! This gives wrong estimates for the uncertainty (and covariances) of
     # H_0(0, 0) because the number of signal and background events are treated
@@ -175,10 +173,10 @@ def calculateAllMoments(
     nmbFitAttempts          = 100
     nmbParallelFitProcesses = 100
     randomSeed              = 123456789
-    for label, mCs in momentCalculators.items():  #TODO improve naming
-      with timer.timeThis(f"Time to fit moments to '{label}' real data with {len(mCs)} bins running {nmbFitAttempts} fit attempts in {nmbParallelFitProcesses} processes and subtracting background at moment level"):
-        print(f"Fitting moments to '{label}' real data with {len(mCs)} bins running {nmbFitAttempts} fit attempts in {nmbParallelFitProcesses} processes and subtracting background at moment level")
-        mCs.fitMomentsMultipleAttempts(
+    for labelDataSample, momentCalculatorsDataSample in momentCalculators.items():
+      with timer.timeThis(f"Time to fit moments to '{labelDataSample}' real data with {len(momentCalculatorsDataSample)} bins running {nmbFitAttempts} fit attempts in {nmbParallelFitProcesses} processes and subtracting background at moment level"):
+        print(f"Fitting moments to '{labelDataSample}' real data with {len(momentCalculatorsDataSample)} bins running {nmbFitAttempts} fit attempts in {nmbParallelFitProcesses} processes and subtracting background at moment level")
+        momentCalculatorsDataSample.fitMomentsMultipleAttempts(
           nmbFitAttempts          = nmbFitAttempts,
           nmbParallelFitProcesses = nmbParallelFitProcesses,
           randomSeed              = randomSeed,
