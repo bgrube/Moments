4639128b3edbefa206b1b0854241c3755c0e6de0
bgrube <bgrube@jlab.org>
2025-08-15 13:19:12 -0400

    Extend code to generate phase-space data in case no input data is specified. Ensure that each mass bin uses a different random seed. Parallelize merging of ROOT files. Add config for JPAC MC data. Improve naming and console output.

--- photoProdWeightData.py
+++ photoProdWeightData.py
@@ -25,6 +25,7 @@ from AnalysisConfig import (
   CFG_KEVIN,
   CFG_POLARIZED_PIPI,
   CFG_UNPOLARIZED_PIPI_CLAS,
+  CFG_UNPOLARIZED_PIPI_JPAC,
   CFG_UNPOLARIZED_PIPI_PWA,
 )
 from MomentCalculator import MomentResultsKinematicBinning
@@ -37,54 +38,92 @@ import Utilities
 print = functools.partial(print, flush = True)
 
 
-def weightAccPhaseSpaceWithIntensity(
-  intensityFormula:   str,  # formula for intensity function
-  kinematicBinFilter: str,  # filter string that selects kinematic bin
-  outFileName:        str,  # ROOT file to which weighted events are written
-  cfg:                AnalysisConfig,
-  seed:               int = 123456789,
+def weightDataWithIntensity(
+  intensityFormula: str,  # formula for intensity function
+  massBinIndex:     int,  # index of mass bin to generate data for
+  outFileName:      str,  # ROOT file to which weighted events are written
+  cfg:              AnalysisConfig,
+  inputDataType:    AnalysisConfig.DataType | None = None,    # if `None`, phase-space distribution in angles is generated
+  nmbGenPsEvents:   int                            = 100000,  # number phase-space events to generate
+  seed:             int                            = 123456789,
 ) -> None:
   """Weight accepted phase-space MC data with given intensity formula"""
-  # read accepted phase-space MC data
-  psAccData = cfg.loadData(AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE).Filter(kinematicBinFilter)
-  nmbPsAccEvents = psAccData.Count().GetValue()
-  print(f"File '{cfg.psAccFileName}' contains {nmbPsAccEvents} events in bin '{kinematicBinFilter}'")
+  kinematicBinFilter: str = cfg.massBinning.binFilter(massBinIndex)
+  kinematicBinRange: tuple[float, float] = cfg.massBinning.binValueRange(massBinIndex)
+  # get input data
+  dataToWeight: ROOT.RDataFrame | None = None
+  if inputDataType is None:
+    print(f"Generating phase-space distribution with {nmbGenPsEvents} events")
+    ROOT.gRandom.SetSeed(seed)
+    dataToWeight = (
+      ROOT.RDataFrame(nmbGenPsEvents)
+          .Define("cosTheta", "(Double32_t)gRandom->Uniform(-1, +1)")
+          .Define("theta",    "(Double32_t)std::acos(cosTheta)")
+          .Define("phiDeg",   "(Double32_t)gRandom->Uniform(-180, +180)")
+          .Define("phi",      "(Double32_t)phiDeg * TMath::DegToRad()")
+          .Define("mass",    f"(Double32_t)gRandom->Uniform({kinematicBinRange[0]}, {kinematicBinRange[1]})")
+          .Filter('if (rdfentry_ == 0) { cout << "Running event loop in weightAccPhaseSpaceWithIntensity()" << endl; } return true;')  # noop filter that just logs when event loop is running
+    )
+    if cfg.polarization is not None:
+      # polarized case: add Phi and polarization columns
+      dataToWeight = (
+        dataToWeight.Define("PhiDeg",   "(Double32_t)gRandom->Uniform(-180, +180)")
+                    .Define("Phi",      "(Double32_t)PhiDeg * TMath::DegToRad()")
+      )
+      if isinstance(cfg.polarization, float):
+        dataToWeight = dataToWeight.Define("beamPol", f"(Double32_t){cfg.polarization}")
+      elif isinstance(cfg.polarization, str):
+        raise ValueError(f"Cannot read polarization from column '{cfg.polarization}'")
+  else:
+    print(f"Loading data of type '{inputDataType}'")
+    dataToWeight = cfg.loadData(inputDataType)
+    assert dataToWeight is not None, f"Could not load data of type '{inputDataType}'"
+    dataToWeight = dataToWeight.Filter(kinematicBinFilter)
+    nmbInputEvents = dataToWeight.Count().GetValue()
+    print(f"Input data contain {nmbInputEvents} events in bin '{kinematicBinFilter}'")
   # calculate intensity weight and random number in [0, 1] for each event
   print(f"Calculating weights using formula '{intensityFormula}'")
-  ROOT.gRandom.SetSeed(seed)
-  psAccData = (
-    psAccData.Define("intensityWeight", f"(Double32_t){intensityFormula}")
-             .Define("rndNmb",           "(Double32_t)gRandom->Rndm()")
+  # ROOT.gRandom.SetSeed(seed)
+  dataToWeight = (
+    dataToWeight.Define("intensityWeight", f"(Double32_t){intensityFormula}")
+                .Define("rndNmb",           "(Double32_t)gRandom->Rndm()")  # random number in [0, 1] for each event
   )
+  tmpFileName = f"{outFileName}.unweighted.root"
+  dataToWeight.Snapshot(cfg.treeName, tmpFileName)  # write unweighted data to file to ensure that random columns are filled only once
+  dataToWeight = ROOT.RDataFrame(cfg.treeName, tmpFileName)  # read data back
   # determine maximum weight
-  maxIntensityWeight = psAccData.Max("intensityWeight").GetValue()
+  maxIntensityWeight = dataToWeight.Max("intensityWeight").GetValue()
   print(f"Maximum intensity is {maxIntensityWeight}")
   # accept each event with probability intensityWeight / maxIntensityWeight
-  weightedPsAccData = (
-    psAccData.Define("acceptEvent", f"(bool)(rndNmb < (intensityWeight / {maxIntensityWeight}))")
-             .Filter("acceptEvent == true")
+  weightedData = (
+    dataToWeight.Define("acceptEvent", f"(bool)(rndNmb < (intensityWeight / {maxIntensityWeight}))")
+                .Filter("acceptEvent == true")
   )
-  nmbWeightedEvents = weightedPsAccData.Count().GetValue()
-  print(f"After weighting with the intensity function, the sample contains {nmbWeightedEvents} accepted events; generator efficiency is {nmbWeightedEvents / nmbPsAccEvents}")
+  nmbWeightedEvents = weightedData.Count().GetValue()
+  print(f"After weighting with the intensity function, the sample contains {nmbWeightedEvents} accepted events; generator efficiency is {nmbWeightedEvents / nmbGenPsEvents}")
   # write weighted data to file
   print(f"Writing data weighted with intensity function to file '{outFileName}'")
-  weightedPsAccData.Snapshot(cfg.treeName, outFileName)
+  weightedData.Snapshot(cfg.treeName, outFileName)
+  subprocess.run(f"rm -f {tmpFileName}", shell = True)
 
 
 if __name__ == "__main__":
   # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_CLAS)  # perform analysis of unpolarized pi+ pi- data
   # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_PWA)  # perform analysis of unpolarized pi+ pi- data
+  cfg = deepcopy(CFG_UNPOLARIZED_PIPI_JPAC)  # perform analysis of unpolarized pi+ pi- data
   # cfg = deepcopy(CFG_POLARIZED_PIPI)  # perform analysis of polarized pi+ pi- data
-  cfg = deepcopy(CFG_KEVIN)  # perform analysis of Kevin's polarized K- K_S Delta++ data
+  # cfg = deepcopy(CFG_KEVIN)  # perform analysis of Kevin's polarized K- K_S Delta++ data
 
   tBinLabels = (
-    "tbin_0.1_0.5",
+    # "tbin_0.1_0.5",
+    "tbin_0.4_0.5",
   )
   beamPolLabels = (
-    "PARA_0",
+    # "PARA_0",
     # "PARA_135",
     # "PERP_45",
     # "PERP_90",
+    "Unpol",
   )
   maxLs = (
     4,
@@ -113,36 +152,42 @@ if __name__ == "__main__":
 
           print(f"Using configuration:\n{cfg}")
           timer.start("Total execution time")
-          momentResultsFileBaseName = f"{cfg.outFileDirName}/{cfg.outFileNamePrefix}_moments"
-          print(f"Reading physical moments from file '{momentResultsFileBaseName}_phys.pkl'")
-          momentResults = MomentResultsKinematicBinning.loadPickle(f"{momentResultsFileBaseName}_phys.pkl")
-          # momentResults = MomentResultsKinematicBinning.loadPickle(f"{momentResultsFileBaseName}_pwa_SPD.pkl")
+          momentResultsFileName = f"{cfg.outFileDirName}/{cfg.outFileNamePrefix}_moments_phys.pkl"
+          # momentResultsFileName = f"{cfg.outFileDirName}/{cfg.outFileNamePrefix}_moments_pwa_SPD.pkl"
+          print(f"Reading physical moments from file '{momentResultsFileName}'")
+          momentResults = MomentResultsKinematicBinning.loadPickle(momentResultsFileName)
           for momentResultsForBin in momentResults:
             massBinCenter = momentResultsForBin.binCenters[cfg.massBinning.var]
             massBinIndex  = cfg.massBinning.findBin(massBinCenter)
             assert massBinIndex is not None, f"Could not find bin for mass value of {massBinCenter} GeV"
-            outFileBaseName = f"{cfg.outFileDirName}/psAccData_weighted_flat"
-            # outFileBaseName = f"{cfg.outFileDirName}/psAccData_weighted_pwa_SPD_flat"
+            outFileBaseName = f"{cfg.outFileDirName}/data_weighted_flat"
+            # outFileBaseName = f"{cfg.outFileDirName}/data_weighted_pwa_SPD_flat"
             outFileName     = f"{outFileBaseName}_{massBinIndex}.root"
             print(f"Writing accepted phase-space events for bin {massBinIndex} at {massBinCenter:.2f} {cfg.massBinning.var.unit} weighted by intensity function into file '{outFileName}'")
-            weightAccPhaseSpaceWithIntensity(
-              intensityFormula   = momentResultsForBin.intensityFormula(
+            weightDataWithIntensity(
+              intensityFormula = momentResultsForBin.intensityFormula(  #TODO include imaginary parts into intensity formula
                 polarization = cfg.polarization,
                 thetaFormula = "theta",
                 phiFormula   = "phi",
                 PhiFormula   = "Phi",
                 printFormula = False,
               ),
-              kinematicBinFilter = cfg.massBinning.binFilter(massBinIndex),
-              outFileName        = outFileName,
-              cfg                = cfg,
+              massBinIndex     = massBinIndex,
+              outFileName      = outFileName,
+              cfg              = cfg,
+              # inputDataType    = AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE,
+              inputDataType    = None,  # generate phase-space distribution in angles
+              nmbGenPsEvents   = 100000,
+              seed             = 12345 + massBinIndex,  # ensure random data in mass bins are independent
             )
 
           # merge trees with weighted MC data from different mass bins into single file
-          outFileName = f"{outFileBaseName}.maxL_{cfg.maxL}.root"
-          cmd = f"hadd {outFileName} {outFileBaseName}_*.root"
-          print(f"Merging files from all mass bins: '{cmd}'")
-          gitInfo = subprocess.run(cmd, shell = True)
+          nmbParallelJobs = 10
+          with timer.timeThis(f"Time to merge ROOT files from all mass bins using hadd with {nmbParallelJobs} parallel jobs"):
+            outFileName = f"{outFileBaseName}.root"
+            cmd = f"hadd -j {nmbParallelJobs} {outFileName} {outFileBaseName}_*.root"
+            print(f"Merging ROOT files from all mass bins: '{cmd}'")
+            subprocess.run(cmd, shell = True)
 
           timer.stop("Total execution time")
           print(timer.summary)
