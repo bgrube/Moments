0a0983eacaa167b9dcc9213b1afd1eef27a57b93
bgrube <bgrube@jlab.org>
2025-08-21 17:04:17 -0400

    Add first implementation of function that boosts data to lab frame and generates vertex distribution. There is still an issue that the beam is not along z in the lab frame.

--- dataPhotoProdPiPiUnpolJPAC/boostMcDataToLab.py
+++ dataPhotoProdPiPiUnpolJPAC/boostMcDataToLab.py
@@ -7,9 +7,15 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import IntEnum
+import os
 
 import ROOT
 import hddm_s
+
+from makeMomentsInputTree import (
+  lorentzVectors,
+  readData,
+)
 
 
 class ParticleIDsGeant(IntEnum):
@@ -121,22 +127,94 @@ def fillHddmRecord(
     momentum.pz = momLv.Pz()
 
 
-if __name__ == "__main__":
-  eventData = EventData(
-    runNmb       = 30731,
-    eventNmb     = 1,
-    beamMomLv    = ROOT.TLorentzVector( 0,          0,          8.58286,  8.58286),  # [GeV]
-    recoilMomLv  = ROOT.TLorentzVector(-0.429367,  -0.00775752, 0.162113, 1.04453),  # [GeV]
-    piPlusMomLv  = ROOT.TLorentzVector( 0.0960281, -0.382075,   4.61858,  4.63745),  # [GeV]
-    piMinusMomLv = ROOT.TLorentzVector( 0.333339,   0.389832,   3.80217,  3.83915),  # [GeV]
-    vertexPos    = ROOT.TVector3(0, 0, 0),  # [cm]
+def defineDataFrameColumns(
+  df:       ROOT.RDataFrame,
+  lvBeam:   str,  # function-argument list with Lorentz-vector components of beam photon
+  lvTarget: str,  # function-argument list with Lorentz-vector components of target proton
+  lvRecoil: str,  # function-argument list with Lorentz-vector components of recoil proton
+  lvPip:    str,  # function-argument list with Lorentz-vector components of pi^+
+  lvPim:    str,  # function-argument list with Lorentz-vector components of pi^-
+) -> ROOT.RDataFrame:
+  """Returns RDataFrame with additional columns"""
+  vertexPosFcn = f"""
+    // define target geometry
+    const double targetR    =  2;  // [cm]
+    const double targetZmin = 52;  // [cm]
+    const double targetZmax = 78;  // [cm]
+    // throw random point in target volume
+    const double r   = targetR * sqrt(gRandom->Uniform(0, 1));
+    const double phi = gRandom->Uniform(0, TMath::TwoPi());
+    const double z   = gRandom->Uniform(targetZmin, targetZmax);
+    return TVector3(r * cos(phi), r * sin(phi), z);
+  """
+  df = (
+    df.Define("lvBeam",      f"TLorentzVector({lvBeam})")
+      .Define("lvTarget",    f"TLorentzVector({lvTarget})")
+      .Define("lvRecoil",    f"TLorentzVector({lvRecoil})")
+      .Define("lvPip",       f"TLorentzVector({lvPip})")
+      .Define("lvPim",       f"TLorentzVector({lvPim})")
+      # boost events to target rest frame = lab frame
+      .Define("lvBeamLab",   f"TLorentzVector temp = lvBeam;   temp.Boost(-lvTarget.BoostVector()); return temp")
+      .Define("lvTargetLab", f"TLorentzVector temp = lvTarget; temp.Boost(-lvTarget.BoostVector()); return temp")
+      .Define("lvRecoilLab", f"TLorentzVector temp = lvRecoil; temp.Boost(-lvTarget.BoostVector()); return temp")
+      .Define("lvPipLab",    f"TLorentzVector temp = lvPip;    temp.Boost(-lvTarget.BoostVector()); return temp")
+      .Define("lvPimLab",    f"TLorentzVector temp = lvPim;    temp.Boost(-lvTarget.BoostVector()); return temp")
+      # generate vertex distribution
+      .Define("vertexPos",   vertexPosFcn)
+      .Define("vertexPosX",  "vertexPos.X()")
+      .Define("vertexPosY",  "vertexPos.Y()")
   )
+  return df
 
-  # create output stream
-  outFileName = "./test.hddm"
-  outStream = hddm_s.ostream(outFileName)
 
-  # create, fill, and write record
-  record = hddm_s.HDDM()
-  fillHddmRecord(record, eventData)
-  outStream.write(record)
+if __name__ == "__main__":
+  ROOT.gROOT.SetBatch(True)
+
+  inputData: dict[str, str] = {  # mapping of t-bin labels to input file names
+    "tbin_0.4_0.5" : "./mc/mc_full_model/mc0.4-0.5_ful.dat",
+    # "tbin_0.5_0.6" : "./mc/mc_full_model/mc0.5-0.6_ful.dat",
+    # "tbin_0.6_0.7" : "./mc/mc_full_model/mc0.6-0.7_ful.dat",
+    # "tbin_0.7_0.8" : "./mc/mc_full_model/mc0.7-0.8_ful.dat",
+    # "tbin_0.8_0.9" : "./mc/mc_full_model/mc0.8-0.9_ful.dat",
+    # "tbin_0.9_1.0" : "./mc/mc_full_model/mc0.9-1.0_ful.dat",
+  }
+  outputDirName  = "mc_full"
+  outputTreeName = "PiPi"
+  outputColumns  = ("lvBeam", "lvTarget", "lvRecoil", "lvPip", "lvPim")
+
+  for tBinLabel, inputFileName in inputData.items():
+    os.makedirs(f"{outputDirName}/{tBinLabel}", exist_ok = True)
+    # outputFileName = f"{outputDirName}/{tBinLabel}/data_labFrame_flat.root"
+    outputFileName = f"./data_labFrame_flat.root"
+
+    df = defineDataFrameColumns(
+      df = readData(inputFileName),
+      **lorentzVectors(),
+    # ).Snapshot(outputTreeName, outputFileName, outputColumns)
+    ).Snapshot(outputTreeName, outputFileName)
+    print(f"ROOT DataFrame columns: {list(df.GetColumnNames())}")
+    print(f"ROOT DataFrame entries: {df.Count().GetValue()}")
+
+    hist = df.Histo2D(ROOT.RDF.TH2DModel(f"vertexXY", ";x_{vert} [cm];y_{vert} [cm];", 100, -2, +2, 100, -2, +2), "vertexPosX", "vertexPosY").GetValue()
+    canv = ROOT.TCanvas()
+    hist.Draw("COLZ")
+    canv.SaveAs(f"./{hist.GetName()}.pdf")
+
+  # eventData = EventData(
+  #   runNmb       = 30731,
+  #   eventNmb     = 1,
+  #   beamMomLv    = ROOT.TLorentzVector( 0,          0,          8.58286,  8.58286),  # [GeV]
+  #   recoilMomLv  = ROOT.TLorentzVector(-0.429367,  -0.00775752, 0.162113, 1.04453),  # [GeV]
+  #   piPlusMomLv  = ROOT.TLorentzVector( 0.0960281, -0.382075,   4.61858,  4.63745),  # [GeV]
+  #   piMinusMomLv = ROOT.TLorentzVector( 0.333339,   0.389832,   3.80217,  3.83915),  # [GeV]
+  #   vertexPos    = ROOT.TVector3(0, 0, 0),  # [cm]
+  # )
+
+  # # create output stream
+  # outFileName = "./test.hddm"
+  # outStream = hddm_s.ostream(outFileName)
+
+  # # create, fill, and write record
+  # record = hddm_s.HDDM()
+  # fillHddmRecord(record, eventData)
+  # outStream.write(record)
