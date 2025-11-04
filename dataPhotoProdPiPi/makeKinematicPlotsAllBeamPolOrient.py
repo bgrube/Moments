ea9beaa4b6c60a091bf0c36b5b6bae2dd12f9c4c
bgrube <bgrube@jlab.org>
2025-11-03 19:28:51 -0500

    Fix directory names. Switch to 2018 data. Improve variable naming.

--- dataPhotoProdPiPi/makeKinematicPlotsAllBeamPolOrient.py
+++ dataPhotoProdPiPi/makeKinematicPlotsAllBeamPolOrient.py
@@ -44,14 +44,14 @@ if __name__ == "__main__":
   dataDirName = "./polarized"
   # data sets to process
   dataPeriods = (
-    "2017_01",
-    # "2018_08",
+    # "2017_01",
+    "2018_08",
   )
   tBinLabels  = (
     "tbin_0.1_0.2",
     "tbin_0.2_0.3",
-    # "tbin_0.3_0.4",
-    # "tbin_0.4_0.5",
+    "tbin_0.3_0.4",
+    "tbin_0.4_0.5",
   )
   subsystems  = (
     "PiPi",
@@ -70,10 +70,11 @@ if __name__ == "__main__":
     for tBinLabel in tBinLabels:
       for subsystem in subsystems:
         # generate real-data plots for all beam-polarization orientations combined
-        plotDir = f"{dataDirName}/{dataPeriod}/{tBinLabel}/{subsystem}/plots_realData"
+        plotBaseDir = f"{dataDirName}/{dataPeriod}/{tBinLabel}/{subsystem}"
         # `hadd` ROOT files with plots for real data for all beam-polarization orientations
-        sourcePlotFileNames = tuple(f"{plotDir}/{beamPolLabel}/plots.root" for beamPolLabel in beamPolLabels)
-        mergedPlotFileName  = f"{plotDir}/plots.root"
+        sourcePlotFileNames = tuple(f"{plotBaseDir}/plots_REAL_DATA/{beamPolLabel}/plots.root" for beamPolLabel in beamPolLabels)
+        mergedPlotFileName  = f"{plotBaseDir}/plots_REAL_DATA/plots.root"
+        mergedPlotFileName = f"{plotBaseDir}/plots_REAL_DATA/{beamPolLabels[0]}/plots.root"
         print(f"`hadd`ing files for data period '{dataPeriod}', t bin '{tBinLabel}', subsystem '{subsystem}': {sourcePlotFileNames}")
         subprocess.run(f"hadd -f {mergedPlotFileName} {' '.join(sourcePlotFileNames)}", shell = True, check = True)
         # plot all histograms in `hadd`ed ROOT file
@@ -82,12 +83,12 @@ if __name__ == "__main__":
         for key in mergedPlotFile.GetListOfKeys():
           obj = key.ReadObj()
           if obj.InheritsFrom("TH1"):
-            plotHistogram(obj, plotDir)
+            plotHistogram(obj, plotBaseDir)
             # plot log version of selected 2D histograms
             histName = obj.GetName()
             if histName.startswith("massPiPiVs"):
               obj.SetName(f"{histName}_log")
-              plotHistogram(obj, plotDir, logZ = True)
+              plotHistogram(obj, plotBaseDir, logZ = True)
 
         # overlay real-data mass distribution for all beam-polarization orientations combined
         # with mass distribution from accepted phase-space MC
@@ -95,7 +96,7 @@ if __name__ == "__main__":
         histMassData = mergedPlotFile.Get(histMassName)
         #!NOTE! we use the same phase-space MC for all beam-polarization orientations, hence we
         #       overlay the MC distribution for only one orientation
-        mcPlotFile = ROOT.TFile.Open(f"{dataDirName}/{dataPeriod}/{tBinLabel}/{subsystem}/plots_mcReco/PARA_0/plots.root", "READ")
+        mcPlotFile = ROOT.TFile.Open(f"{plotBaseDir}/plots_ACCEPTED_PHASE_SPACE/PARA_0/plots.root", "READ")
         histMassMc = mcPlotFile.Get(histMassName)
         canv = ROOT.TCanvas()
         histStack = ROOT.THStack(f"mass{subsystem}DataAndMc", f";{histMassMc.GetXaxis().GetTitle()};{histMassMc.GetYaxis().GetTitle()}")
@@ -109,6 +110,6 @@ if __name__ == "__main__":
         histMassData.SetTitle("RF-subtracted Data")
         histMassMc.SetTitle("Accepted Phase-Space MC")
         canv.BuildLegend(0.7, 0.8, 0.99, 0.99)
-        canv.SaveAs(f"{plotDir}/{histStack.GetName()}.pdf")
+        canv.SaveAs(f"{plotBaseDir}/{histStack.GetName()}.pdf")
         mergedPlotFile.Close()
         mcPlotFile.Close()
