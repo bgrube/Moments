3d3dc511fa7b9bd45f02070ef165ebdd642733e9
bgrube <bgrube@jlab.org>
2025-12-12 11:04:22 -0500

    Create 3D histograms for phi_HF-even and -odd components of phase-space acceptance. Plot phase-space acceptance in slices of Phi. Improve code structure.

--- dataPhotoProdPiPi/testEfficiencyHoles.py
+++ dataPhotoProdPiPi/testEfficiencyHoles.py
@@ -16,96 +16,130 @@ if __name__ == "__main__":
   assert ROOT.gROOT.LoadMacro("../rootlogon.C") == 0, "Error loading '../rootlogon.C'"
   ROOT.gStyle.SetOptStat(False)
 
-  plotsFile = ROOT.TFile.Open("./polarized/2018_08/tbin_0.1_0.2/PiPi/plots_ACCEPTED_PHASE_SPACE/PARA_0/plots.root", "READ")
-  hist = plotsFile.Get("anglesHFPiPi_0.72_0.76")
-  canv = ROOT.TCanvas()
-  hist.Draw("COLZ")
-  # box = ROOT.TBox()
-  # box.SetFillStyle(0)
-  # box.SetLineWidth(2)
-  # box.SetLineColor(ROOT.kRed + 1)
-  # box.DrawBox(-0.9,  -30, -0.2,  +30)
-  # box.DrawBox(+0.2, -180, +0.9, -150)
-  # box.DrawBox(+0.2, +150, +0.9, +180)
-  # box.DrawBox(+0.3, -180, +0.7, -120)
-  ellipse = ROOT.TEllipse()
-  ellipse.SetFillStyle(0)
-  ellipse.SetLineWidth(2)
-  ellipse.SetLineColor(ROOT.kRed + 1)
-  ellipse.DrawEllipse(-0.55,    0, 0.35, 30,   0, 360, 0)
-  ellipse.DrawEllipse( 0.55, -180, 0.35, 30,   0, 180, 0)
-  ellipse.DrawEllipse( 0.55, +180, 0.35, 30, 180, 360, 0)
-  canv.RedrawAxis()
-  canv.SaveAs(f"./{hist.GetName()}.pdf")
-
-  # decompose acceptance histogram into phi-odd and phi-even parts
-  histOdd  = hist.Clone(f"{hist.GetName()}_odd")
-  histEven = hist.Clone(f"{hist.GetName()}_even")
-  assert hist.GetNbinsY() % 2 == 0, "Number of phi bins is odd!"
-  for cosThetaBin in range(1, hist.GetNbinsX() + 1):
-    for phiBinNeg in range(1, hist.GetNbinsY() // 2 + 1):
-      phiBinPos = hist.GetYaxis().FindBin(-hist.GetYaxis().GetBinCenter(phiBinNeg))
-      valPhiPos = hist.GetBinContent(cosThetaBin, phiBinPos)
-      valPhiNeg = hist.GetBinContent(cosThetaBin, phiBinNeg)
-      valPhiOdd  = (valPhiPos - valPhiNeg) / 2
-      valPhiEven = (valPhiPos + valPhiNeg) / 2
-      histOdd.SetBinContent (cosThetaBin, phiBinPos, +valPhiOdd)
-      histOdd.SetBinContent (cosThetaBin, phiBinNeg, -valPhiOdd)
-      histEven.SetBinContent(cosThetaBin, phiBinPos, valPhiEven)
-      histEven.SetBinContent(cosThetaBin, phiBinNeg, valPhiEven)
-  histSum = hist.Clone(f"{hist.GetName()}_sum")
-  histSum.Add(histOdd, histEven)
-  for hist in (histEven, histSum):
+  if True:
+    plotsFile = ROOT.TFile.Open("./polarized/2018_08/tbin_0.1_0.2/PiPi/plots_ACCEPTED_PHASE_SPACE/PARA_0/plots.root", "READ")
+    hist = plotsFile.Get("anglesHFPiPi_0.72_0.76")
     canv = ROOT.TCanvas()
     hist.Draw("COLZ")
+    # box = ROOT.TBox()
+    # box.SetFillStyle(0)
+    # box.SetLineWidth(2)
+    # box.SetLineColor(ROOT.kRed + 1)
+    # box.DrawBox(-0.9,  -30, -0.2,  +30)
+    # box.DrawBox(+0.2, -180, +0.9, -150)
+    # box.DrawBox(+0.2, +150, +0.9, +180)
+    # box.DrawBox(+0.3, -180, +0.7, -120)
+    ellipse = ROOT.TEllipse()
+    ellipse.SetFillStyle(0)
+    ellipse.SetLineWidth(2)
+    ellipse.SetLineColor(ROOT.kRed + 1)
+    ellipse.DrawEllipse(-0.55,    0, 0.35, 30,   0, 360, 0)
+    ellipse.DrawEllipse( 0.55, -180, 0.35, 30,   0, 180, 0)
+    ellipse.DrawEllipse( 0.55, +180, 0.35, 30, 180, 360, 0)
+    canv.RedrawAxis()
     canv.SaveAs(f"./{hist.GetName()}.pdf")
-    with ROOT.TFile.Open(f"./{hist.GetName()}.root", "RECREATE") as outFile:
-      hist.Write()
-  zRange = max(abs(histOdd.GetMaximum()), abs(histOdd.GetMinimum()))
-  histOdd.SetMaximum(+zRange)
-  histOdd.SetMinimum(-zRange)
-  ROOT.gStyle.SetPalette(ROOT.kLightTemperature)  # use pos/neg color palette and symmetric z axis
-  histOdd.Draw("COLZ")
-  canv.SaveAs(f"./{histOdd.GetName()}.pdf")
-  with ROOT.TFile.Open(f"./{histOdd.GetName()}.root", "RECREATE") as outFile:
-    histOdd.Write()
-  ROOT.gStyle.SetPalette(ROOT.kBird)  # restore default color palette
 
-  accPsData = ROOT.RDataFrame("PiPi", "./polarized/2018_08/tbin_0.1_0.2/PiPi/phaseSpace_acc_flat_PARA_0.root")
-  CUT_CPP_CODE = """
-    bool
-    isInHoles(
-      const double cosTheta,
-      const double phi
-    ) {
-      static std::vector<TEllipse*> holes = {
-        new TEllipse(-0.55,    0, 0.35, 30),
-        new TEllipse( 0.55, -180, 0.35, 30),
-        new TEllipse( 0.55, +180, 0.35, 30)
-      };
-      for (const auto& hole : holes) {
-        if (hole->IsInside(cosTheta, phi)) {
-          return true;
+    accPsData = ROOT.RDataFrame("PiPi", "./polarized/2018_08/tbin_0.1_0.2/PiPi/phaseSpace_acc_flat_PARA_0.root")
+    CUT_CPP_CODE = """
+      bool
+      isInHoles(
+        const double cosTheta,
+        const double phi
+      ) {
+        static std::vector<TEllipse*> holes = {
+          new TEllipse(-0.55,    0, 0.35, 30),
+          new TEllipse( 0.55, -180, 0.35, 30),
+          new TEllipse( 0.55, +180, 0.35, 30)
+        };
+        for (const auto& hole : holes) {
+          if (hole->IsInside(cosTheta, phi)) {
+            return true;
+          }
         }
+        return false;
       }
-      return false;
-    }
-  """
-  ROOT.gInterpreter.Declare(CUT_CPP_CODE)
+    """
+    ROOT.gInterpreter.Declare(CUT_CPP_CODE)
 
-  hist = (
-    accPsData.Filter("(0.72 < mass and mass < 0.76)")
-             .Filter("not isInHoles(cosTheta, phiDeg)")
-            #  .Filter("not ((cosTheta > 0.8) and (-100 < phiDeg and phiDeg < +100))")
-            #  .Filter("not ((0.3 < cosTheta and cosTheta < 0.7) and (-180 < phiDeg and phiDeg < -120))")
-            #  .Filter("not (-90 < phiDeg and phiDeg < +90)")
-            #  .Filter("(phiDeg > 0)")
-             .Histo2D(
-               ("anglesHFPiPi_0.72_0.76_cut", ";cos#theta_{HF};#phi_{HF} [deg]", 100, -1,+1, 72, -180, +180),
-               "cosTheta", "phiDeg"
-             ).GetValue()
-  )
-  canv = ROOT.TCanvas()
-  hist.Draw("COLZ")
-  canv.RedrawAxis()
-  canv.SaveAs(f"./{hist.GetName()}.pdf")
+    hist = (
+      accPsData.Filter("(0.72 < mass and mass < 0.76)")
+              .Filter("not isInHoles(cosTheta, phiDeg)")
+              #  .Filter("not ((cosTheta > 0.8) and (-100 < phiDeg and phiDeg < +100))")
+              #  .Filter("not ((0.3 < cosTheta and cosTheta < 0.7) and (-180 < phiDeg and phiDeg < -120))")
+              #  .Filter("not (-90 < phiDeg and phiDeg < +90)")
+              #  .Filter("(phiDeg > 0)")
+              .Histo2D(
+                ("anglesHFPiPi_0.72_0.76_cut", ";cos#theta_{HF};#phi_{HF} [deg]", 100, -1,+1, 72, -180, +180),
+                "cosTheta", "phiDeg"
+              ).GetValue()
+    )
+    canv = ROOT.TCanvas()
+    hist.Draw("COLZ")
+    canv.RedrawAxis()
+    canv.SaveAs(f"./{hist.GetName()}.pdf")
+
+  df = ROOT.RDataFrame("PiPi", "./polarized/2018_08/tbin_0.1_0.2/PiPi/phaseSpace_acc_flat_PARA_0.root")
+  if True:
+    # decompose acceptance histogram into phi-odd and phi-even parts
+    histAccPs = df.Histo3D(
+      ("accPs3D", ";cos#theta_{HF};#phi_{HF} [deg];#Phi [deg]", 100, -1, +1, 72, -180, +180, 72, -180, +180),
+      "cosTheta", "phiDeg", "PhiDeg",
+    ).GetValue()
+    histAccPsOdd  = histAccPs.Clone(f"{histAccPs.GetName()}_odd")
+    histAccPsEven = histAccPs.Clone(f"{histAccPs.GetName()}_even")
+    assert histAccPs.GetNbinsY() % 2 == 0, "Number of phi bins must be even!"
+    for cosThetaBin in range(1, histAccPs.GetNbinsX() + 1):
+      for PhiBin in range(1, histAccPs.GetNbinsZ() + 1):
+        for phiBinNeg in range(1, histAccPs.GetNbinsY() // 2 + 1):
+          phiBinPos = histAccPs.GetYaxis().FindBin(-histAccPs.GetYaxis().GetBinCenter(phiBinNeg))
+          valPhiPos = histAccPs.GetBinContent(cosThetaBin, phiBinPos, PhiBin)
+          valPhiNeg = histAccPs.GetBinContent(cosThetaBin, phiBinNeg, PhiBin)
+          valPhiOdd  = (valPhiPos - valPhiNeg) / 2
+          valPhiEven = (valPhiPos + valPhiNeg) / 2
+          histAccPsOdd.SetBinContent (cosThetaBin, phiBinPos, PhiBin, +valPhiOdd)
+          histAccPsOdd.SetBinContent (cosThetaBin, phiBinNeg, PhiBin, -valPhiOdd)
+          histAccPsEven.SetBinContent(cosThetaBin, phiBinPos, PhiBin, valPhiEven)
+          histAccPsEven.SetBinContent(cosThetaBin, phiBinNeg, PhiBin, valPhiEven)
+    histAccPsSum = histAccPs.Clone(f"{histAccPs.GetName()}_sum")
+    histAccPsSum.Add(histAccPsOdd, histAccPsEven)
+    for hist in (histAccPsEven, histAccPsOdd, histAccPsSum):
+      hist.GetXaxis().SetTitleOffset(1.5)
+      hist.GetYaxis().SetTitleOffset(2)
+      hist.GetZaxis().SetTitleOffset(1.5)
+      with ROOT.TFile.Open(f"./{hist.GetName()}.root", "RECREATE") as outFile:
+        hist.Write()
+      hist.Rebin3D(2, 2, 2)
+    for hist in (histAccPsEven, histAccPsSum):
+      canv = ROOT.TCanvas()
+      hist.Draw("COLZ")
+      canv.SaveAs(f"./{hist.GetName()}.pdf")
+    print(f"{histAccPsOdd.GetName()}: {histAccPsOdd.GetMinimum()=}, {histAccPsOdd.GetMaximum()=}")
+    valRange = max(abs(histAccPsOdd.GetMaximum()), abs(histAccPsOdd.GetMinimum()))
+    histAccPsOdd.SetMaximum(+valRange)
+    histAccPsOdd.SetMinimum(-valRange)
+    canv = ROOT.TCanvas()
+    ROOT.gStyle.SetPalette(ROOT.kLightTemperature)  # use pos/neg color palette and symmetric z axis
+    histAccPsOdd.Draw("COLZ")
+    canv.SaveAs(f"./{histAccPsOdd.GetName()}.pdf")
+    ROOT.gStyle.SetPalette(ROOT.kBird)  # restore default color palette
+
+  if True:
+    PhiDegNmbBins = 24
+    PhiDegBinWidth = 360 / PhiDegNmbBins
+    hists = []
+    for binIndex in range(0, PhiDegNmbBins):
+      PhiDegBinMin    = -180 + binIndex * PhiDegBinWidth
+      PhiDegBinMax    = PhiDegBinMin + PhiDegBinWidth
+      PhiDegBinFilter = f"({PhiDegBinMin} < PhiDeg) and (PhiDeg < {PhiDegBinMax})"
+      histNameSuffix  = f"_{PhiDegBinMin:.0f}_{PhiDegBinMax:.0f}"
+      hists += [
+        df.Filter(PhiDegBinFilter) \
+          .Histo2D(
+            (f"accPs2D{histNameSuffix}", ";cos#theta_{HF};#phi_{HF} [deg]", 100, -1, +1, 72, -180, +180),
+            "cosTheta", "phiDeg",
+          ),
+      ]
+    for hist in hists:
+      canv = ROOT.TCanvas()
+      hist.Draw("COLZ")
+      canv.SaveAs(f"./{hist.GetName()}.pdf")
