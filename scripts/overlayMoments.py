a4c06dab79a0e1dbeacd5950cca1c34111d84924
bgrube <bgrube@jlab.org>
2026-08-10 16:28:52 -0400

    Use only one data structure to define moment results to overlay. Use full paths of pickle files to define input. Improve naming and comments.

--- scripts/overlayMoments.py
+++ scripts/overlayMoments.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 from copy import deepcopy
+from dataclasses import dataclass
 import functools
 
 import ROOT
@@ -57,40 +58,65 @@ def getHistFromMomentValues(
   return histData
 
 
+@dataclass
+class ResultToOverlay:
+  """Stores input data that define a single overlay of moment results"""
+  pklFilePath:    str  # path to pickled MomentResultsKinematicBinning file
+  label:          str  # legend label for moment values
+  scaleFactor:    float | None = None  # optional scale factor to apply to moment values
+  _momentResults: MomentResultsKinematicBinning | None = None  # moment results loaded from pklFilePath
+
+  @property
+  def momentResults(self) -> MomentResultsKinematicBinning:
+    """Returns moment results and ensures it exists"""
+    assert self._momentResults is not None, f"Moment results for '{self.label}' have not been loaded yet. Call loadMomentResults() first."
+    return self._momentResults
+
+  def loadMomentResults(self) -> MomentResultsKinematicBinning:
+    """Loads moment results from pklFilePath and caches them in _momentResults"""
+    print(f"Loading moment results from file '{self.pklFilePath}'")
+    try:
+      self._momentResults = MomentResultsKinematicBinning.loadPickle(self.pklFilePath)
+    except FileNotFoundError as e:
+      print(f"Cannot not find file '{self.pklFilePath}'. Skipping.")
+      raise
+    return self._momentResults
+
+
 def overlayMoments1D(
-  momentResultsToOverlay: dict[str, tuple[MomentResultsKinematicBinning, float | None]],  # key: legend label, value: (moment results, optional scale factor)
-  qnIndex:                QnMomentIndex,    # defines specific moment
-  binning:                HistAxisBinning,  # binning to use for plot
-  normalizedMoments:      bool = True,  # indicates whether moment values were normalized to H_0(0, 0)
-  pdfFileNamePrefix:      str  = "",    # name prefix for output files
-  styleIndexOffset:       int  = 0,     # allows to offset style indices for overlaid plots
-  styleIndexStride:       int  = 1,     # step size, by which style indices are incremented
-  yAxisUnit:              str  = "",    # allows to override default y-axis title
+  resultsToOverlay:  Sequence[ResultToOverlay],
+  qnIndex:           QnMomentIndex,    # defines specific moment
+  binning:           HistAxisBinning,  # binning to use for plot
+  normalizedMoments: bool = True,  # indicates whether moment values were normalized to H_0(0, 0)
+  pdfFileNamePrefix: str  = "",    # name prefix for output files
+  styleIndexOffset:  int  = 0,     # allows to offset style indices for overlaid plots
+  styleIndexStride:  int  = 1,     # step size, by which style indices are incremented
+  yAxisUnit:         str  = "",    # allows to override default y-axis title
 ) -> None:
-  """Overlays moments H_i(L, M) from different analyses as function of kinematical variable"""
+  """Overlays moments from different analyses as function of kinematical variable"""
   print(f"Overlaying {qnIndex.label} moments as a function of the '{binning.var.name}' variable")
   for momentPart, momentPartLabel in (("Re", "Real Part"), ("Im", "Imag Part")):  # plot real and imaginary parts separately
     histStack = ROOT.THStack(
       f"{pdfFileNamePrefix}overlay_{qnIndex.label}_{momentPart}",
       f"{qnIndex.title} {momentPartLabel};{binning.axisTitle};" + (("Normalized" if normalizedMoments else "Unnormalized") + " Moment Value") + yAxisUnit,
     )
-    for index, (legendLabel, (momentResults, scaleFactor)) in enumerate(momentResultsToOverlay.items()):
+    for overlayIndex, resultToOverlay in enumerate(resultsToOverlay):
       # filter out specific moment given by qnIndex
-      HVals: tuple[MomentValue, ...] = tuple(HPhys[qnIndex] for HPhys in momentResults if qnIndex in HPhys)
+      HVals: tuple[MomentValue, ...] = tuple(momentResult[qnIndex] for momentResult in resultToOverlay.momentResults if qnIndex in momentResult)
       histData = getHistFromMomentValues(
         HVals      = HVals,
         binning    = binning,
         momentPart = momentPart,
-        histName   = legendLabel,
+        histName   = resultToOverlay.label,
       )
       setCbFriendlyStyle(
         graphOrHist   = histData,
-        styleIndex    = index * styleIndexStride + styleIndexOffset,
+        styleIndex    = overlayIndex * styleIndexStride + styleIndexOffset,
         filledMarkers = True,
       )
-      if scaleFactor is not None:
-        print(f"Applying scale factor {scaleFactor} to fit result '{legendLabel}'")
-        histData.Scale(scaleFactor)
+      if resultToOverlay.scaleFactor is not None:
+        print(f"Applying scale factor {resultToOverlay.scaleFactor} to moment result '{resultToOverlay.label}'")
+        histData.Scale(resultToOverlay.scaleFactor)
       histStack.Add(histData, "PE1X0")
     canv = ROOT.TCanvas()
     histStack.Draw("NOSTACK")
@@ -127,10 +153,9 @@ if __name__ == "__main__":
   # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_PWA)  # perform analysis of unpolarized pi+ pi- data
   # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_JPAC)  # perform analysis of unpolarized pi+ pi- data
   # cfg = deepcopy(CFG_POLARIZED_PIPI)  # perform analysis of polarized pi+ pi- data
-  # cfg.outFileDirBaseName += ".ideal"
   # cfg.polarization = None  # treat data as unpolarized
 
-  normToFirstResult = True  # if set moments are normalized to H_0(0, 0) of first fit result
+  normToFirstResult = True  # if set moments are normalized to H_0(0, 0) of first moment result
   # normToFirstResult = False
   crossSectionScaleFactors = {
     # [ub / GeV^3] = 1 / ([40 MeV mass bin width] * [0.1 GeV^2 t bin width] * L)
@@ -147,67 +172,57 @@ if __name__ == "__main__":
                                       None
       scaleFactor_2018_08_PARA_0 = 1.8352200424810305  # scale factor to match Spring 2017 H_0(0, 0) integral for L_max = 4
       scaleFactor_2018_08_AMO = 7.241007048362434  # scale factor to match Fall 2018 PARA 0 H_0(0, 0) integral for L_max = 4
-      fitResults: tuple[tuple[str, str, str, float | None], ...] = (  # tuple: (<moment type>, <directory name>, <legend label>, optional: <scale factor>); last fit result defines which moments are plotted
-        # eta pi0
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_4",     "GJ, L_{max} = 4", None),
-        # ("phys", f"{cfg.outFileDirBaseName}.bak/{dataPeriod}/{tBinLabel}/All.maxL_4", "HF, L_{max} = 4", None),
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_4", "Physical", None),
-        # ("meas", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_4", "Measured", None),
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_5", "L_{max} = 5", None),
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_6", "L_{max} = 6", None),
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_7", "L_{max} = 7", None),
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/All.maxL_8", "L_{max} = 8", None),
-        #
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/{tBinLabel}/Unpol.maxL_4", "LOWT",   None),
-        # ("phys", f"{cfg.outFileDirBaseName}/{dataPeriod}/XSCUTS/Unpol.maxL_4",      "XSCUTS", None),
+      resultsToOverlay: tuple[ResultToOverlay, ...] = (  # last moment result in this tuple defines, which moments are plotted
+        # # eta pi0
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_4/{cfg.outFileNamePrefix}_moments_phys.pkl",     "GJ, L_{max} = 4", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}.bak/{dataPeriod}/{tBinLabel}/All.maxL_4/{cfg.outFileNamePrefix}_moments_phys.pkl", "HF, L_{max} = 4", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_4/{cfg.outFileNamePrefix}_moments_phys.pkl", "Physical", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_4/{cfg.outFileNamePrefix}_moments_phys.pkl", "Measured", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_5/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 5", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_6/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 6", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_7/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 7", None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/All.maxL_8/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 8", None),
+        # #
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}/Unpol.maxL_4/{cfg.outFileNamePrefix}_moments_phys.pkl", "LOWT",   None),
+        # ResultToOverlay(f"{cfg.outFileDirBasePath}/{dataPeriod}/XSCUTS/Unpol.maxL_4/{cfg.outFileNamePrefix}_moments_phys.pkl",      "XSCUTS", None),
         # K_S K_L
-        ("phys", cfg.outFileDirPath(dataPeriod, tBinLabel, beamPolLabel = "PARA_0", maxL = 4), "L_{max} = 4", None),
-        ("phys", cfg.outFileDirPath(dataPeriod, tBinLabel, beamPolLabel = "PARA_0", maxL = 6), "L_{max} = 6", None),
-        ("phys", cfg.outFileDirPath(dataPeriod, tBinLabel, beamPolLabel = "PARA_0", maxL = 8), "L_{max} = 8", None),
+        ResultToOverlay(f"{cfg.outFileDirPath(dataPeriod, tBinLabel, beamPolLabel = 'PARA_0', maxL = 4)}/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 4"),
+        ResultToOverlay(f"{cfg.outFileDirPath(dataPeriod, tBinLabel, beamPolLabel = 'PARA_0', maxL = 6)}/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 6"),
+        ResultToOverlay(f"{cfg.outFileDirPath(dataPeriod, tBinLabel, beamPolLabel = 'PARA_0', maxL = 8)}/{cfg.outFileNamePrefix}_moments_phys.pkl", "L_{max} = 8"),
       )
       outputDirPath = Utilities.makeDirPath(f"{cfg.outFileDirBasePath}/{dataPeriod}/{tBinLabel}.overlay")
-      # outputDirPath = Utilities.makeDirPath(f"{cfg.outFileDirBaseName}/{tBinLabel}.overlay")
 
       # load moment results
-      momentResultsToOverlay: dict[str, tuple[MomentResultsKinematicBinning, float | None]] = {}  # key: legend label, value: (moment results, optional scale factor)
-      for momentType, fitResultDirPath, fitResultLabel, scaleFactor in fitResults:
-        momentResultsFilePath = f"{fitResultDirPath}/{cfg.outFileNamePrefix}_moments_{momentType}.pkl"
-        print(f"Loading moment results from file {momentResultsFilePath}")
-        try:
-          momentResultsPhys = MomentResultsKinematicBinning.loadPickle(momentResultsFilePath)
-        except FileNotFoundError as e:
-          print(f"Cannot not find file '{momentResultsFilePath}'. Skipping directory '{fitResultDirPath}'")
-          continue
-        momentResultsToOverlay[fitResultLabel] = (momentResultsPhys, scaleFactor)
+      for resultToOverlay in resultsToOverlay:
+        resultToOverlay.loadMomentResults()
 
       # ensure that all moment results have identical kinematic binning and identical order of kinematic bins
-      momentResults: tuple[MomentResultsKinematicBinning, ...]         = tuple(value[0] for value in momentResultsToOverlay.values())
+      momentResults: tuple[MomentResultsKinematicBinning, ...]         = tuple(resultToOverlay.momentResults for resultToOverlay in resultsToOverlay)
       binCenters:    tuple[dict[KinematicBinningVariable, float], ...] = momentResults[0].binCenters  # bin centers of first moment result
       for momentResult in momentResults[1:]:
         assert momentResult.binCenters == binCenters
 
       if normToFirstResult:
-        # set scale factors such that all moments are normalized to H_0(0, 0) of the first fit result
-        firstMomentResults = list(momentResultsToOverlay.values())[0][0]
-        for label, (moments, _) in momentResultsToOverlay.items():
-          scaleFactor = moments.normalizeTo(
+        # set scale factors such that all moments are normalized to H_0(0, 0) of the first moment result
+        firstMomentResults = resultsToOverlay[0].momentResults
+        for resultToOverlay in resultsToOverlay:
+          scaleFactor = resultToOverlay.momentResults.normalizeTo(
             firstMomentResults,
             normBinIndex = None,  # normalize to integral over mass bins
           )
-          print(f"Applying scale factor {scaleFactor} to fit result '{label}'")
+          print(f"Applying scale factor {scaleFactor} to moment result '{resultToOverlay.label}'")
 
       # plot kinematic dependences of all moments
-      lastLabel = fitResults[-1][2]
-      for qnIndex in momentResultsToOverlay[lastLabel][0][0].indices.qnIndices:
+      for qnIndex in resultsToOverlay[-1].momentResults[0].indices.qnIndices:
         overlayMoments1D(
-          momentResultsToOverlay = momentResultsToOverlay,
-          qnIndex                = qnIndex,
-          binning                = cfg.massBinning,
-          normalizedMoments      = cfg.normalizeMoments,
-          pdfFileNamePrefix      = f"{outputDirPath}/{cfg.outFileNamePrefix}_phys_{cfg.massBinning.var.name}_",
-          # styleIndexOffset       = 1,
-          # styleIndexStride       = 2,
-          # yAxisUnit              = " [#mub/GeV^{3}]",
+          resultsToOverlay  = resultsToOverlay,
+          qnIndex           = qnIndex,
+          binning           = cfg.massBinning,
+          normalizedMoments = cfg.normalizeMoments,
+          pdfFileNamePrefix = f"{outputDirPath}/{cfg.outFileNamePrefix}_phys_{cfg.massBinning.var.name}_",
+          # styleIndexOffset  = 1,
+          # styleIndexStride  = 2,
+          # yAxisUnit         = " [#mub/GeV^{3}]",
         )
 
   timer.stop("Total execution time")
