d6fd7ac7d5d03a5704d0badb3f64854c2beb62c4
bgrube <bgrube@jlab.org>
2026-08-19 10:10:43 -0400

    Separate parsing of command-line arguments from code.

--- scripts/mergeSigAndBkgTrees.py
+++ scripts/mergeSigAndBkgTrees.py
@@ -19,24 +19,10 @@ from workflow import Utilities
 print = functools.partial(print, flush = True)
 
 
-if __name__ == "__main__":
+def main(args: argparse.Namespace) -> None:
   timer = Utilities.Timer()
   timer.start("Total execution time")
   ROOT.gROOT.SetBatch(True)
-
-  parser = argparse.ArgumentParser(description = "Merges signal and background trees with event weights into a single output tree.")
-  parser.add_argument("--sigFilePath",         type = str,                          help = "Path to ROOT file with signal events")
-  parser.add_argument("--bkgFilePath",         type = str,                          help = "Path to ROOT file with background events")
-  parser.add_argument("--inTreeName",          type = str,                          help = "Name of tree in input files")
-  parser.add_argument("--outFilePath",         type = str,                          help = "Path of output ROOT file")
-  parser.add_argument("--outTreeName",         type = str, default = None,          help = "Name of the output tree (default: name of input tree)")
-  parser.add_argument("--sigWeightFormula",    type = str, default = "1.0",         help = "Formula for calculating the event weight for signal events (default: '%(default)s')")
-  parser.add_argument("--bkgWeightFormula",    type = str, default = "-1.0",        help = "Formula for calculating the event weight for background events (default: '%(default)s')")
-  parser.add_argument("--weightColNameOutput", type = str, default = "eventWeight", help = "Name of column in output tree that contains the event weight (default: '%(default)s')")
-  args = parser.parse_args()
-  if args.outTreeName is None:
-    args.outTreeName = args.inTreeName
-  Utilities.print_command_line_arguments(args)
 
   mergedDf: ROOT.RDataFrame = DataConversionUtilities.getDataFrameWithCorrectEventWeights(
     dataSigRegionFilePaths  = (args.sigFilePath, ),
@@ -51,3 +37,22 @@ if __name__ == "__main__":
 
   timer.stop("Total execution time")
   print(timer.summary)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+    description = "Merges signal and background trees with event weights into a single output tree."
+  )
+  parser.add_argument("--sigFilePath",         type = str,                          help = "Path to ROOT file with signal events")
+  parser.add_argument("--bkgFilePath",         type = str,                          help = "Path to ROOT file with background events")
+  parser.add_argument("--inTreeName",          type = str,                          help = "Name of tree in input files")
+  parser.add_argument("--outFilePath",         type = str,                          help = "Path of output ROOT file")
+  parser.add_argument("--outTreeName",         type = str, default = None,          help = "Name of the output tree (default: name of input tree)")
+  parser.add_argument("--sigWeightFormula",    type = str, default = "1.0",         help = "Formula for calculating the event weight for signal events (default: '%(default)s')")
+  parser.add_argument("--bkgWeightFormula",    type = str, default = "-1.0",        help = "Formula for calculating the event weight for background events (default: '%(default)s')")
+  parser.add_argument("--weightColNameOutput", type = str, default = "eventWeight", help = "Name of column in output tree that contains the event weight (default: '%(default)s')")
+  args = parser.parse_args()
+  if args.outTreeName is None:
+    args.outTreeName = args.inTreeName
+  Utilities.print_command_line_arguments(args)
+  main(args)
