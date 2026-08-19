6fd5cda6480ecd76f56afa2b81a4e7598b0f51ef
bgrube <bgrube@jlab.org>
2026-08-19 10:13:33 -0400

    Move code that reads moment values into separate function. Add function to read covariance matrix and use it to read the three covariance matrices.

--- cpp/readMomentResultsFromJson.cc
+++ cpp/readMomentResultsFromJson.cc
@@ -3,7 +3,6 @@
 //   g++ -std=c++17 -I. -o cpp/readMomentResultsFromJson{,.cc}
 
 
-#include <cassert>
 #include <complex>
 #include <fstream>
 #include <iostream>
@@ -96,6 +95,9 @@ struct MomentResult {
 	MomentIndices                     indices;
 	std::vector<BinCenter>            binCenters;
 	std::vector<std::complex<double>> _valsFlatIndex;
+	std::vector<std::vector<double>>  _V_ReReFlatIndex;
+	std::vector<std::vector<double>>  _V_ImImFlatIndex;
+	std::vector<std::vector<double>>  _V_ReImFlatIndex;
 };
 
 
@@ -142,24 +144,56 @@ from_json(
 }
 
 void
+readMomentValue(
+	const nlohmann::json&              json,
+	std::vector<std::complex<double>>& values
+) {
+	const auto&  jsonData    = json.at("data");
+	const size_t nmbValues   = json.at("shape").at(0).get<size_t>();
+	if (jsonData.size() != nmbValues)
+		throw std::runtime_error("Data size = " + std::to_string(jsonData.size())
+			+ " does not match 'shape' = [" + std::to_string(nmbValues) + "]");
+	values.resize(nmbValues);
+	for (size_t indexValue = 0; indexValue < nmbValues; ++indexValue) {
+		const auto& jsonValue = jsonData.at(indexValue);
+		values[indexValue] = std::complex<double>(jsonValue.at("re").get<double>(), jsonValue.at("im").get<double>());
+	}
+}
+
+void
+readCovMatrix(
+	const nlohmann::json&             json,
+	std::vector<std::vector<double>>& values
+) {
+	const auto&  jsonData    = json.at("data");
+	const size_t nmbRows     = json.at("shape").at(0).get<size_t>();
+	const size_t nmbColumns  = json.at("shape").at(1).get<size_t>();
+	values.resize(nmbRows, std::vector<double>(nmbColumns));
+	if (jsonData.size() != nmbRows)
+		throw std::runtime_error("Data size = " + std::to_string(jsonData.size())
+			+ " does not match 'shape' = [" + std::to_string(nmbRows) + ", " + std::to_string(nmbColumns) + "]");
+	for (size_t indexRow = 0; indexRow < nmbRows; ++indexRow) {
+		const auto& jsonRow = jsonData.at(indexRow);
+		if (jsonRow.size() != nmbColumns)
+			throw std::runtime_error("Row size = " + std::to_string(jsonRow.size())
+				+ " does not match 'shape' = [" + std::to_string(nmbRows) + ", " + std::to_string(nmbColumns) + "]");
+		for (size_t indexCol = 0; indexCol < nmbColumns; ++indexCol) {
+			values[indexRow][indexCol] = jsonRow.at(indexCol).get<double>();
+		}
+	}
+}
+
+void
 from_json(
 	const nlohmann::json& json,
 	MomentResult&         result
 ) {
 	json.at("indices").get_to(result.indices);
 	json.at("binCenters").at("binCenters").get_to(result.binCenters);
-	// read moment values
-	const auto&  jsonNdarray = json.at("_valsFlatIndex");
-	const auto&  jsonData    = jsonNdarray.at("data");
-	const size_t nmbValues   = jsonNdarray.at("shape").at(0).get<size_t>();
-	if (jsonData.size() != nmbValues)
-		throw std::runtime_error("Data size = " + std::to_string(jsonData.size())
-			+ " does not match 'shape' = [" + std::to_string(nmbValues) + "]");
-	result._valsFlatIndex.resize(nmbValues);
-	for (size_t indexValue = 0; indexValue < nmbValues; ++indexValue) {
-		const auto& jsonValue = jsonData.at(indexValue);
-		result._valsFlatIndex[indexValue] = std::complex<double>(jsonValue.at("re").get<double>(), jsonValue.at("im").get<double>());
-	}
+	readMomentValue(json.at("_valsFlatIndex"), result._valsFlatIndex);
+	readCovMatrix(json.at("_V_ReReFlatIndex"), result._V_ReReFlatIndex);
+	readCovMatrix(json.at("_V_ImImFlatIndex"), result._V_ImImFlatIndex);
+	readCovMatrix(json.at("_V_ReImFlatIndex"), result._V_ReImFlatIndex);
 }
 
 
@@ -175,8 +209,9 @@ main()
 	}
 	nlohmann::json json;
 	inFile >> json;
-
 	std::vector<MomentResult> momentResults = json.at("results").get<std::vector<MomentResult>>();
+
+	// print data
 	for (const auto& result : momentResults) {
 		std::cout << "indices.maxL = "      << result.indices.maxL << std::endl;
 		std::cout << "indices.polarized = " << result.indices.polarized << std::endl;
@@ -188,13 +223,31 @@ main()
 			          << ", binCenter.nmbDigits = "   << binCenter.nmbDigits
 			          << std::endl;
 		}
-		assert(result.indices.nmbMoments() == result._valsFlatIndex.size() && "Mismatch between number of moments and values");
-		for (size_t flatIndex = 0; flatIndex < result.indices.nmbMoments(); ++flatIndex) {
+		const size_t nmbMoments = result.indices.nmbMoments();
+		if (nmbMoments != result._valsFlatIndex.size()) {
+			throw std::runtime_error("Mismatch between number of moments = " + std::to_string(nmbMoments)
+				+ " and values = " + std::to_string(result._valsFlatIndex.size()));
+		}
+		for (size_t flatIndex = 0; flatIndex < nmbMoments; ++flatIndex) {
 			const QnMomentIndex&        qnIndex = result.indices[flatIndex];
 			const std::complex<double>& val     = result._valsFlatIndex[flatIndex];
 			std::cout << "result._valsFlatIndex[" << flatIndex << "] = {momentIndex: " << qnIndex.momentIndex
 			          << ", L: " << qnIndex.L << ", M: " << qnIndex.M
 			          << ", val: " << val << "}" << std::endl;
+		}
+		if (nmbMoments != result._V_ReReFlatIndex.size()) {
+			throw std::runtime_error("Mismatch between number of moments = " + std::to_string(nmbMoments)
+				+ " and number of rows = " + std::to_string(result._V_ReReFlatIndex.size()));
+		}
+		for (size_t indexRow = 0; indexRow < nmbMoments; ++indexRow) {
+			const auto& row = result._V_ReReFlatIndex[indexRow];
+			if (nmbMoments != row.size()) {
+				throw std::runtime_error("Mismatch between number of moments = " + std::to_string(nmbMoments)
+					+ " and number of columns = " + std::to_string(row.size()));
+			}
+			for (size_t indexCol = 0; indexCol < row.size(); ++indexCol) {
+				std::cout << "result._V_ReReFlatIndex[" << indexRow << "][" << indexCol << "] = " << row[indexCol] << std::endl;
+			}
 		}
 	}
 
