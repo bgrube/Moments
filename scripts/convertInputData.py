e04857649b091554ab839b87738b67e17a9bf001
bgrube <bgrube@jlab.org>
2026-09-11 17:45:44 -0400

    Move code that converts input data into high-level function.

--- scripts/convertInputData.py
+++ scripts/convertInputData.py
@@ -39,7 +39,10 @@ from workflow.PlottingUtilities import (
   HistAxisBinning,
   setupPlotStyle,
 )
-from workflow.RootUtilities import loadBasisFunctionsLibrary
+from workflow.RootUtilities import (
+  loadBasisFunctionsLibrary,
+  runOnlyOnce,
+)
 from workflow import Utilities
 
 
@@ -47,11 +50,11 @@ from workflow import Utilities
 print = functools.partial(print, flush = True)
 
 
-if __name__ == "__main__":
+@runOnlyOnce
+def init() -> None:
+  """Loads libraries and initializes ROOT environment"""
   loadBasisFunctionsLibrary()  # initializes OpenMP and loads `cpp/basisFunctions.C`
   Utilities.printGitInfo()
-  timer = Utilities.Timer()
-  timer.start("Total execution time")
   ROOT.gROOT.SetBatch(True)
   # ROOT.EnableImplicitMT()
   setupPlotStyle()
@@ -63,42 +66,37 @@ if __name__ == "__main__":
   ROOT.gInterpreter.Declare(CPP_CODE_MANDELSTAM_T)
   ROOT.gInterpreter.Declare(CPP_CODE_TRACKDISTFDC)
 
-  outputColumnsUnpolarized = ("theta", "phi", "mass", "minusT")
-  outputColumnsPolarized   = ("beamPol", "beamPolPhiLabDeg", "Phi")
+
+def convertInputData(
+  cfg: AnalysisConfig,
+  outputColumnsUnpolarized: tuple[str, ...] = (
+    "theta",
+    "phi",
+    "mass",
+    "minusT",
+  ),
+  outputColumnsPolarized: tuple[str, ...] = (
+    "beamPol",
+    "beamPolPhiLabDeg",
+    "Phi",
+  ),
   #TODO move these two also into AnalysisConfig
-  additionalColumnDefs     = {  # additional columns for each data type
+  additionalColumnDefs: dict[AnalysisConfig.DataType, dict[str, str]] = {  # additional columns for each data type
     AnalysisConfig.DataType.REAL_DATA             : {},
     AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE  : {},
     AnalysisConfig.DataType.GENERATED_PHASE_SPACE : {},
-  }
-  additionalFilterDefs     = {  # additional filters for each data type
+  },
+  additionalFilterDefs: dict[AnalysisConfig.DataType, list[str]] = {  # additional filters for each data type
     AnalysisConfig.DataType.REAL_DATA             : [],
     AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE  : [],
     AnalysisConfig.DataType.GENERATED_PHASE_SPACE : [],
-  }
-  # reweightMinusTDistribution = True
-  reweightAccPSMCMinusTDistribution = False
-
-  cfg = deepcopy(CFG_POLARIZED_PIPI)  # polarized gamma p -> (pi+ pi-) p data
-  if False:  # cut away forward tracks in reconstructed data
-    for inputDataType in (AnalysisConfig.DataType.REAL_DATA, AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE):
-      lvs = lorentzVectors(dataFormat = AnalysisConfig.DataFormat.ALEX)
-      additionalColumnDefs[inputDataType] = {
-        "DistFdcPip": f"(Double32_t)trackDistFdc(pip_x4_kin.Z(), {lvs['pip']})",
-        "DistFdcPim": f"(Double32_t)trackDistFdc(pim_x4_kin.Z(), {lvs['pim']})",
-      }
-      additionalFilterDefs[inputDataType] = ["(DistFdcPip > 4) and (DistFdcPim > 4)"]  # require minimum distance of tracks at FDC position [cm]
-  # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_CLAS)  # unpolarized gamma p -> (pi+ pi-) p data in CLAS kinematic range
-  # cfg = deepcopy(CFG_POLARIZED_ETAPI0)  # polarized gamma p -> (eta pi0) p data, with eta -> gamma gamma from Nizar's analysis
-  # for inputDataType in additionalColumnDefs:
-  #   additionalColumnDefs[inputDataType] = {
-  #     "beamPol"          : "Pol",         # use this column for beam polarization degree
-  #     "beamPolPhiLabDeg" : "BeamAngle",   # use this column for beam polarization angle in lab frame
-  #   }
-  # additionalColumnDefs[AnalysisConfig.DataType.REAL_DATA]["eventWeight"] = "weightASBS"  # use this column as event weights
-  # cfg = deepcopy(CFG_UNPOLARIZED_ETAPETA)  # unpolarized gamma p -> (eta' eta) p data from Will's analysis
-  # cfg = deepcopy(CFG_POLARIZED_KSKL)  # polarized gamma p -> (K_S K_L) p data from Gabriel's analysis
-  # additionalColumnDefs[AnalysisConfig.DataType.REAL_DATA]["eventWeight"] = "Weight"  # use this column as event weight
+  },
+  reweightAccPSMCMinusTDistribution: bool = False,
+) -> None:
+  """Converts input data into the format expected by `MomentCalculator`"""
+  timer = Utilities.Timer()
+  timer.start("Total execution time")
+  init()
 
   print(f"Using analysis configuration:\n{cfg}")
   print(f"Setting up subsystem '{cfg.subsystem}':")
@@ -130,6 +128,7 @@ if __name__ == "__main__":
             additionalFilterDefs = additionalFilterDefs[inputDataType],
           ).Filter(('if (rdfentry_ == 0) { std::cout << "Running event loop" << std::endl; } return true;'))  # no-op filter that logs when event loop is running
           outputFilePath = cfg.convertedFilePath(inputDataType, dataPeriod, tBinLabel, beamPolLabel)
+          assert outputFilePath is not None
           outputTreeName = cfg.subsystem.pairLabel
           outputColumns  = outputColumnsUnpolarized
           if beamPolInfo is not None:
@@ -157,3 +156,44 @@ if __name__ == "__main__":
 
   timer.stop("Total execution time")
   print(timer.summary)
+
+
+if __name__ == "__main__":
+  additionalColumnDefs: dict[AnalysisConfig.DataType, dict[str, str]] = {  # additional columns for each data type
+    AnalysisConfig.DataType.REAL_DATA             : {},
+    AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE  : {},
+    AnalysisConfig.DataType.GENERATED_PHASE_SPACE : {},
+  }
+  additionalFilterDefs: dict[AnalysisConfig.DataType, list[str]] = {  # additional filters for each data type
+    AnalysisConfig.DataType.REAL_DATA             : [],
+    AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE  : [],
+    AnalysisConfig.DataType.GENERATED_PHASE_SPACE : [],
+  }
+
+  cfg = deepcopy(CFG_POLARIZED_PIPI)  # polarized gamma p -> (pi+ pi-) p data
+  if False:  # cut away forward tracks in reconstructed data
+    for inputDataType in (AnalysisConfig.DataType.REAL_DATA, AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE):
+      lvs = lorentzVectors(dataFormat = AnalysisConfig.DataFormat.ALEX)
+      additionalColumnDefs[inputDataType] = {
+        "DistFdcPip": f"(Double32_t)trackDistFdc(pip_x4_kin.Z(), {lvs['pip']})",
+        "DistFdcPim": f"(Double32_t)trackDistFdc(pim_x4_kin.Z(), {lvs['pim']})",
+      }
+      additionalFilterDefs[inputDataType] = ["(DistFdcPip > 4) and (DistFdcPim > 4)"]  # require minimum distance of tracks at FDC position [cm]
+  # cfg = deepcopy(CFG_UNPOLARIZED_PIPI_CLAS)  # unpolarized gamma p -> (pi+ pi-) p data in CLAS kinematic range
+  # cfg = deepcopy(CFG_POLARIZED_ETAPI0)  # polarized gamma p -> (eta pi0) p data, with eta -> gamma gamma from Nizar's analysis
+  # for inputDataType in additionalColumnDefs:
+  #   additionalColumnDefs[inputDataType] = {
+  #     "beamPol"          : "Pol",         # use this column for beam polarization degree
+  #     "beamPolPhiLabDeg" : "BeamAngle",   # use this column for beam polarization angle in lab frame
+  #   }
+  # additionalColumnDefs[AnalysisConfig.DataType.REAL_DATA]["eventWeight"] = "weightASBS"  # use this column as event weights
+  # cfg = deepcopy(CFG_UNPOLARIZED_ETAPETA)  # unpolarized gamma p -> (eta' eta) p data from Will's analysis
+  # cfg = deepcopy(CFG_POLARIZED_KSKL)  # polarized gamma p -> (K_S K_L) p data from Gabriel's analysis
+  # additionalColumnDefs[AnalysisConfig.DataType.REAL_DATA]["eventWeight"] = "Weight"  # use this column as event weight
+
+  convertInputData(
+    cfg                               = cfg,
+    additionalColumnDefs              = additionalColumnDefs,
+    additionalFilterDefs              = additionalFilterDefs,
+    # reweightAccPSMCMinusTDistribution = True,
+  )
