5c266dfeb9180f23ad5e575c4e5df2364d898a7e
bgrube <bgrube@jlab.org>
2026-09-11 15:30:50 -0400

    Improve code layout.

--- workflow/RootUtilities.py
+++ workflow/RootUtilities.py
@@ -16,6 +16,24 @@ import ROOT
 print = functools.partial(print, flush = True)
 
 
+def printRootACLiCSettings() -> None:
+  """Prints ROOT settings that affect ACLiC compilation"""
+  print(f" GetBuildArch()               = {ROOT.gSystem.GetBuildArch()}")
+  print(f" GetBuildCompiler()           = {ROOT.gSystem.GetBuildCompiler()}")
+  print(f" GetBuildCompilerVersion()    = {ROOT.gSystem.GetBuildCompilerVersion()}")
+  print(f" GetBuildCompilerVersionStr() = {ROOT.gSystem.GetBuildCompilerVersionStr()}")
+  print(f" GetBuildDir()                = {ROOT.gSystem.GetBuildDir()}")
+  print(f" GetFlagsDebug()              = {ROOT.gSystem.GetFlagsDebug()}")
+  print(f" GetFlagsOpt()                = {ROOT.gSystem.GetFlagsOpt()}")
+  print(f" GetIncludePath()             = {ROOT.gSystem.GetIncludePath()}")
+  print(f" GetDynamicPath()             = {ROOT.gSystem.GetDynamicPath()}")
+  print(f" GetLinkedLibs()              = {ROOT.gSystem.GetLinkedLibs()}")
+  print(f" GetLinkdefSuffix()           = {ROOT.gSystem.GetLinkdefSuffix()}")
+  print(f" GetLibraries()               = {ROOT.gSystem.GetLibraries()}")
+  print(f" GetMakeExe()                 = {ROOT.gSystem.GetMakeExe()}")
+  print(f" GetMakeSharedLib()           = {ROOT.gSystem.GetMakeSharedLib()}")
+
+
 def runOnce(func) -> Callable[..., Any | None]:
   """Decorator that ensures that the decorated function is executed only once; subsequent calls only return the result of the first call"""
   threadLock = threading.Lock()
@@ -33,24 +51,6 @@ def runOnce(func) -> Callable[..., Any | None]:
     return funcResult
 
   return wrapper
-
-
-def printRootACLiCSettings() -> None:
-  """Prints ROOT settings that affect ACLiC compilation"""
-  print(f" GetBuildArch()               = {ROOT.gSystem.GetBuildArch()}")
-  print(f" GetBuildCompiler()           = {ROOT.gSystem.GetBuildCompiler()}")
-  print(f" GetBuildCompilerVersion()    = {ROOT.gSystem.GetBuildCompilerVersion()}")
-  print(f" GetBuildCompilerVersionStr() = {ROOT.gSystem.GetBuildCompilerVersionStr()}")
-  print(f" GetBuildDir()                = {ROOT.gSystem.GetBuildDir()}")
-  print(f" GetFlagsDebug()              = {ROOT.gSystem.GetFlagsDebug()}")
-  print(f" GetFlagsOpt()                = {ROOT.gSystem.GetFlagsOpt()}")
-  print(f" GetIncludePath()             = {ROOT.gSystem.GetIncludePath()}")
-  print(f" GetDynamicPath()             = {ROOT.gSystem.GetDynamicPath()}")
-  print(f" GetLinkedLibs()              = {ROOT.gSystem.GetLinkedLibs()}")
-  print(f" GetLinkdefSuffix()           = {ROOT.gSystem.GetLinkdefSuffix()}")
-  print(f" GetLibraries()               = {ROOT.gSystem.GetLibraries()}")
-  print(f" GetMakeExe()                 = {ROOT.gSystem.GetMakeExe()}")
-  print(f" GetMakeSharedLib()           = {ROOT.gSystem.GetMakeSharedLib()}")
 
 
 @runOnce
