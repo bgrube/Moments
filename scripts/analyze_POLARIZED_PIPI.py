f7f93dec3dd6dab720b0357776db29d7eaada817
bgrube <bgrube@jlab.org>
2026-09-15 20:04:57 -0400

    Add step that performs MC I/O study with realistic GlueX acceptance to pi+pi- analysis script.

--- scripts/analyze_POLARIZED_PIPI.py
+++ scripts/analyze_POLARIZED_PIPI.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
   if True:
   # if False:
     print("\n=== Step 1A: plot kinematic distributions =====================================")
-    # ROOT.EnableImplicitMT()  #TODO multi-threading leads to non-reproducible results when calculating moments in step 2
+    # ROOT.EnableImplicitMT()  #TODO moment calculation gives wrong values with multi-threading enabled
     additionalFilterDefs = {  # kinematic range used in SDME analysis; for 2017_01_ver05 data
       AnalysisConfig.DataType.REAL_DATA             : ["(0.60 < massPiPi and massPiPi < 0.88)"],
       AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE  : ["(0.60 < massPiPi and massPiPi < 0.88)"],
@@ -134,7 +134,7 @@ if __name__ == "__main__":
       scaleFactorPhysicalMoments  = 1.0,  # no scaling
       # compareTo                   = None,
       # compareTo                   = ComparisonMomentsType.PWA,
-      compareTo                   = ("./plots/PiPiPol/2017_01_ver05/tbin_0.100_0.114/PARA_0.maxL_6/unnorm_moments_phys_shifted.pkl", "True Values"),
+      compareTo                   = (f"{cfg.outFileDirPath(dataPeriod = '2017_01_ver05', tBinLabel = 'tbin_0.100_0.114', beamPolLabel = 'PARA_0', maxL = 4)}/{cfg.outFileNamePrefix}_moments_phys_shifted.pkl", "Shifted Values L_{max} = 4"),  #TODO make this more general
       normalizeComparisonMoments  = True,  # scale comparison moments to estimated moments
       # plotComparisonMomentsUncert = True,
       outFileType                 = "pdf",
@@ -189,7 +189,7 @@ if __name__ == "__main__":
   # if False:
     print("\n=== Step 6: overlay weighted MC from shifted moments and real data ============")
     # weight accepted phase-space data in input format for generating kinematic plots in mass bins
-    massBinningForWeighting = deepcopy(cfg.massBinning)  # same binning as for moment values
+    massBinningForWeighting = deepcopy(cfg.massBinning)
     massBinningForWeighting.nmbBins *= 10  # finer binning than for moment values
     weightDataWithMoments(
       cfg                       = cfg,
@@ -202,7 +202,7 @@ if __name__ == "__main__":
       weightedDataDirPathSuffix = "_shifted",
     )
     # overlay weighted MC and real data
-    ROOT.EnableImplicitMT()
+    # ROOT.EnableImplicitMT()  #TODO moment calculation gives wrong values with multi-threading enabled
     plotWeightedMc(
       cfg                       = cfg,
       useIntensityTerms         = MomentResult.IntensityTermsType.PARITY_CONSERVING,
@@ -275,6 +275,65 @@ if __name__ == "__main__":
       yAxisUnit                   = "",
     )
 
+  if True:
+  # if False:
+    print("\n=== Step 8: MC input-output study with GlueX acceptance =======================")
+    maxL = 4
+    # maxL = 6
+    cfgAcc = deepcopy(CFG_POLARIZED_PIPI)
+    cfgAcc.maxLs = (maxL, )
+    # weight accepted phase-space data in converted format
+    massBinningForWeighting = deepcopy(cfg.massBinning)
+    massBinningForWeighting.nmbBins *= 10  # finer binning than for moment values
+    # weightDataWithMoments(
+    #   cfg                       = cfgAcc,
+    #   momentsFileName           = "moments_phys_shifted.pkl",
+    #   dataType                  = AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE,
+    #   useIntensityTerms         = MomentResult.IntensityTermsType.PARITY_CONSERVING,
+    #   weightInputData           = False,
+    #   massBinningForWeighting   = massBinningForWeighting,
+    #   reweightMassDistribution  = True,
+    #   weightedDataDirPathSuffix = "_shifted",
+    # )
+    # calculate moments from weighted MC
+    def convertedFilePathNoAcc(
+      cfg:          AnalysisConfig,
+      dataType:     AnalysisConfig.DataType,
+      dataPeriod:   str,
+      tBinLabel:    str,
+      beamPolLabel: str
+    ) -> str | None:
+      """Default function that returns path of data file in converted format based on data type, data period, t bin label, and beam polarization label"""
+      # one input file for each data type
+      if dataType == AnalysisConfig.DataType.REAL_DATA:
+        return f"{cfg.convertedDataDirBasePath(dataPeriod, tBinLabel)}/weightedMc.maxL_{cfg.maxLs[0]}_shifted/{beamPolLabel}/weighted_mc_ACCEPTED_PHASE_SPACE_parityConserving_flat_reweighted.root"
+      elif dataType == AnalysisConfig.DataType.ACCEPTED_PHASE_SPACE:
+        return f"{cfg.convertedDataDirBasePath(dataPeriod, tBinLabel)}/phaseSpace_acc_flat_{beamPolLabel}.root"
+      elif dataType == AnalysisConfig.DataType.GENERATED_PHASE_SPACE:
+        return f"{cfg.convertedDataDirBasePath(dataPeriod, tBinLabel)}/phaseSpace_gen_flat_{beamPolLabel}.root"
+      else:
+        raise ValueError(f"Unknown data type: {dataType}")
+    cfgAcc._convertedFilePath = convertedFilePathNoAcc
+    cfgAcc.outFileDirBasePath = f"./plots/PiPiPol.trueMaxL_{maxL}.foo"
+    calculateMoments(
+      cfg                            = cfgAcc,
+      additionalColumnDefs           = {},
+      additionalCuts                 = (
+        # "(0.100 < minusT and minusT < 0.114)",
+      ),
+      forceIntegralMatrixCalculation = True,
+    )
+    # plot moments from weighted MC
+    cfgAcc.plotMomentsInBins = True
+    plotMoments(
+      cfg                         = cfgAcc,
+      scaleFactorPhysicalMoments  = 1.0,  # no scaling
+      compareTo                   = (f"{cfg.outFileDirPath(dataPeriod = '2017_01_ver05', tBinLabel = 'tbin_0.100_0.114', beamPolLabel = 'PARA_0', maxL = maxL)}/{cfg.outFileNamePrefix}_moments_phys_shifted.pkl", "True Values"),
+      normalizeComparisonMoments  = True,  # scale comparison moments to estimated moments
+      outFileType                 = "pdf",
+      # outFileType                 = "root",
+      yAxisUnit                   = "",
+    )
 
   timer.stop("Total time for analysis")
   print(timer.summary)
